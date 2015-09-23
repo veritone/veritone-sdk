3a6e71a866380da2180db90650ab5dd74f458b4d
James Bailey <jbailey@codisms.com>
2015-09-23 05:25:58 +0000

    Code review

--- index.js
+++ index.js
@@ -610,7 +610,7 @@ ApiClient.prototype.getAssetMetadata = function getAssetMetadata(recordingId, as
 			}
 
 			if (response.statusCode !== 200) {
-				return callback('Received status: ' + response.statusCode + ' ' + util.inspect(body));
+				return callback('Received status: ' + response.statusCode, body);
 			}
 			callback(null, body);
 		});
