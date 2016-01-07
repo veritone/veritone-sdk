6034dffbed8809c4c89d58f18af00420d0bc9cf8
Mike Lin <linfongi@gmail.com>
2016-01-07 13:49:43 -0800

    VT-5738 getMetricsForAllCollections()

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function ApiClient(options) {
 }
 
 var applicationEndpoint = '/application/',
+	collectionEndpoint = '/collection/',
 	dropboxWatcherEndpoint = '/watcher/dropbox/',
 	recordingEndpoint = '/recording/',
 	facesetEndpoint = '/face-recognition/faceset/',
@@ -1959,6 +1960,48 @@ ApiClient.prototype.updateFaceset = function updateFaceset(faceset, callback) {
 			url: self._baseUri + facesetEndpoint + encodeURIComponent(faceset.faceSetId),
 			headers: generateHeaders(self._token),
 			json: faceset
+		}, function requestCallback(err, response, body) {
+			if (err) {
+				return callback(err, body);
+			}
+			if (response.statusCode !== 200) {
+				return callback('Received status: ' + response.statusCode, body);
+			}
+			callback(null, body);
+		});
+	}
+
+	self._retryHelper.retry(task, function retryCallback(err, body) {
+		if (err) {
+			return callback(err, body);
+		}
+		callback(null, body);
+	});
+};
+
+ApiClient.prototype.getMetricsForAllCollections = function getMetricsForAllCollections(options, callback) {
+	if (typeof options === 'function' && !callback) {
+		callback = options;
+		options = {};
+	} else if (typeof options !== 'object') {
+		throw new Error('Missing options!');
+	}
+	if (typeof callback !== 'function') {
+		throw new Error('Missing callback!');
+	}
+
+	var uri = this._baseUri + collectionEndpoint + 'metrics/';
+	if (options.organizationId) {
+		uri += '?organizationId=' + options.organizationId;
+	}
+
+	var self = this;
+	function task(callback) {
+		request({
+			method: 'GET',
+			uri: uri,
+			headers: generateHeaders(self._token),
+			json: true
 		}, function requestCallback(err, response, body) {
 			if (err) {
 				return callback(err, body);
