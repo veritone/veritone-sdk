862c6c4828770b0cbdcb0ae8bb7f36204adab981
Mezzi Sotoodeh <mehrzad55@gmail.com>
2017-03-13 15:40:14 -0700

    added todos

--- apis/libraries.js
+++ apis/libraries.js
@@ -11,7 +11,46 @@ module.exports = function init() {
 		getLibrary: getLibrary,
 		createLibrary: createLibrary,
 		updateLibrary: updateLibrary,
-		deleteLibrary: deleteLibrary
+		deleteLibrary: deleteLibrary,
+		publishLibraryChanges: publishLibraryChanges
+
+		// TODO:
+
+		//getLibraryTypes: getLibraryTypes,
+		//getLibraryType: getLibraryType,
+		//createLibraryType: createLibraryType,
+		//updateLibraryType: updateLibraryType,
+
+		//getLibraryEngineModels: getLibraryEngineModels,
+		//getLibraryEngineModel: getLibraryEngineModel,
+		//createLibraryEngineModel: createLibraryEngineModel,
+		//updateLibraryEngineModel: updateLibraryEngineModel,
+		//deleteLibraryEngineModel: deleteLibraryEngineModel,
+
+		//getLibraryCollaborators: getLibraryCollaborators,
+		//getLibraryCollaborator: getLibraryCollaborator,
+		//createLibraryCollaborator: createLibraryCollaborator,
+		//updateLibraryCollaborator: updateLibraryCollaborator,
+		//deleteLibraryCollaborator: deleteLibraryCollaborator,
+
+		//getEntities: getEntities,
+		//getEntity: getEntity,
+		//createEntity: createEntity,
+		//updateEntity: updateEntity,
+		//uploadEntityProfileImage: uploadEntityProfileImage,
+		//deleteEntity: deleteEntity,
+
+		//getEntityIdentifierTypes: getEntityIdentifierTypes,
+		//getEntityIdentifierType: getEntityIdentifierType,
+		//createEntityIdentifierType: createEntityIdentifierType,
+		//updateEntityIdentifierType: updateEntityIdentifierType,
+
+		//getEntityIdentifiers: getEntityIdentifiers,
+		//getEntityIdentifier: getEntityIdentifier,
+		//createEntityIdentifier: createEntityIdentifier,
+		//uploadEntityIdentifier: uploadEntityIdentifier,
+		//updateEntityIdentifier: updateEntityIdentifier,
+		//deleteEntityIdentifier: deleteEntityIdentifier,
 	};
 
 	function getLibraries(callback) {
@@ -105,6 +144,26 @@ module.exports = function init() {
 		var options = {
 			method: 'DELETE',
 			url: self._baseUri + librariesApiBaseUri + '/library/' + libraryId,
+			headers: self.generateHeaders(self._token),
+			json: true
+		};
+
+		self._retryHelper.retry(function task(callback) {
+			request(options, callback);
+		}, callback);
+	}
+
+	function publishLibraryChanges(libraryId, callback) {
+		if (!libraryId) {
+			throw new Error('libraryId is required');
+		}
+		if (typeof callback != 'function') {
+			throw new Error('Missing callback!');
+		}
+
+		var options = {
+			method: 'POST',
+			url: self._baseUri + librariesApiBaseUri + '/library/' + libraryId + '/version',
 			headers: self.generateHeaders(self._token),
 			json: true
 		};
