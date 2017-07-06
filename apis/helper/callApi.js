949a307b5d9027a50ad0beee3e4707486a3d711b
Mitch Robb <mrobb@veritone.com>
2017-07-06 15:18:26 -0700

    request options: validation, additional headers, timeouts

--- apis/helper/callApi.js
+++ apis/helper/callApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import validate from 'validate.js';
 
 const noop = () => {};
 
@@ -6,6 +7,7 @@ export function callApi({ token, baseUri }, handlerFn, requestOptions = {}) {
 	validateAuthToken(token);
 	validateBaseUri(baseUri);
 	validateHandlerFn(handlerFn);
+	validateRequestOptions(requestOptions);
 
 	return function apiRequest(
 		params,
@@ -14,8 +16,13 @@ export function callApi({ token, baseUri }, handlerFn, requestOptions = {}) {
 		requestOptionOverrides
 	) {
 		const { method, path, data, query, headers } = handlerFn(params, reqData);
+		const options = {
+			...requestOptions,
+			...requestOptionOverrides
+		};
 
-		// don't return axios' promise directly, so we can support both promise and cb styles
+		// don't return axios' promise directly, so we can support both
+		// promise and cb styles:
 		return new Promise((resolve, reject) => {
 			axios
 				.request({
@@ -25,9 +32,12 @@ export function callApi({ token, baseUri }, handlerFn, requestOptions = {}) {
 					params: query,
 					headers: {
 						Authorization: `Bearer ${token}`,
-						...headers
+						...headers,
+						...options.headers
 					},
-					baseURL: baseUri
+					baseURL: baseUri,
+					timeout: options.timeoutMs,
+
 				})
 				.then(res => {
 					resolve(res);
@@ -55,5 +65,54 @@ function validateBaseUri(uri) {
 function validateHandlerFn(fn) {
 	if (typeof fn !== 'function') {
 		throw new Error(`expected ${fn} to be a handler function`);
+	}
+}
+
+function validateRequestOptions(options) {
+	const supportedOptions = [
+		'maxRetries',
+		'withCredentials',
+		'timeoutMs',
+		'headers'
+		// 'cancelToken',
+		// onUploadProgress,
+		// onDownloadProgress,
+	];
+
+	const constraints = {
+		maxRetries: {
+			numericality: { onlyInteger: true, greaterThanOrEqualTo: 0 }
+		},
+		timeoutMs: {
+			numericality: { onlyInteger: true, greaterThanOrEqualTo: 0 }
+		},
+		withCredentials: {
+			inclusion: { within: [true, false], message: 'should be a boolean' }
+		}
+	};
+
+	Object.keys(options).forEach(opt => {
+		if (!supportedOptions.includes(opt)) {
+			throw new Error(
+				`unexpected requestOption: ${opt}. Supported options are: ` +
+					JSON.stringify(supportedOptions)
+			);
+		}
+	});
+
+	const errors = validate(options, constraints);
+
+	if (errors) {
+		throw new Error(Object.values(errors)[0])
+	}
+
+	if (
+		options.headers !== undefined &&
+		options.headers !== null &&
+		typeof options.headers !== 'object'
+	) {
+		throw new Error(
+			`requestOptions.headers should be an object. got: ${options.headers}.`
+		);
 	}
 }
--- apis/helper/callApi.spec.js
+++ apis/helper/callApi.spec.js
@@ -61,7 +61,32 @@ describe('callApi', function() {
 		).not.to.throw();
 	});
 
-	it('validates requestOptions');
+	it('validates requestOptions', function() {
+		const _callApi = this.callApi.bind(null, () => ({
+			method: 'get',
+			path: 'test-path'
+		}));
+
+		expect(() => _callApi()).not.to.throw();
+		expect(() => _callApi({ unknownOption: true })).to.throw();
+
+		expect(() => _callApi({ maxRetries: 0 })).not.to.throw();
+		expect(() => _callApi({ maxRetries: -1 })).to.throw();
+		expect(() => _callApi({ maxRetries: 1.1 })).to.throw();
+
+		expect(() => _callApi({ timeoutMs: 0 })).not.to.throw();
+		expect(() => _callApi({ timeoutMs: -1 })).to.throw();
+		expect(() => _callApi({ timeoutMs: 1.1 })).to.throw();
+
+		expect(() => _callApi({ withCredentials: 'ok' })).to.throw();
+		expect(() => _callApi({ withCredentials: false })).not.to.throw();
+		expect(() => _callApi({ withCredentials: true })).not.to.throw();
+
+		expect(() => _callApi({ headers: 'ok' })).to.throw();
+		expect(() => _callApi({ headers: {} })).not.to.throw();
+		expect(() => _callApi({ headers: { someHeader: 'ok' } })).not.to.throw();
+		expect(() => _callApi({ headers: null })).not.to.throw();
+	});
 
 	it('returns a function', function() {
 		const requestFn = this.callApi(() => ({
@@ -211,6 +236,70 @@ describe('callApi', function() {
 			path: 'test-path',
 			headers: { it: 'worked' }
 		}));
+
+		return requestFn().then(() => scope.done());
+	});
+
+	it('should attach additional headers if specified in requestOptions', function() {
+		const scope = nock(apiBaseUri, {
+			reqheaders: {
+				it: 'worked'
+			}
+		})
+			.get('/test-path')
+			.reply(200, 'ok');
+
+		const requestFn = this.callApi(
+			() => ({
+				method: 'get',
+				path: 'test-path'
+			}),
+			{ headers: { it: 'worked' } }
+		);
+
+		return requestFn().then(() => scope.done());
+	});
+
+	it('should use a request timeout if timeout option is specified (times out)', function(
+		done
+	) {
+		const scope = nock(apiBaseUri)
+			.get('/test-path')
+			.delayConnection(1000)
+			.reply(200, 'ok');
+
+		const requestFn = this.callApi(
+			() => ({
+				method: 'get',
+				path: 'test-path'
+			}),
+			{ timeoutMs: 500 }
+		);
+
+		requestFn()
+			.then(() => {
+				done(new Error('Expected requestFn to throw.'));
+			})
+			.catch(err => {
+				expect(err.code).to.equal('ECONNABORTED');
+				done();
+				scope.done();
+			});
+	});
+
+	it('should use a request timeout if timeout option is specified (does not time out)', function() {
+		const scope = nock(apiBaseUri)
+			.get('/test-path')
+			.delayConnection(50)
+			.reply(200, 'ok');
+
+		const requestFn = this.callApi(
+			() => ({
+				method: 'get',
+				path: 'test-path'
+			}),
+			{ timeoutMs: 100 }
+		);
 
 		return requestFn().then(() => scope.done());
 	});
