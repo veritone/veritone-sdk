80c45eca8f766992290530cd81865a3a2df10816
Mitch Robb <mrobb@veritone.com>
2017-07-14 16:58:57 -0700

    kill path helper -- not needed

--- apis/helper/util.js
+++ apis/helper/util.js
@@ -1,36 +1,3 @@
-import Route from 'route-parser';
-
-export function path(path, params = {}) {
-	validatePath(path);
-	validateParams(params);
-
-	const route = new Route(path);
-	const pathWithParams = route.reverse(params);
-
-	if (!pathWithParams) {
-		const requiredRouteParams = Object.keys(route.match(path));
-		throw new Error(
-			'all path parameters are required to make a request; ' +
-				`required: ${requiredRouteParams}. ` +
-				`got: ${Object.keys(params)}.`
-		);
-	}
-
-	return pathWithParams.startsWith('/') ? pathWithParams : `/${pathWithParams}`;
-}
-
-function validatePath(path) {
-	if (typeof path !== 'string' || path.length === 0) {
-		throw new Error(`expected path ${path} to be a string of length > 0.`);
-	}
-}
-
-function validateParams(params) {
-	if (params && typeof params !== 'object') {
-		throw new Error(`expected ${params} to be an object.`);
-	}
-}
-
 export function mapObject(obj, fn) {
 	let result = {};
 
