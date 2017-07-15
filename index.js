decb30a38692892b32d5fdca37558f1a74dfcf90
Mitch Robb <mrobb@veritone.com>
2017-07-14 17:05:53 -0700

    import polyfills

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import veritoneApi from './apis/helper/ApiClient';
 import apis from './apis';
 import legacyApis from './apis/legacy_deleteme'
+import './polyfill';
 
 export default function ApiClient(options) {
 	return veritoneApi(options, apis, legacyApis)
