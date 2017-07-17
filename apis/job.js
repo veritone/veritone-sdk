d61e8e9059d929f7fe325b74ccaf612dc928a86b
Mitch Robb <mrobb@veritone.com>
2017-07-16 23:54:54 -0700

    migrate job endpoints

---
+++ apis/job.js
@@ -0,0 +1,103 @@
+import validate from 'validate.js';
+import { endpoints } from './config';
+
+export default {
+	createJob(job) {
+		if (typeof job !== 'object') {
+			throw new Error('Missing job!');
+		}
+		if (typeof job.recordingId === 'number') {
+			job.recordingId = job.recordingId + '';
+		}
+
+		const validation = {
+			tasks: {
+				presence: true
+			}
+		};
+		const validationErrors = validate(job, validation);
+		if (validationErrors) {
+			throw new Error('Invalid job object!');
+		}
+
+		return {
+			method: 'post',
+			path: endpoints.job,
+			data: job
+		};
+	},
+
+	getJobs({ limit, offset } = {}) {
+		return {
+			method: 'get',
+			path: endpoints.job,
+			query: { limit, offset }
+		}
+	},
+
+	getJobsForRecording(options) {
+		if (typeof options === 'string') {
+			options = {
+				recordingId: options
+			};
+		} else if (typeof options !== 'object') {
+			throw new Error('Missing options!');
+		}
+		if (typeof options.recordingId === 'number') {
+			options.recordingId = options.recordingId + '';
+		}
+		if (typeof options.recordingId !== 'string' || options.recordingId === '') {
+			throw new Error('Missing options.recordingId!');
+		}
+
+		return {
+			method: 'get',
+			path: `${endpoints.job}/recording/${options.recordingId}`,
+			query: { offset: options.offset, limit: options.limit }
+		};
+	},
+
+	getJob(jobId) {
+		if (typeof jobId !== 'string' || jobId === '') {
+			throw new Error('Missing jobId!');
+		}
+
+		return {
+			method: 'get',
+			path: `${endpoints.job}/${jobId}`
+		};
+	},
+
+	restartJob(jobId) {
+		if (typeof jobId !== 'string' || jobId === '') {
+			throw new Error('Missing jobId!');
+		}
+
+		return {
+			method: 'put',
+			path: `${endpoints.job}/${jobId}/restart`
+		};
+	},
+
+	retryJob(jobId) {
+		if (typeof jobId !== 'string' || jobId === '') {
+			throw new Error('Missing jobId!');
+		}
+
+		return {
+			method: 'put',
+			path: `${endpoints.job}/${jobId}/retry`
+		};
+	},
+
+	cancelJob(jobId) {
+		if (typeof jobId !== 'string' || jobId === '') {
+			throw new Error('Missing jobId!');
+		}
+
+		return {
+			method: 'delete',
+			path: `${endpoints.job}/${jobId}`
+		};
+	},
+}
---
+++ apis/job.spec.js
@@ -0,0 +1,183 @@
+import { expect } from 'chai';
+import nock from 'nock';
+nock.disableNetConnect();
+
+import { noop } from './helper/util';
+import { assertMatches } from '../apis/helper/test-util';
+import jobHandlers from './job';
+
+const apiBaseUrl = 'http://fake.domain';
+
+describe('Job', function() {
+	describe('createJob', function() {
+		it('validates the job', function() {
+			expect(() => jobHandlers.createJob(undefined, noop)).to.throw(/job/);
+			expect(() => jobHandlers.createJob({}, noop)).to.throw(/job/);
+			expect(() => jobHandlers.createJob({ tasks: 'ok' }, noop)).not.to.throw(
+				/job/
+			);
+		});
+
+		it('posts the job to the API', function() {
+			const job = { tasks: 'ok' };
+
+			const expected = {
+				method: 'post',
+				path: /job/,
+				data: job
+			};
+
+			const result = jobHandlers.createJob(job);
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('getJob', function() {
+		it('validates jobId', function() {
+			expect(() => jobHandlers.getJob(undefined, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.getJob({}, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.getJob('ok', noop)).not.to.throw(/jobId/);
+		});
+
+		it('gets the job from the API', function() {
+			const expected = {
+				method: 'get',
+				path: /job-id/
+			};
+
+			const result = jobHandlers.getJob('job-id');
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('getJobs', function() {
+		it('makes a get request to list jobs', function() {
+			const expected = {
+				method: 'get',
+				path: /job/
+			};
+
+			const result = jobHandlers.getJobs();
+			assertMatches(result, expected);
+		});
+
+		it('can be called with offset/limit', function() {
+			const expected = {
+				method: 'get',
+				path: /job/,
+				query: {
+					offset: 1,
+					limit: 2
+				}
+			};
+
+			const result = jobHandlers.getJobs({ offset: 1, limit: 2 });
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('getJobsForRecording', function() {
+		it('validates options', function() {
+			expect(() => jobHandlers.getJobsForRecording(undefined, noop)).to.throw(
+				/options/
+			);
+			expect(() => jobHandlers.getJobsForRecording({}, noop)).to.throw(
+				/recordingId/
+			);
+		});
+
+		it('can be called with a string recordingId', function() {
+			const expected = {
+				method: 'get',
+				path: /recording\/recording-id/
+			};
+
+			const result = jobHandlers.getJobsForRecording('recording-id');
+			assertMatches(result, expected);
+		});
+
+		it('can be called with a recording object', function() {
+			const expected = {
+				method: 'get',
+				path: /recording\/recording-id/
+			};
+
+			const result = jobHandlers.getJobsForRecording({
+				recordingId: 'recording-id'
+			});
+			assertMatches(result, expected);
+		});
+
+		it('can be called with offset/limit', function() {
+			const expected = {
+				method: 'get',
+				path: /recording\/recording-id/,
+				query: {
+					offset: 1,
+					limit: 2
+				}
+			};
+
+			const result = jobHandlers.getJobsForRecording({
+				recordingId: 'recording-id',
+				offset: 1,
+				limit: 2
+			});
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('restartJob', function() {
+		it('validates jobId', function() {
+			expect(() => jobHandlers.restartJob(undefined, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.restartJob({}, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.restartJob('ok', noop)).not.to.throw(/jobId/);
+		});
+
+		it("puts to the job's restart endpoint (no body)", function() {
+			const expected = {
+				method: 'put',
+				path: /job-id\/restart/
+			};
+
+			const result = jobHandlers.restartJob('job-id');
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('retryJob', function() {
+		it('validates jobId', function() {
+			expect(() => jobHandlers.retryJob(undefined, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.retryJob({}, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.retryJob('ok', noop)).not.to.throw(/jobId/);
+		});
+
+		it('puts the job to the API', function() {
+			const expected = {
+				method: 'put',
+				path: /job-id\/retry/
+			};
+
+			const result = jobHandlers.retryJob('job-id');
+			assertMatches(result, expected);
+		});
+	});
+
+	describe('cancelJob', function() {
+		it('validates jobId', function() {
+			expect(() => jobHandlers.cancelJob(undefined, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.cancelJob({}, noop)).to.throw(/jobId/);
+			expect(() => jobHandlers.cancelJob('ok', noop)).not.to.throw(/jobId/);
+		});
+
+		it('makes a delete request to the job', function() {
+			const expected = {
+				method: 'delete',
+				path: /job-id/
+			};
+
+			const result = jobHandlers.cancelJob('job-id');
+			assertMatches(result, expected);
+		});
+	});
+});
