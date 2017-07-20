7785d897b2fce94ce7acf56e4bda39e480c3dd9d
Mitch Robb <mrobb@veritone.com>
2017-07-20 12:43:01 -0700

    return a plain objecet from libraries

--- apis/libraries.js
+++ apis/libraries.js
@@ -1,51 +1,48 @@
-import { endpoints } from './config';
+import generateHandler from './helper/generate-handler';
 
-module.exports = function init() {
-	const generateHandler = require('./helper/generate-handler')(this, this._baseUri + endpoints.libraries);
+//prettier-ignore
+export default {
+	getLibraryTypes: generateHandler('GET', '/library-type'),
+	getLibraryType: generateHandler('GET', '/library-type/:libraryTypeId'),
+	createLibraryType: generateHandler('POST', '/library-type'),
+	updateLibraryType: generateHandler('PUT', '/library-type/:libraryTypeId'),
 
-	return {
-		getLibraryTypes: generateHandler('GET', '/library-type'),
-		getLibraryType: generateHandler('GET', '/library-type/:libraryTypeId'),
-		createLibraryType: generateHandler('POST', '/library-type'),
-		updateLibraryType: generateHandler('PUT', '/library-type/:libraryTypeId'),
+	getLibraries: generateHandler('GET', '/library'),
+	getLibrary: generateHandler('GET', '/library/:libraryId'),
+	createLibrary: generateHandler('POST', '/library'),
+	updateLibrary: generateHandler('PUT', '/library/:libraryId'),
+	deleteLibrary: generateHandler('DELETE', '/library/:libraryId'),
+	publishLibraryChanges: generateHandler('POST', '/library/:libraryId/version'),
 
-		getLibraries: generateHandler('GET', '/library'),
-		getLibrary: generateHandler('GET', '/library/:libraryId'),
-		createLibrary: generateHandler('POST', '/library'),
-		updateLibrary: generateHandler('PUT', '/library/:libraryId'),
-		deleteLibrary: generateHandler('DELETE', '/library/:libraryId'),
-		publishLibraryChanges: generateHandler('POST', '/library/:libraryId/version'),
+	getLibraryEngineModels: generateHandler('GET', '/library/:libraryId/engine-model'),
+	getLibraryEngineModel: generateHandler('GET', '/library/:libraryId/engine-model/:libraryEngineModelId'),
+	createLibraryEngineModel: generateHandler('POST', '/library/:libraryId/engine-model'),
+	updateLibraryEngineModel: generateHandler('PUT', '/library/:libraryId/engine-model/:libraryEngineModelId'),
+	deleteLibraryEngineModel: generateHandler('DELETE', '/library/:libraryId/engine-model/:libraryEngineModelId'),
 
-		getLibraryEngineModels: generateHandler('GET', '/library/:libraryId/engine-model'),
-		getLibraryEngineModel: generateHandler('GET', '/library/:libraryId/engine-model/:libraryEngineModelId'),
-		createLibraryEngineModel: generateHandler('POST', '/library/:libraryId/engine-model'),
-		updateLibraryEngineModel: generateHandler('PUT', '/library/:libraryId/engine-model/:libraryEngineModelId'),
-		deleteLibraryEngineModel: generateHandler('DELETE', '/library/:libraryId/engine-model/:libraryEngineModelId'),
+	getLibraryCollaborators: generateHandler('GET', '/library/:libraryId/collaborator'),
+	getLibraryCollaborator: generateHandler('GET', '/library/:libraryId/collaborator/:collaboratorOrgId'),
+	createLibraryCollaborator: generateHandler('POST', '/library/:libraryId/collaborator'),
+	updateLibraryCollaborator: generateHandler('PUT', '/library/:libraryId/collaborator/:collaboratorOrgId'),
+	deleteLibraryCollaborator: generateHandler('DELETE', '/library/:libraryId/collaborator/:collaboratorOrgId'),
 
-		getLibraryCollaborators: generateHandler('GET', '/library/:libraryId/collaborator'),
-		getLibraryCollaborator: generateHandler('GET', '/library/:libraryId/collaborator/:collaboratorOrgId'),
-		createLibraryCollaborator: generateHandler('POST', '/library/:libraryId/collaborator'),
-		updateLibraryCollaborator: generateHandler('PUT', '/library/:libraryId/collaborator/:collaboratorOrgId'),
-		deleteLibraryCollaborator: generateHandler('DELETE', '/library/:libraryId/collaborator/:collaboratorOrgId'),
+	getEntityIdentifierTypes: generateHandler('GET', '/entity-identifier-type'),
+	getEntityIdentifierType: generateHandler('GET', '/entity-identifier-type/:entityIdentifierTypeId'),
+	createEntityIdentifierType: generateHandler('POST', '/entity-identifier-type'),
+	updateEntityIdentifierType: generateHandler('PUT', '/entity-identifier-type/:entityIdentifierTypeId'),
 
-		getEntityIdentifierTypes: generateHandler('GET', '/entity-identifier-type'),
-		getEntityIdentifierType: generateHandler('GET', '/entity-identifier-type/:entityIdentifierTypeId'),
-		createEntityIdentifierType: generateHandler('POST', '/entity-identifier-type'),
-		updateEntityIdentifierType: generateHandler('PUT', '/entity-identifier-type/:entityIdentifierTypeId'),
+	getEntities: generateHandler('GET', '/library/:libraryId/entity'),
+	getEntity: generateHandler('GET', '/library/:libraryId/entity/:entityId'),
+	createEntity: generateHandler('POST', '/library/:libraryId/entity'),
+	updateEntity: generateHandler('PUT', '/library/:libraryId/entity/:entityId'),
+	uploadEntityProfileImage: generateHandler('POST', '/library/:libraryId/entity/:entityId/profile-image', ['Content-Type']),
+	deleteEntity: generateHandler('DELETE', '/library/:libraryId/entity/:entityId'),
+	entityLookup: generateHandler('POST', '/entity-lookup'),
 
-		getEntities: generateHandler('GET', '/library/:libraryId/entity'),
-		getEntity: generateHandler('GET', '/library/:libraryId/entity/:entityId'),
-		createEntity: generateHandler('POST', '/library/:libraryId/entity'),
-		updateEntity: generateHandler('PUT', '/library/:libraryId/entity/:entityId'),
-		uploadEntityProfileImage: generateHandler('POST', '/library/:libraryId/entity/:entityId/profile-image', ['Content-Type']),
-		deleteEntity: generateHandler('DELETE', '/library/:libraryId/entity/:entityId'),
-		entityLookup: generateHandler('POST', '/entity-lookup'),
-
-		getEntityIdentifiers: generateHandler('GET', '/library/:libraryId/entity/:entityId/identifier'),
-		getEntityIdentifier: generateHandler('GET', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId'),
-		createEntityIdentifier: generateHandler('POST', '/library/:libraryId/entity/:entityId/identifier'),
-		updateEntityIdentifier: generateHandler('PUT', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId'),
-		uploadEntityIdentifier: generateHandler('POST', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierTypeId', ['Content-Type']),
-		deleteEntityIdentifier: generateHandler('DELETE', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId'),
-	};
+	getEntityIdentifiers: generateHandler('GET', '/library/:libraryId/entity/:entityId/identifier'),
+	getEntityIdentifier: generateHandler('GET', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId'),
+	createEntityIdentifier: generateHandler('POST', '/library/:libraryId/entity/:entityId/identifier'),
+	updateEntityIdentifier: generateHandler('PUT', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId'),
+	uploadEntityIdentifier: generateHandler('POST', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierTypeId', ['Content-Type']),
+	deleteEntityIdentifier: generateHandler('DELETE', '/library/:libraryId/entity/:entityId/identifier/:entityIdentifierId')
 };
