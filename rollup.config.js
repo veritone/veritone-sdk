df9d7c06e75ec2c74a05e57712d418b3d737bd7a
Mitch Robb <mrobb@veritone.com>
2017-07-25 16:24:52 -0700

    add minification/sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import babili from 'rollup-plugin-babili';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 
 export default {
 	entry: './index.js',
 	format: 'cjs',
+	sourceMap: true,
 	plugins: [
 		babel({
 			exclude: 'node_modules/**'
 		}),
 		resolve({ jsnext: true }),
 		commonjs(),
-		json()
+		json(),
+		babili({
+			comments: false,
+			sourceMap: true
+		})
 	],
 	dest: 'dist/bundle.js'
 };
