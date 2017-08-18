e679262d00e6a833d6f4f1b33ee115a14d1cbf88
Mitch Robb <mrobb@veritone.com>
2017-08-18 11:20:16 -0700

    add babel polyfill to entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MinifyPlugin = require('babili-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-	entry: './index.js',
+	entry: ['babel-polyfill', './index.js'],
 	output: {
 		filename: process.env.BUILD_ENV === 'browser'
 		 ? 'dist/bundle-browser.js'
