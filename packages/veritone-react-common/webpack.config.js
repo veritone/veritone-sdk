cd9be96c2a9b7cb4823a280c9fafebe058b1f1be
Mitch Robb <mrobb@veritone.com>
2017-09-28 01:19:09 -0700

    working import between veritone-react-common and veritone-widgets

---
+++ packages/veritone-react-common/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: { 'veritone-react-common': path.join(__dirname, './src/index.js')},
+  output: {
+    filename: 'dist/bundle.js',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  resolve: {
+    alias: {
+      helpers: path.join(__dirname, '../src/helpers')
+    },
+  },
+  module: {
+    // noParse: [],
+    rules: [
+      // JavaScript / ES6
+      {
+        test: /\.jsx?$/,
+        include: path.resolve('./src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[local]--[hash:base64:5]'
+            }
+          },
+          'sass-loader'
+        ],
+        include: path.resolve('./src')
+      }
+    ]
+  }
+};
