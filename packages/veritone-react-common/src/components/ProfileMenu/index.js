17f5f13cca0fc630888720e389cde9164ac9f097
Mitch Robb <olordhux+github@gmail.com>
2017-10-14 18:02:23 -0700

    move contents of ProfileMenu into InnerProfileMenu so it can be tested independent of menu/portals

--- packages/veritone-react-common/src/components/ProfileMenu/index.js
+++ packages/veritone-react-common/src/components/ProfileMenu/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { get } from 'lodash';
 import AccountIcon from 'material-ui-icons/AccountCircle';
-import PowerIcon from 'material-ui-icons/PowerSettingsNew';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import { ListItemIcon, ListItemText } from 'material-ui/List';
-import Button from 'material-ui/Button';
+import Menu from 'material-ui/Menu';
 import IconButton from 'material-ui/IconButton';
-import Avatar from 'material-ui/Avatar';
-import ListSubheader from 'material-ui/List/ListSubheader';
 import { string, func, shape } from 'prop-types';
 
 import withMuiThemeProvider from 'helpers/withMuiThemeProvider';
 
-import styles from './styles.scss';
+import InnerProfileMenu from './InnerProfileMenu';
 
 @withMuiThemeProvider
 export default class ProfileMenu extends React.Component {
@@ -49,6 +43,7 @@ export default class ProfileMenu extends React.Component {
   };
 
   handleEditProfile = () => {
+    // fixme: show edit profile modal
     window.location =
       'https://enterprise.veritone.com/switch-app/default/edit-profile';
   };
@@ -56,11 +51,7 @@ export default class ProfileMenu extends React.Component {
   render() {
     return (
       <div>
-        <IconButton
-          // style={{ fontSize: 'inherit' }} // fixme: needed?
-          className={this.props.className}
-          onClick={this.openMenu}
-        >
+        <IconButton className={this.props.className} onClick={this.openMenu}>
           <AccountIcon color="white" />
         </IconButton>
         <Menu
@@ -72,37 +63,11 @@ export default class ProfileMenu extends React.Component {
           // https://github.com/callemall/material-ui/issues/7961#issuecomment-326215406
           getContentAnchorEl={null}
         >
-          <ListSubheader className={styles['header']}>
-            <div className={styles['user-avatar']}>
-              <Avatar
-                src={get(
-                  this.props.user,
-                  'kvp.image',
-                  '//static.veritone.com/veritone-ui/default-avatar-2.png'
-                )}
-              />
-            </div>
-            <div className={styles['user-profile']}>
-              <div className={styles['full-name']}>
-                {get(this.props.user, 'kvp.firstName')}&nbsp;
-                {get(this.props.user, 'kvp.lastName')}
-              </div>
-              <div className={styles['username']}>
-                {get(this.props.user, 'userName')}
-              </div>
-              <div className={styles['editButton']}>
-                <Button raised color="primary" onClick={this.handleEditProfile}>
-                  Edit Profile
-                </Button>
-              </div>
-            </div>
-          </ListSubheader>
-          <MenuItem onClick={this.handleLogout}>
-            <ListItemIcon>
-              <PowerIcon />
-            </ListItemIcon>
-            <ListItemText primary="Log out" />
-          </MenuItem>
+          <InnerProfileMenu
+            user={this.props.user}
+            onLogout={this.handleLogout}
+            onEditProfile={this.handleEditProfile}
+          />
         </Menu>
       </div>
     );
