9730242b8b55c33935f39682b1fd3712f33066f5
Mitch Robb <mrobb@veritone.com>
2017-11-01 13:42:40 -0700

    add new exports

--- packages/veritone-react-common/src/index.js
+++ packages/veritone-react-common/src/index.js
@@ -1,9 +1,12 @@
 import '../src/styles/global.scss';
 import 'normalize.css';
 
-export Avatar from './components/Avatar';
 export AppBar from './components/AppBar';
+export AppFooter from './components/AppFooter';
 export AppSwitcher from './components/AppSwitcher';
+export Avatar from './components/Avatar';
+export Chip from './components/Chip';
+export DiscoverySideBar from './components/DiscoverySideBar';
 export FullScreenDialog from './components/FullScreenDialog';
 export ProfileMenu from './components/ProfileMenu';
 export RaisedTextField from './components/RaisedTextField';
