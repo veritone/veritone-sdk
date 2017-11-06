b32494d7add498070c3cb852af6e8aee4dff77e7
Mitch Robb <mrobb@veritone.com>
2017-11-06 15:54:00 -0800

    Add config, login behavior to VeritoneApp

--- packages/veritone-widgets/src/shared/VeritoneApp.js
+++ packages/veritone-widgets/src/shared/VeritoneApp.js
@@ -7,24 +7,27 @@ import { Provider } from 'react-redux';
 import { object, arrayOf } from 'prop-types';
 
 import { modules } from 'veritone-redux-common';
-const userModule = modules.user;
+const { user: userModule, config: configModule } = modules;
 
+import appConfig from '../../config.json';
 import configureStore from '../redux/configureStore';
 
 export default class VeritoneApp {
-  constructor(...widgets) {
-    this._widgets = widgets;
-  }
-
   _store = configureStore();
   _containerEl = null;
   _token = null;
+
+  constructor(...widgets) {
+    this._widgets = widgets;
+
+    this._store.dispatch(configModule.setConfig(appConfig));
+  }
 
   // auth can follow two paths:
   // 1. internal apps, make /current-user api call with appInstance.login().
   //    this can happen automatically.
   // 2. oauth apps, popup window flow. must be initiated by user action (clicking a button).
-  //    oauth apps must add an OAuthLoginButtonWidget
+  //    oauth apps must add an OAuthLoginButtonWidget and do not need to call login()
   login({ token } = {}) {
     // new VeritoneApp(...widgets)
     //   .login() // try to use an existing cookie
@@ -32,12 +35,21 @@ export default class VeritoneApp {
     //   .login({ token }); // use provided token in header
 
     // todo: handle promise result
+    // make sure it rejects on bad auth
     if (token) {
       this._token = token;
-      return this._store.dispatch(userModule.fetchUser({ token }));
+      return this._store
+        .dispatch(userModule.fetchUser({ token }))
+        .then(this._handleLoginResponse);
     } else {
-      return this._store.dispatch(userModule.fetchUser());
+      return this._store
+        .dispatch(userModule.fetchUser())
+        .then(this._handleLoginResponse);
     }
+  }
+
+  _handleLoginResponse(action) {
+    return action.error ? Promise.reject(action) : action;
   }
 
   mount() {
--- packages/veritone-widgets/src/widgets/AppBar/story.js
+++ packages/veritone-widgets/src/widgets/AppBar/story.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
 
 import VeritoneApp from '../../shared/VeritoneApp';
 import AppBarWidget from './';
-
-import { login } from '../../shared/VeritoneAuth';
-const handleLogin = () =>
-  login('http://localhost:5001/auth/veritone')
-    .then(a => console.log(a))
-    .catch(e => console.log('error', e));
 
 const app = new VeritoneApp(
   new AppBarWidget({
@@ -19,10 +14,9 @@ const app = new VeritoneApp(
   })
 );
 
-const mountApp = app.mount.bind(app);
-const destroyApp = app.destroy.bind(app);
-
 storiesOf('AppBar', module).add('Base', () => {
+  const token = text('Api Session Token', 'fixme');
+
   return (
     <div>
       this is where the widget should go:
@@ -31,9 +25,11 @@ storiesOf('AppBar', module).add('Base', () => {
       <br />
       <br />
       <br />
-      <button onClick={mountApp}>Mount</button>
-      <button onClick={destroyApp}>destroy</button>
-      <button onClick={handleLogin}>log in</button>
+      <button onClick={() => app.mount()}>Mount</button>
+      <button onClick={() => app.destroy()}>destroy</button>
+      <button onClick={() => app.login({ token })}>
+        log in (set token in knobs panel)
+      </button>
     </div>
   );
 });
