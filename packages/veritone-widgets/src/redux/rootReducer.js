4f8b270dd9f807b9642c1e1c9ef519237ada3653
Mitch Robb <mrobb@veritone.com>
2017-11-07 12:21:03 -0800

    oauth reducer; get oauth token into state

--- packages/veritone-widgets/src/redux/rootReducer.js
+++ packages/veritone-widgets/src/redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-
 import { modules } from 'veritone-redux-common';
+
+import OAuthReducer, { namespace as OAuthNamespace } from './modules/oauth';
 
 const {
   user: { reducer: userReducer, namespace: userNamespace },
@@ -9,6 +10,7 @@ const {
 
 export default function createReducer(asyncReducers) {
   return combineReducers({
+    [OAuthNamespace]: OAuthReducer,
     [configNamespace]: configReducer,
     [userNamespace]: userReducer,
     ...asyncReducers
