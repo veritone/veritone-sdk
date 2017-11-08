0e9efc6cbcc3f9cec8fedd4ff9a22f820e3753d6
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-07 19:28:24 -0800

    Added some more tests for the FileUploader.

--- packages/veritone-react-common/src/components/FilePicker/index.test.js
+++ packages/veritone-react-common/src/components/FilePicker/index.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Tab } from 'material-ui/Tabs';
+import Button from 'material-ui/Button';
 
 import FilePicker from './';
 import FileUploader from './FileUploader';
@@ -10,29 +12,53 @@ describe('FilePicker', () => {
         wrapper = mount(<FilePicker />);
     });
 
-    it('renders without crashing', () => {
-        expect(wrapper).toBeDefined();
+    it('has tab bar with two tabs', () => {
+        expect(wrapper.find(Tab).length).toEqual(2);
     });
 
-    it('has tab bar with two tabs', () => {
-        const tabsBar = wrapper.find('Tabs');
-        expect(tabsBar.find('Tab').length).toEqual(2);
-    })
+    describe('when "Upload" tab is clicked', () => {
+        let uploadTab;
+        beforeEach(() => {
+            uploadTab = wrapper.find('Tab[label="Upload"]');
+        })
+        it('should display a FileUploader component', () => {
+            uploadTab.simulate('click');
+            expect(wrapper.find(FileUploader).length).toEqual(1);
+        });
+    });
+
+    describe('when "By Url" tab is clicked', () => {
+        let byUrlTab;
+        beforeEach(() => {
+            byUrlTab = wrapper.find('Tab[label="By URL"]');
+        })
+        it('should hide the FileUploader component', () => {
+            byUrlTab.simulate('click');
+            expect(wrapper.find(FileUploader).length).toEqual(0);
+        });
+    });
 });
 
 describe('FileUploader', () => {
     let wrapper;
+    const sampleFiles = {
+        0: {
+            
+        }
+    };
+
     beforeEach(() => {
         wrapper = mount(<FileUploader />);
     });
 
-    it('renders without crashing', () => {
-        expect(wrapper).toBeDefined();
+    it('should contain a "CHOOSE FILE" button when a file has not been selected', () => {
+        const chooseFileButton = wrapper.find(Button);
+        expect(chooseFileButton.length).toEqual(1);
+        expect(chooseFileButton.text()).toMatch(/choose file/i);
     });
 
-    it('should contain a "CHOOSE FILE" button when a file has not been selected', () => {
-        const chooseFileButton = wrapper.find('Button');
-        expect(chooseFileButton.length).toEqual(1);
-        expect(chooseFileButton.text()).toBe('Choose File');
+    it('should contain a hidden file input', () => {
+        const hiddenInput = wrapper.find('input[type="file"]');
+        expect(hiddenInput.length).toEqual(1);
     });
 })
