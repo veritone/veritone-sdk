43940329bdc80a23de1ae376e0215c6ac4faebd8
Mitch Robb <mrobb@veritone.com>
2017-11-08 21:33:17 -0800

    no need to pass meta redux-form props to input

--- packages/veritone-react-common/src/components/formComponents/Input.js
+++ packages/veritone-react-common/src/components/formComponents/Input.js
@@ -4,7 +4,7 @@ import LibInput from 'material-ui/Input';
 // fixme: not sure why this passthrough is needed?
 /* eslint-disable react/prop-types */
 const Input = ({ meta, input, ...props }) => {
-  return <LibInput {...meta} {...input} {...props} />;
+  return <LibInput {...input} {...props} />;
 };
 
 export default Input;
