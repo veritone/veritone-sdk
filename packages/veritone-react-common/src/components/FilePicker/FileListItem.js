a0bc90cf3d034f4dc31d1c60701258d090dca415
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-08 18:32:58 -0800

    This should be 1000 so that it better represents what the user sees in their file selector.

--- packages/veritone-react-common/src/components/FilePicker/FileListItem.js
+++ packages/veritone-react-common/src/components/FilePicker/FileListItem.js
@@ -14,7 +14,7 @@ class FileListItem extends Component {
     // taken from https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
     formatBytes = (bytes) => {
         if(bytes == 0) return '0 Bytes';
-        var k = 1024,
+        var k = 1000,
             sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
             i = Math.floor(Math.log(bytes) / Math.log(k));
         return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
