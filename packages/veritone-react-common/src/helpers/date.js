4cbfc4f25e85ee4368f6467b5d62f48b1709ac42
Mitch Robb <mrobb@veritone.com>
2017-11-09 12:28:24 -0800

    Interval class + tests, needed for date pickers

---
+++ packages/veritone-react-common/src/helpers/date.js
@@ -0,0 +1,176 @@
+import { isArray, isMatch, isDate } from 'lodash';
+import {
+  subHours,
+  subDays,
+  subMonths,
+  format as libFormat,
+  isValid
+} from 'date-fns';
+
+export class Interval {
+  constructor({ label, start, end, window } = {}) {
+    this.label = label;
+    this._start = start;
+    this._end = end;
+    this._window = window;
+
+    this.validate();
+  }
+
+  get start() {
+    if (this._window) {
+      return Interval._translateBucket(this._window).start;
+    }
+
+    return this._start;
+  }
+
+  get end() {
+    if (this._window) {
+      return Interval._translateBucket(this._window).end;
+    }
+
+    return this._end;
+  }
+
+  set start(val) {
+    this._start = val;
+    this.validate();
+  }
+
+  set end(val) {
+    this._end = val;
+    this.validate();
+  }
+
+  isStatic() {
+    return !this._window;
+  }
+
+  isSliding() {
+    return !this.isStatic();
+  }
+
+  isEqual(interval) {
+    if (this.isSliding()) {
+      return isMatch(interval, {
+        _window: this._window
+      });
+    }
+
+    return isMatch(interval, {
+      _start: this._start,
+      _end: this._end
+    });
+  }
+
+  toJSON() {
+    return {
+      start: this.start,
+      end: this.end,
+      _start: this._start,
+      _end: this._end,
+      _window: this._window
+    };
+  }
+
+  toString() {
+    const dateFormat = dateFormats.short;
+
+    return this.label
+      ? this.label
+      : `${format(this.start, dateFormat)} – ${format(this.end, dateFormat)}`;
+  }
+
+  validate() {
+    if (this._window) {
+      if (!isArray(this._window)) {
+        throw new Error('Expected window to be an array');
+      }
+
+      if (this._window.length !== 2) {
+        throw new Error('Expected window array to have two elements');
+      }
+    }
+
+    if (!this._window) {
+      if (!(this._start && this._end)) {
+        throw new Error(
+          'Expected interval to either have _window or _start & _end keys'
+        );
+      }
+
+      if (!isDate(this._start)) {
+        throw new Error('Expected _start to be an instance of Date');
+      }
+
+      if (!isDate(this._end)) {
+        throw new Error('Expected _end to be an instance of Date');
+      }
+    }
+  }
+
+  static _translateBucket(window) {
+    const subFn = {
+      h: subHours,
+      d: subDays,
+      m: subMonths
+    }[window[1]];
+
+    if (!subFn) {
+      throw new Error('Bucket had unexpected unit:', window[1]);
+    }
+
+    return {
+      start: subFn(new Date(), window[0]),
+      end: new Date()
+    };
+  }
+
+  static fromJSON(jsonInterval) {
+    const interval = JSON.parse(jsonInterval);
+
+    return new Interval({
+      window: interval._window,
+      start: interval._start ? new Date(interval._start) : undefined,
+      end: interval._end ? new Date(interval._end) : undefined
+    });
+  }
+}
+
+export const defaultIntervals = {
+  day: new Interval({
+    label: 'Last day',
+    window: [1, 'd']
+  }),
+  week: new Interval({
+    label: 'Last 7 days',
+    window: [7, 'd']
+  }),
+  month: new Interval({
+    label: 'Last 30 days',
+    window: [30, 'd']
+  }),
+
+  quarter: new Interval({
+    label: 'Last 90 days',
+    window: [90, 'd']
+  }),
+  year: new Interval({
+    label: 'Last 365 days',
+    window: [365, 'd']
+  })
+};
+
+export const dateFormats = {
+  full: 'MMM Do, YYYY hh:mmA',
+  short: 'MM/DD/YYYY'
+};
+
+export function format(date, ...args) {
+  if (!date) {
+    return '';
+  }
+
+  return isValid(new Date(date)) ? libFormat(date, ...args) : '';
+}
---
+++ packages/veritone-react-common/src/helpers/date.test.js
@@ -0,0 +1,361 @@
+import sinon from 'sinon';
+import { subDays, subHours, subMonths } from 'date-fns';
+
+import { Interval, format } from './date';
+
+describe('date interval helpers', function() {
+  describe('Interval class', function() {
+    let clock;
+
+    beforeEach(function() {
+      // ensure date tests are stable by stopping time
+      clock = sinon.useFakeTimers(new Date('Apr 01 2017'));
+    });
+
+    afterEach(function() {
+      clock.restore();
+    });
+
+    it('throws an error if a malformed interval is passed in', function() {
+      let assertions = 6;
+
+      try {
+        new Interval({ start: new Date() });
+      } catch (e) {
+        assertions--;
+      }
+
+      try {
+        new Interval({ end: new Date() });
+      } catch (e) {
+        assertions--;
+      }
+
+      try {
+        new Interval({ window: new Date() });
+      } catch (e) {
+        assertions--;
+      }
+
+      try {
+        new Interval({ _start: new Date(), _end: 5 });
+      } catch (e) {
+        assertions--;
+      }
+
+      try {
+        new Interval({ _start: 5, _end: new Date() });
+      } catch (e) {
+        assertions--;
+      }
+
+      new Interval({ window: [24, 'h'], start: new Date() });
+      assertions--;
+
+      expect(assertions).toEqual(0);
+    });
+
+    it('translates a window to a concrete start/end', function() {
+      const hourRange = new Interval({ window: [12, 'h'] });
+      const dayRange = new Interval({ window: [2, 'd'] });
+      const monthRange = new Interval({ window: [2, 'm'] });
+
+      expect(hourRange.start).toEqual(subHours(new Date(), 12));
+      expect(hourRange.end).toEqual(new Date());
+
+      expect(dayRange.start).toEqual(subDays(new Date(), 2));
+      expect(dayRange.end).toEqual(new Date());
+
+      expect(monthRange.start).toEqual(subMonths(new Date(), 2));
+      expect(monthRange.end).toEqual(new Date());
+    });
+
+    it('returns the same interval given a fixed interval', function() {
+      const start = subDays(new Date(), 2);
+      const end = subDays(new Date(), 1);
+
+      const interval = new Interval({ start, end });
+
+      expect(interval.start).toEqual(start);
+      expect(interval.end).toEqual(end);
+    });
+
+    it('prioritizes the window if both window and start/end exist in a interval', function() {
+      const interval = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date(),
+        window: [12, 'h']
+      });
+
+      expect(interval.start).toEqual(subHours(new Date(), 12));
+      expect(interval.end).toEqual(new Date());
+    });
+
+    it('returns whether the interval will be treated as static/sliding', function() {
+      const intervalBoth = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date(),
+        window: [12, 'h']
+      });
+
+      expect(intervalBoth.isStatic()).toEqual(false);
+      expect(intervalBoth.isSliding()).toEqual(true);
+
+      const intervalWindow = new Interval({
+        window: [12, 'h']
+      });
+
+      expect(intervalWindow.isStatic()).toEqual(false);
+      expect(intervalWindow.isSliding()).toEqual(true);
+
+      const intervalStatic = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date()
+      });
+
+      expect(intervalStatic.isStatic()).toEqual(true);
+      expect(intervalStatic.isSliding()).toEqual(false);
+    });
+
+    it('tests equality across instances with isEqual', function() {
+      expect(
+        new Interval({
+          start: subDays(new Date(), 2),
+          end: new Date(),
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 2),
+            end: new Date(),
+            window: [12, 'h']
+          })
+        )
+      ).toEqual(true);
+
+      expect(
+        new Interval({
+          start: subDays(new Date(), 2),
+          end: new Date()
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 2),
+            end: new Date()
+          })
+        )
+      ).toEqual(true);
+
+      expect(
+        new Interval({
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            window: [12, 'h']
+          })
+        )
+      ).toEqual(true);
+
+      expect(
+        new Interval({
+          start: subDays(new Date(), 2),
+          end: new Date(),
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 2),
+            end: new Date(),
+            window: [13, 'h']
+          })
+        )
+      ).toEqual(false);
+
+      expect(
+        new Interval({
+          start: subDays(new Date(), 2),
+          end: new Date(),
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 3),
+            end: new Date(),
+            window: [12, 'h']
+          })
+        )
+      ).toEqual(true); // if window is present, ignore start/end
+
+      expect(
+        new Interval({
+          start: subDays(new Date(), 2),
+          end: new Date()
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 3),
+            end: new Date()
+          })
+        )
+      ).toEqual(false);
+
+      expect(
+        new Interval({
+          start: subDays(new Date(), 3),
+          end: new Date()
+        }).isEqual(
+          new Interval({
+            start: subDays(new Date(), 3),
+            end: subDays(new Date(), 2)
+          })
+        )
+      ).toEqual(false);
+
+      expect(
+        new Interval({
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            window: [13, 'h']
+          })
+        )
+      ).toEqual(false);
+
+      expect(
+        new Interval({
+          label: 'last 12 hours',
+          window: [12, 'h']
+        }).isEqual(
+          new Interval({
+            label: 'previous 12 hours',
+            window: [12, 'h']
+          })
+        )
+      ).toEqual(true); // ignores label
+    });
+
+    it('JSON stringifies to start/end, also serializing private properties', function() {
+      const windowInterval = new Interval({
+        window: [12, 'h']
+      });
+      const staticInterval = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date()
+      });
+
+      expect(JSON.stringify(windowInterval)).toEqual(
+        JSON.stringify({
+          start: subHours(new Date(), 12),
+          end: new Date(),
+          _window: [12, 'h']
+        })
+      );
+
+      expect(JSON.stringify(staticInterval)).toEqual(
+        JSON.stringify({
+          start: subDays(new Date(), 2),
+          end: new Date(),
+          _start: subDays(new Date(), 2),
+          _end: new Date()
+        })
+      );
+    });
+
+    it('parses back from JSON', function() {
+      const windowInterval = new Interval({
+        window: [12, 'h']
+      });
+      const staticInterval = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date()
+      });
+
+      expect(
+        Interval.fromJSON(JSON.stringify(windowInterval)).isEqual(
+          windowInterval
+        )
+      ).toEqual(true);
+
+      expect(
+        Interval.fromJSON(JSON.stringify(staticInterval)).isEqual(
+          staticInterval
+        )
+      ).toEqual(true);
+    });
+
+    it('sets/gets a label', function() {
+      const interval = new Interval({
+        label: 'My Interval',
+        window: [12, 'h']
+      });
+
+      expect(interval.label).toEqual('My Interval');
+    });
+
+    it('formats the interval as a human-readable string', function() {
+      const labelInterval = new Interval({
+        label: 'My Interval',
+        window: [12, 'h']
+      });
+
+      expect(labelInterval.toString()).toEqual('My Interval');
+
+      const windowInterval = new Interval({
+        window: [36, 'h']
+      });
+
+      expect(windowInterval.toString()).toEqual('03/30/2017 – 04/01/2017');
+
+      const staticInterval = new Interval({
+        start: subDays(new Date(), 3),
+        end: new Date()
+      });
+
+      expect(staticInterval.toString()).toEqual('03/29/2017 – 04/01/2017');
+    });
+
+    it('allows setting start/end', function() {
+      let staticInterval = new Interval({
+        start: subDays(new Date(), 2),
+        end: new Date()
+      });
+
+      staticInterval.start = subDays(new Date(), 3);
+      staticInterval.end = subDays(new Date(), 1);
+
+      expect(
+        staticInterval.isEqual(
+          new Interval({
+            start: subDays(new Date(), 3),
+            end: subDays(new Date(), 1)
+          })
+        )
+      ).toEqual(true);
+
+      let validated = 0;
+      try {
+        staticInterval.start = 'asdf';
+      } catch (e) {
+        validated++;
+      }
+
+      try {
+        staticInterval.end = 'asdf';
+      } catch (e) {
+        validated++;
+      }
+
+      expect(validated).toEqual(2, 'validates when setting start/end');
+    });
+  });
+
+  describe('format', function() {
+    it('returns empty string if date is null or undefined', function() {
+      expect(format(null)).toEqual('');
+      expect(format(undefined)).toEqual('');
+    });
+
+    it('returns empty string if date is invalid', function() {
+      expect(format('invalid')).toEqual('');
+    });
+
+    it('returns formatted string for valid dates', function() {
+      expect(format('jan 1, 2018', 'MM/DD/YYYY')).toEqual('01/01/2018');
+    });
+  });
+});
