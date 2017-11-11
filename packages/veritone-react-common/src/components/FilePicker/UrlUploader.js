23ce7ad02b92fcd6ed6729176ceb3643e33224a4
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-11 15:54:22 -0800

    Added some styling and functionality to UrlUpload component.

--- packages/veritone-react-common/src/components/FilePicker/UrlUploader.js
+++ packages/veritone-react-common/src/components/FilePicker/UrlUploader.js
@@ -3,7 +3,7 @@ import { FormControl } from 'material-ui/Form';
 import Input, { InputLabel } from 'material-ui/Input';
 
 import {
-    string
+    func
   } from 'prop-types';
 
 import withMuiThemeProvider from 'helpers/withMuiThemeProvider';
@@ -12,13 +12,42 @@ import styles from './styles.scss';
 
 @withMuiThemeProvider
 class UrlUploader extends Component {
+    state = {
+        image: "",
+        fetchingImage: false
+    }
+
     preventInput = (evt) => {
         evt.preventDefault();
     }
 
     handlePaste = (evt) => {
-        console.log(evt);
-        console.log(evt.target);
+        this.setState({
+            image: "",
+            fetchingImage: true
+        });
+        fetch(evt.clipboardData.getData('Text'))
+        .then((response) => {
+            if (response.status === 200 || response.status === 0) {
+                return response.blob();
+            } else {
+                return Promise.reject(new Error('Error loading: ' + evt.clipboardData.getData('Text')))
+            }
+        })
+        .then((responseBlob) => {
+            let fileReader = new FileReader();
+            fileReader.onload = () => {
+                this.setState({
+                    image: fileReader.result,
+                    fetchingImage: false
+                });
+            };
+            fileReader.readAsDataURL(responseBlob);
+        }, (error) => {
+            this.setState({
+                fetchingImage: false
+            });
+        });
     }
 
     render () {
@@ -39,16 +68,24 @@ class UrlUploader extends Component {
                             inkbar: styles.fileUrlInputInkbar,
                         }}
                         id="url-input"
-                        onKeyPress={this.preventInput}
-                        onPaste={this.handlePaste}
+                        onKeyPress={this.preventInput.bind(this)}
+                        onPaste={this.handlePaste.bind(this)}
                     />
                 </FormControl>
-                <div className={styles.urlUploaderInfoBox}>
-                    <span className={styles.correctUrlText}>If the URL is correct the image will
-                    display here.</span>
-                    <span className={styles.confirmLicenseText}>Remember, only use images that you 
-                    have confirmed that you have the license to use</span>
-                </div>
+                {
+                    this.state.image.length || this.state.fetchingImage ? 
+                        <div className={styles.imageContainer}>
+                            <div className={styles.fileImage}>
+                                <img src={this.state.image}></img>
+                            </div>
+                        </div> :
+                        <div className={styles.urlUploaderInfoBox}>
+                            <span className={styles.correctUrlText}>If the URL is correct the image will
+                            display here.</span>
+                            <span className={styles.confirmLicenseText}>Remember, only use images that you 
+                            have confirmed that you have the license to use</span>
+                        </div>
+                }
             </div>
         );
     }
