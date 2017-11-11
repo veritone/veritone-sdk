79c8201d3e4a1db4a05a502efad243bbd8b08659
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-10 17:42:31 -0800

    Added a FilePickerHeader component just to minimize component size the index.js was getting bloated

---
+++ packages/veritone-react-common/src/components/FilePicker/FilePickerHeader/FilePickerHeader.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import IconButton from 'material-ui/IconButton';
+import withMuiThemeProvider from 'helpers/withMuiThemeProvider';
+import styles from './styles.scss';
+import _ from 'lodash';
+
+import {
+    string,
+    func
+  } from 'prop-types';
+
+
+@withMuiThemeProvider
+class FilePickerHeader extends Component {
+    handleTabChange = (event, value) => {
+        this.props.onSelectTab(value);
+    }
+
+    render () {
+        return (
+            <div className={styles.filePickerHeader}>
+                <span className={styles.filePickerTitle}>File Picker</span>
+                <IconButton style={{
+                        height:'28px', 
+                        width: '28px',
+                        position: 'fixed',
+                        top: 15,
+                        right: 15
+                    }}>
+                    <i className='icon-close-exit'></i>
+                </IconButton>
+                <Tabs value={this.props.selectedTab}
+                    indicatorColor="primary"
+                    onChange={this.handleTabChange} 
+                    className={styles.filePickerTabs}>
+                    <Tab label="Upload" value="upload"></Tab>
+                    <Tab label="By URL" value="by-url"></Tab>
+                </Tabs>
+            </div>
+        );
+    }
+};
+
+FilePickerHeader.propTypes = {
+    selectedTab: string,
+    onSelectTab: func
+}
+
+export default FilePickerHeader;
