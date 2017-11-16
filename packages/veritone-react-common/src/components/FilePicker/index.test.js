9df2dae665dc55d5966274cc4996e5f740b09922
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-15 22:56:40 -0800

    Added some tests for the FilePickerFooter and FilePickerHeader.

--- packages/veritone-react-common/src/components/FilePicker/index.test.js
+++ packages/veritone-react-common/src/components/FilePicker/index.test.js
@@ -1,64 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { mount } from 'enzyme';
-import { Tab } from 'material-ui/Tabs';
+
+import Tabs, { Tab } from 'material-ui/Tabs';
 import Button from 'material-ui/Button';
 
 import FilePicker from './';
 import FileUploader from './FileUploader';
+import FilePickerHeader from './FilePickerHeader/FilePickerHeader';
+import FilePickerFooter from './FilePickerFooter/FilePickerFooter';
 
-describe('FilePicker', () => {
-    let wrapper;
+
+describe('FilePickerHeader', () => {
+    let wrapper, onSelectTab, onCloseModal;
     beforeEach(() => {
-        wrapper = mount(<FilePicker />);
+        onSelectTab = jest.fn();
+        onCloseModal = jest.fn();
+        wrapper = mount(<FilePickerHeader selectedTab="upload" 
+                                          onSelectTab={onSelectTab}
+                                          onCloseModal={onCloseModal}/>);
+    })
+
+    it('should have a title of "File Picker"', () => {
+        const filePickerTitle = wrapper.find('.filePickerTitle');
+        expect(filePickerTitle.exists()).toEqual(true);
+        expect(filePickerTitle.text() === "File Picker");
     });
 
-    it('has tab bar with two tabs', () => {
-        expect(wrapper.find(Tab).length).toEqual(2);
+    it('should have a Tabs bar with two tabs', () => {
+        const tabsBar = wrapper.find(Tabs);
+        expect(tabsBar.exists);
+        expect(tabsBar.find(Tab)).toHaveLength(2);
     });
 
-    describe('when "Upload" tab is clicked', () => {
-        let uploadTab;
-        beforeEach(() => {
-            uploadTab = wrapper.find('Tab[label="Upload"]');
-        })
-        it('should display a FileUploader component', () => {
-            uploadTab.simulate('click');
-            expect(wrapper.find(FileUploader).length).toEqual(1);
-        });
+    it('onSelectTab should be called when a tab is clicked', () => {
+        const urlUploadTab = wrapper.find(Tab).at(1);
+        urlUploadTab.simulate('click');
+        expect(onSelectTab).toHaveBeenCalled();
     });
 
-    describe('when "By Url" tab is clicked', () => {
-        let byUrlTab;
-        beforeEach(() => {
-            byUrlTab = wrapper.find('Tab[label="By URL"]');
-        })
-        it('should hide the FileUploader component', () => {
-            byUrlTab.simulate('click');
-            expect(wrapper.find(FileUploader).length).toEqual(0);
-        });
+    it('should have a close "x" button', () => {
+        const xButton = wrapper.find(".icon-close-exit");
+        expect(xButton.exists()).toEqual(true);
     });
-});
 
-describe('FileUploader', () => {
-    let wrapper;
-    const sampleFiles = {
-        0: {
-            
-        }
-    };
+    it('onCloseModal should be called when the close button is clicked', () => {
+        const xButton = wrapper.find(".icon-close-exit");
+        xButton.simulate('click');
+        expect(onCloseModal).toHaveBeenCalled();
+    });
+})
 
+describe('FilePickerFooter', () => {
+    let wrapper, onCloseModal;
     beforeEach(() => {
-        wrapper = mount(<FileUploader />);
-    });
+        onCloseModal = jest.fn();
+        wrapper = mount(<FilePickerFooter />);
+    })
 
-    it('should contain a "CHOOSE FILE" button when a file has not been selected', () => {
-        const chooseFileButton = wrapper.find(Button);
-        expect(chooseFileButton.length).toEqual(1);
-        expect(chooseFileButton.text()).toMatch(/choose file/i);
-    });
-
-    it('should contain a hidden file input', () => {
-        const hiddenInput = wrapper.find('input[type="file"]');
-        expect(hiddenInput.length).toEqual(1);
-    });
+    it('should have an "Upload" button', () => {
+        const uploadButton = wrapper.find(Button);
+    })
 })
