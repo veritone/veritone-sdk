8deb34fcb9323f65fb4cc0fcd54a91a96d86c7e1
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-18 09:37:38 -0800

    Moved the FileUploader to its own directory forgot to include htsi in the last commit.

--- packages/veritone-react-common/src/components/FilePicker/index.test.js
+++ packages/veritone-react-common/src/components/FilePicker/index.test.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import Dialog from 'material-ui/Dialog';
 
 import FilePicker from './';
-import FileUploader from './FileUploader';
+import FileUploader from './FileUploader/FileUploader';
 
 describe('FilePicker', () => {
     it('should render', () => {
         // TODO: implement file picker test.
+        // I believe the Dialog library uses react portals which aren't supported by
+        // enzyme and the momment according to this
+        // https://github.com/airbnb/enzyme/blob/master/docs/common-issues.md
+        // will look into other ways of testing.
     })
 })
