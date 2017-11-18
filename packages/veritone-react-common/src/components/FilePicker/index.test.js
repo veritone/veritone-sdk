dc35df774990583dc7a006b39f2818176150f5ef
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-11-18 14:28:04 -0800

    Added some rough tests for the FilePicker but not sure how good they are.

--- packages/veritone-react-common/src/components/FilePicker/index.test.js
+++ packages/veritone-react-common/src/components/FilePicker/index.test.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import  ReactTestUtils from 'react-dom/test-utils';
 import { shallow, mount } from 'enzyme';
 import Dialog from 'material-ui/Dialog';
 
 import FilePicker from './';
 import FileUploader from './FileUploader/FileUploader';
 
+
+// I believe the Dialog library uses react portals which aren't supported by
+// enzyme and the momment according to this
+// https://github.com/airbnb/enzyme/blob/master/docs/common-issues.md
+// will look into other ways of testing.
+// Not sure if this is the best way to test this but it is better than nothing
+// I suppose.
 describe('FilePicker', () => {
-    it('should render', () => {
-        // TODO: implement file picker test.
-        // I believe the Dialog library uses react portals which aren't supported by
-        // enzyme and the momment according to this
-        // https://github.com/airbnb/enzyme/blob/master/docs/common-issues.md
-        // will look into other ways of testing.
-    })
+    let onClose = jest.fn();
+    let onUploadFiles = jest.fn();
+    let filePickerComponent =  ReactTestUtils.renderIntoDocument(
+        <FilePicker isOpen={true} 
+                    onUploadFiles={onUploadFiles} 
+                    onCloseModal={onClose}/>
+    );
+
+    it('should have a header', () => {
+        ReactTestUtils.findRenderedComponentWithType(filePickerComponent, FilePicker);
+        
+        var filePickerHeader = document.body.getElementsByClassName('filePickerHeader');
+        expect(filePickerHeader.length).toEqual(1);
+    });
+
+    it('should have a footer', () => {
+        ReactTestUtils.findRenderedComponentWithType(filePickerComponent, FilePicker);
+        
+        var filePickerFooter = document.body.getElementsByClassName('filePickerFooter');
+        expect(filePickerFooter.length).toEqual(1);
+    });
+
+    it('should have a body', () => {
+        ReactTestUtils.findRenderedComponentWithType(filePickerComponent, FilePicker);
+        
+        var filePickerBody = document.body.getElementsByClassName('filePickerBody');
+        expect(filePickerBody.length).toEqual(1);
+    });
 })
