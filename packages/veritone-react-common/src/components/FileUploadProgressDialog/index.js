63e19ecd2dc005a0523a260fb16c87ee55192871
Mitch Robb <mrobb@veritone.com>
2017-11-30 23:32:14 -0800

    initial commit for FileUploadProgressDialog

---
+++ packages/veritone-react-common/src/components/FileUploadProgressDialog/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { number, string } from 'prop-types';
+import Paper from 'material-ui/Paper';
+import { CircularProgress } from 'material-ui/Progress';
+
+import withMuiThemeProvider from 'helpers/withMuiThemeProvider';
+import styles from './styles.scss';
+
+@withMuiThemeProvider
+export default class FileUploadProgressDialog extends React.Component {
+  static propTypes = {
+    percentComplete: number,
+    progressMessage: string,
+    height: number,
+    width: number
+  };
+
+  static defaultProps = {
+    percentComplete: 0,
+    progressMessage: '',
+    height: 400,
+    width: 600
+  };
+
+  render() {
+    return (
+      <Paper
+        classes={{ root: styles.container }}
+        style={{ height: this.props.height, width: this.props.width }}
+      >
+        <div className={styles.progressPercentageContainer}>
+          <div className={styles.percentage}>{this.props.percentComplete}%</div>
+          <CircularProgress
+            className={styles.progress}
+            size={125}
+            thickness={1}
+          />
+        </div>
+        <p className={styles.progressMessage}>{this.props.progressMessage}</p>
+      </Paper>
+    );
+  }
+}
