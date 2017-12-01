260ae9b18796aba0fc588393cb6bf6248d54fb34
Mitch Robb <mrobb@veritone.com>
2017-12-01 12:25:44 -0800

    add ProgressDialog

--- packages/veritone-react-common/src/components/ProgressDialog/story.js
+++ packages/veritone-react-common/src/components/ProgressDialog/story.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { number, boolean, text } from '@storybook/addon-knobs';
 
 import ProgressDialog from './';
 
-storiesOf('ProgressDialog', module)
-  .add('Base', () => (
-    <ProgressDialog
-      percentComplete={20}
-      progressMessage="getting urls"
-    />
-  ));
+storiesOf('ProgressDialog', module).add('Base', () => (
+  <ProgressDialog
+    percentComplete={number('percentComplete', 20)}
+    progressMessage={text('progressMessage', 'retrieving signed URLs')}
+    doneSuccess={boolean('doneSuccess', false)}
+    doneFailure={boolean('doneFailure', false)}
+  />
+));
