a2ab3758143648ec461d2ef02a5edc1a8f11f000
Mitch Robb <mrobb@veritone.com>
2017-11-30 22:28:38 -0800

    do not wrap filepicker in a dialog by default. move tests to enzyme now that portals aren't involved.

--- packages/veritone-react-common/src/components/FilePicker/index.test.js
+++ packages/veritone-react-common/src/components/FilePicker/index.test.js
@@ -1,53 +1,23 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
+import { mount } from 'enzyme';
 import FilePicker from './';
 
-// I believe the Dialog library uses react portals which aren't supported by
-// enzyme and the momment according to this
-// https://github.com/airbnb/enzyme/blob/master/docs/common-issues.md
-// will look into other ways of testing.
-// Not sure if this is the best way to test this but it is better than nothing
-// I suppose.
-
-// fixme-- dialog as outer component
 describe('FilePicker', () => {
   let onClose = jest.fn();
   let onUploadFiles = jest.fn();
-  let filePickerComponent = ReactTestUtils.renderIntoDocument(
-    <FilePicker isOpen onUploadFiles={onUploadFiles} onCloseModal={onClose} />
+  let filePickerComponent = mount(
+    <FilePicker onUploadFiles={onUploadFiles} onRequestClose={onClose} />
   );
 
   it('should have a header', () => {
-    ReactTestUtils.findRenderedComponentWithType(
-      filePickerComponent,
-      FilePicker
-    );
-
-    let filePickerHeader = document.body.getElementsByClassName(
-      'filePickerHeader'
-    );
-    expect(filePickerHeader.length).toEqual(1);
+    expect(filePickerComponent.find('FilePickerHeader')).toHaveLength(1);
   });
 
   it('should have a footer', () => {
-    ReactTestUtils.findRenderedComponentWithType(
-      filePickerComponent,
-      FilePicker
-    );
-
-    let filePickerFooter = document.body.getElementsByClassName(
-      'filePickerFooter'
-    );
-    expect(filePickerFooter.length).toEqual(1);
+    expect(filePickerComponent.find('FilePickerFooter')).toHaveLength(1);
   });
 
   it('should have a body', () => {
-    ReactTestUtils.findRenderedComponentWithType(
-      filePickerComponent,
-      FilePicker
-    );
-
-    let filePickerBody = document.body.getElementsByClassName('filePickerBody');
-    expect(filePickerBody.length).toEqual(1);
+    expect(filePickerComponent.find('.filePickerBody')).toHaveLength(1);
   });
 });
