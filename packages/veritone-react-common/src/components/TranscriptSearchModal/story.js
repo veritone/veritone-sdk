bb512eb0672466d7d2a7e76874bad4e73557ef2e
Andrew Tung <atung@veritone.com>
2017-12-07 17:53:07 -0800

    Better storybook knobs and functions.

--- packages/veritone-react-common/src/components/TranscriptSearchModal/story.js
+++ packages/veritone-react-common/src/components/TranscriptSearchModal/story.js
@@ -3,20 +3,20 @@ import { storiesOf } from '@storybook/react';
 import { TranscriptSearchModal } from './';
 import { TranscriptSearchForm } from './';
 
-storiesOf('TranscriptSearchModal', module).add('Base', () => {
-  const logFilter = value => console.log('filter value', value);
-  const cancel = () => console.log("cancel prssed");
+import { boolean, object } from '@storybook/addon-knobs';
 
+storiesOf('TranscriptSearchModal', module).add('withOpenDialogAndDefaultValue', () => {
+  const logFilter = value => console.log('filter value', value);
+  const cancel = () => console.log("cancel pressed");
   return (
     <TranscriptSearchModal
-      open
-      modalState={{ "value": 'Lakers' }}
+      open={boolean("Open", true)}
+      modalState={ object( "Search condition state", { "value": ('Lakers') } ) }
       cancel={ cancel }
       applyFilter={logFilter}
     />
   );
-}).add( 'PureComponent', () => {
-  //const formValued = formValue("lakers");
+}).add( 'withoutDialog', () => {
   return (
     <TranscriptSearchForm
       defaultValue={"lakers"}
