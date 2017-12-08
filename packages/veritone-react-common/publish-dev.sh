c7f44c68c65479ba4e4c3384243c10e376012f86
Mitch Robb <olordhux@gmail.com>
2017-12-07 16:24:31 -0800

    Create publish-dev.sh

---
+++ packages/veritone-react-common/publish-dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+bucketname=dev-sdk-build-artifacts
+pwd=$pwd
+
+#yarn run build
+filename=react-common-$(date +%Y%m%d-%H%M%S).tar.gz
+tar czf ${filename} dist
+aws s3api put-object --bucket ${bucketname} --key ${filename} --body "`pwd`/${filename}"
+
+echo "Artifact stored at https://${bucketname}.s3.amazonaws.com/${filename}"
