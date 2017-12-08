4a5f478d990bef0cae76a9a0395649c1db06cf66
Mitch Robb <olordhux@gmail.com>
2017-12-07 16:28:37 -0800

    remove artifact after putting to s3

--- packages/veritone-react-common/publish-dev.sh
+++ packages/veritone-react-common/publish-dev.sh
@@ -2,9 +2,9 @@
 bucketname=dev-sdk-build-artifacts
 pwd=$pwd
 
-#yarn run build
+yarn run build
 filename=react-common-$(date +%Y%m%d-%H%M%S).tar.gz
 tar czf ${filename} dist
 aws s3api put-object --bucket ${bucketname} --key ${filename} --body "`pwd`/${filename}"
-
+rm ${filename}
 echo "Artifact stored at https://${bucketname}.s3.amazonaws.com/${filename}"
