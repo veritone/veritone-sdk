62adf191951949d1db6218ac6cd426624a63e5e0
Mitch Robb <mrobb@veritone.com>
2017-12-13 14:58:49 -0800

    Add FilePicker widget to bundle

--- packages/veritone-widgets/src/build-entry.js
+++ packages/veritone-widgets/src/build-entry.js
@@ -1,3 +1,4 @@
 export VeritoneApp from './shared/VeritoneApp';
 export AppBar from './widgets/AppBar';
 export OAuthLoginButton from './widgets/OAuthLoginButton';
+export FilePicker from './widgets/FilePicker';
