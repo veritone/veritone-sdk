43d0038b1f1c7145463d6d89d58773135ee340f4
Mitch Robb <mrobb@veritone.com>
2018-01-18 16:49:52 -0800

    make sure AppBar widget gets the user and applications from the store

--- packages/veritone-widgets/src/widgets/AppBar/index.js
+++ packages/veritone-widgets/src/widgets/AppBar/index.js
@@ -1,4 +1,19 @@
+import { connect } from 'react-redux';
 import { AppBar } from 'veritone-react-common';
+import { modules } from 'veritone-redux-common'
+const { user } = modules;
 import widget from '../../shared/widget';
 
-export default widget(AppBar);
+const connectedAppBar = connect(
+  state => ({
+    user: user.selectUser(state),
+    enabledApps: user.selectEnabledApps(state),
+    enabledAppsFailedLoading: user.enabledAppsFailedLoading(state),
+    isFetchingApps: user.isFetchingApps(state)
+  }),
+  { fetchEnabledApps: user.fetchEnabledApps },
+  null,
+  { withRef: true }
+)(AppBar);
+
+export default widget(connectedAppBar);
