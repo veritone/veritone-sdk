8a42f1e76f9e02dedd4bc90244b52d230bfb3f30
Mitch Robb <mrobb@veritone.com>
2018-02-15 15:43:20 -0800

    simplify createStore stuff in veritone-widgets

--- packages/veritone-widgets/src/redux/configureStore.js
+++ packages/veritone-widgets/src/redux/configureStore.js
@@ -1,5 +1,38 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.production').default;
-} else {
-  module.exports = require('./configureStore.development').default;
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import {
+  getBaseMiddlewares,
+  getDevOnlyMiddlewares
+} from './store';
+import createRootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+// Redux devtools browser extension hook
+// this has no effect unless the user has the chrome devtools extension installed.
+// https://medium.com/@zalmoxis/using-redux-devtools-in-production-4c5b56c5600f
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancer = composeEnhancers(
+  applyMiddleware(
+    ...getBaseMiddlewares(),
+    ...getDevOnlyMiddlewares(),
+    sagaMiddleware
+  )
+);
+
+export default function configureStore(initialState) {
+  const store = {
+    ...createStore(createRootReducer(), initialState, enhancer),
+    asyncReducers: {},
+    runSaga: sagaMiddleware.run,
+    sagaMiddleware
+    // close: () => store.dispatch(END);
+  };
+
+  store.runSaga(rootSaga);
+
+  return store;
 }
--- packages/veritone-widgets/src/redux/store.js
+++ packages/veritone-widgets/src/redux/store.js
@@ -3,28 +3,7 @@ import { apiMiddleware } from 'redux-api-middleware-fixed';
 export const getBaseMiddlewares = () => [apiMiddleware(fetch)];
 
 export const getDevOnlyMiddlewares = () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return [require('redux-logger').createLogger({ collapsed: true })];
-  }
+  return process.env.NODE_ENV !== 'production'
+    ? [require('redux-logger').createLogger({ collapsed: true })]
+    : [];
 };
-
-export const getProductionOnlyMiddlewares = () => [];
-
-export const getBaseStoreEnhancers = () => [];
-
-export const getDevOnlyStoreEnhancers = () => {
-  if (process.env.NODE_ENV !== 'production') {
-    // By default we try to read the key from ?debug_session=<key> in the address bar
-    const getDebugSessionKey = function() {
-      const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-      return matches && matches.length ? matches[1] : null;
-    };
-
-    return [
-      // Lets you write ?debug_session=<key> in address bar to persist debug sessions
-      require('redux-devtools').persistState(getDebugSessionKey())
-    ];
-  }
-};
-
-export const getProductionOnlyStoreEnhancers = () => [];
