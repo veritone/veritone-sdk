c599bcdaaa7f5378371b85cd4317be4ef680b1dc
Mitch Robb <mrobb@veritone.com>
2018-02-22 12:14:18 -0800

    bump oauth helpers; polyfill fetch in tests:

--- packages/veritone-redux-common/src/helpers/test/redux.js
+++ packages/veritone-redux-common/src/helpers/test/redux.js
@@ -1,4 +1,5 @@
 import configureStore from 'redux-mock-store';
 import { apiMiddleware } from 'redux-api-middleware-fixed';
+import fetch from 'node-fetch';
 
 export const makeMockStore = () => configureStore([apiMiddleware(fetch)]);
