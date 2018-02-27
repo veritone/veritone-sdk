71cb70d5c076474b1d94d297f4ffd22935d2c510
Mitch Robb <mrobb@veritone.com>
2018-02-27 13:58:52 -0800

    build oauth-helpers with rollup; add CJS bundle

---
+++ packages/veritone-oauth-helpers/rollup.config.js
@@ -0,0 +1,26 @@
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/bundle-es.js',
+        format: 'es',
+        exports: 'named'
+      },
+      {
+        file: 'dist/bundle-cjs.js',
+        format: 'cjs'
+      }
+    ],
+    plugins: [
+      babel({
+        include: ['src/**/*.js'],
+        runtimeHelpers: true
+      })
+    ]
+  }
+];
