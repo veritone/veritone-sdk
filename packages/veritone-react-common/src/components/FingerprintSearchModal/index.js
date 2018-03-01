20ddea8a609844ce40783e08e2801ed0866cd782
Jordan Belford <jbelford@veritone.com>
2018-02-28 16:05:53 -0800

    fix fingerprint modal always selecting the first item

--- packages/veritone-react-common/src/components/FingerprintSearchModal/index.js
+++ packages/veritone-react-common/src/components/FingerprintSearchModal/index.js
@@ -32,7 +32,6 @@ const fingerprintConfig = {
 export default class FingerprintSearchModal extends React.Component {
   static defaultProps = {
     modalState: { queryResults: [], queryString: '', exclude: false },
-    applyFilter: value => console.log('Search fingerprints by entityId', value),
     cancel: () => console.log('You clicked cancel')
   };
 
@@ -62,7 +61,6 @@ export default class FingerprintSearchModal extends React.Component {
       exclude: bool
     }),
     fetchAutocomplete: func,
-    applyFilter: func,
     cancel: func
   };
 
@@ -139,29 +137,14 @@ export default class FingerprintSearchModal extends React.Component {
     }
   };
 
-  applyFilterIfValue = () => {
-    if (isArray(this.state.queryResults) && this.state.queryResults.length) {
-      let firstSection = this.state.queryResults[0];
-      if (isArray(firstSection.items) && firstSection.items.length) {
-        let filterToApply = firstSection.items[0];
-        this.props.applyFilter(filterToApply);
-        //this.props.cancel();
-      }
-    }
-  };
-
   returnValue() {
-    if (isArray(this.state.queryResults) && this.state.queryResults.length) {
-      let firstSection = this.state.queryResults[0];
-      if (isArray(firstSection.items) && firstSection.items.length) {
-        let filterToApply = firstSection.items[0];
-        return {
-          exclude: this.state.exclude,
-          ...filterToApply
-        };
-      }
-    } else {
+    if(!this.state.selectedResult) {
       return;
+    } else {
+      return {
+        exclude: this.state.exclude,
+        ...this.state.selectedResult
+      };
     }
   }
 
@@ -175,7 +158,6 @@ export default class FingerprintSearchModal extends React.Component {
     return (
       <FingerprintSearchForm
         cancel={ this.props.cancel }
-        applyFilter={ this.applyFilterIfValue }
         onChange={ this.onChange }
         modalState={ this.state }
         showAutocomplete={ this.state.showAutocomplete }
@@ -186,7 +168,7 @@ export default class FingerprintSearchModal extends React.Component {
   }
 }
 
-export const FingerprintSearchForm = ( { showAutocomplete, cancel, applyFilter, onChange, onKeyPress, modalState, selectResult, toggleExclude } ) => {
+export const FingerprintSearchForm = ( { showAutocomplete, cancel, onChange, onKeyPress, modalState, selectResult, toggleExclude } ) => {
   return (
     <Grid container spacing={8}>
       <Grid item style={{flex: '1'}}>
