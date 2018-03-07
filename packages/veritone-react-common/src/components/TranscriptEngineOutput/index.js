771923d56b1f6a10a09e9f771e058bdaaacd6b84
Chris Pelletier <cpelletier@veritone.com>
2018-03-07 08:45:35 -0800

    Added select input for engine selector. Not really functional at the moment it only has a single selection with a hard coded value.

--- packages/veritone-react-common/src/components/TranscriptEngineOutput/index.js
+++ packages/veritone-react-common/src/components/TranscriptEngineOutput/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { arrayOf, object } from 'prop-types';
+import classNames from 'classnames';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
 
 import TranscriptContent from './TranscriptContent';
 
@@ -7,13 +10,21 @@ import styles from './styles.scss';
 
 class TranscriptEngineOutput extends Component {
   static propTypes = {
-    assets: arrayOf(object)
+    assets: arrayOf(object),
+    classes: object
   };
+
   render() {
+    let { classes } = this.props;
     return (
-      <div className={styles.transcriptOutputView}>
-        <div className={styles.transcriptViewHeader}>
+      <div className={classNames(styles.transcriptOutputView, classes.root)}>
+        <div className={classNames(styles.transcriptViewHeader, classes.header)}>
           <div className={styles.headerTitle}>Transcription</div>
+          <div className={styles.transcriptActions}>
+            <Select value="temporal">
+              <MenuItem value="temporal">Temporal</MenuItem>
+            </Select>
+          </div>
         </div>
         <TranscriptContent assets={this.props.assets}/>
       </div>
