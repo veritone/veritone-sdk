a79d43b04886cbde43855ac0e123881fe7b1ea8a
Chris Pelletier <cpelletier@veritone.com>
2018-03-14 21:24:48 -0700

    onSnippetClicked shouldn't be required

--- packages/veritone-react-common/src/components/EngineOutputViews/TranscriptEngineOutput/Snippet/index.js
+++ packages/veritone-react-common/src/components/EngineOutputViews/TranscriptEngineOutput/Snippet/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { string, bool, shape, func } from 'prop-types';
 import classNames from 'classnames';
+import { isFunction } from 'lodash';
 
 import styles from './styles.scss';
 
@@ -11,17 +12,19 @@ class Snippet extends Component {
     }),
     boldText: bool,
     editModeEnabled: bool,
-    onSnippetClick: func.isRequired
+    onSnippetClick: func
   };
 
   handleSnippetClick = () => {
-    this.props.onSnippetClick(this.props.snippet);
+    if (isFunction(this.props.onSnippetClick)) {
+      this.props.onSnippetClick(this.props.snippet);
+    }
   }
 
   render() {
     let { snippet, boldText, editModeEnabled } = this.props;
     return (
-      <p 
+      <p
         className={classNames(styles.snippetText, boldText && styles.boldText)}
         onClick={this.handleSnippetClick}
       >{snippet.text} </p>
