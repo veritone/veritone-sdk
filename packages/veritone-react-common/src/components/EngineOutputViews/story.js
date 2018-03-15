4432076ff3f56aa1f78ba5da43f67aa19a8f5e77
Chris Pelletier <cpelletier@veritone.com>
2018-03-15 14:33:42 -0700

    Added a story wrapper to display the use of the engine selector edit radios.

--- packages/veritone-react-common/src/components/EngineOutputViews/story.js
+++ packages/veritone-react-common/src/components/EngineOutputViews/story.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import { fill } from 'lodash';
 
 import styles from './story.styles.scss';
 
@@ -17,11 +18,74 @@ import {
   objectDetectionAssets
 } from './story.data.js';
 
+class TranscriptionStory extends Component {
+  state = {
+    assets: transcriptAssets,
+    editMode: "bulk",
+    selectedEngineId: "67cd4dd0-2f75-445d-a6f0-2f297d6cd182",
+    engines: [
+      {
+        sourceEngineId: "67cd4dd0-2f75-445d-a6f0-2f297d6cd182",
+        sourceEngineName: "Temporal",
+      }
+    ]
+  };
+
+  handleEngineChange = (evt) => {
+    this.setState({
+      ...this.state,
+      selectedEngineId: evt.target.value
+    })
+  }
+  
+  handleSnippetEdit = (snippet, innerHtml, taskId) => {
+    this.setState({
+      ...this.state,
+      assets: this.state.assets.map((task) => {
+        if (task.taskId !== taskId) {
+          return task;
+        } else {
+          return {
+            ...task,
+            series: task.series.map((s) => {
+              if (s.start === snippet.start && s.end === snippet.end) {
+                return {
+                  ...s,
+                  text: innerHtml
+                }
+              } else {
+                return s
+              }
+            })
+          }
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <TranscriptEngineOutput 
+        assets={this.state.assets}
+        editModeEnabled={this.props.editModeEnabled}
+        editMode={this.state.editMode}
+        engines={this.state.engines}
+        selectedEngineId={this.state.selectedEngineId}
+        onEngineChange={this.handleEngineChange}
+        onSnippetClicked={this.props.onSnippetClicked}
+        onSnippetEdit={this.handleSnippetEdit}
+        classes={{ root: styles.outputViewRoot }}
+      />
+    );
+  }
+}
+
 storiesOf('EngineOutputViews', module)
   .add('TranscriptEngineOutput', () => {
-    return (<TranscriptEngineOutput 
+    return (<TranscriptionStory 
         assets={transcriptAssets}
         editModeEnabled={boolean('Edit Mode Enabled', false)}
+        editMode="bulk"
         onSnippetClicked={action('Snippet Clicked')}
         classes={{ root: styles.outputViewRoot }}
     />);
