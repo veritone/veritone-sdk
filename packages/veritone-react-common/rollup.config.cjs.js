904a19ed38c576a62422de33b8a80ee76f44eb42
Mitch Robb <mrobb@veritone.com>
2018-03-21 13:11:20 -0700

    update react-common CJS build to broader browser compatibility

---
+++ packages/veritone-react-common/rollup.config.cjs.js
@@ -0,0 +1,11 @@
+import baseConfig from './rollup.config.base';
+
+export default Object.assign({}, baseConfig, {
+  output: [
+    {
+      file: 'dist/bundle-cjs.js',
+      format:
+        'cjs'
+    }
+  ]
+});
