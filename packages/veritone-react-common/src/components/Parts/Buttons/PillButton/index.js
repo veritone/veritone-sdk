8095045dfc1c4c8f5fa9375d934ec93e3333b637
Khuong Truong <ktruong@veritone.com>
2018-03-22 11:23:15 -0700

    VTN-6758 add styling options to PillButton

--- packages/veritone-react-common/src/components/Parts/Buttons/PillButton/index.js
+++ packages/veritone-react-common/src/components/Parts/Buttons/PillButton/index.js
@@ -7,26 +7,47 @@ export default class PillButton extends Component {
 	static propTypes = {
 		label: PropTypes.string,
 		info: PropTypes.string,
+		style: PropTypes.object,
+		gapStyle: PropTypes.object,
+		labelStyle: PropTypes.object,
+		infoStyle: PropTypes.object,
+		className: PropTypes.string,
 		gapClassName: PropTypes.string,
 		labelClassName: PropTypes.string,
 		infoClassName: PropTypes.string
 	};
 
-	static defaultProps = {};
+	static defaultProps = {
+		style: {},
+		gapStyle: {},
+		infoStyle: {},
+		labelStyle: {}
+	};
 
 	render () {
-		let {label, info, className, gapClassName, labelClassName, infoClassName} = this.props;
+		let {
+			label, 
+			info, 
+			style,
+			gapStyle,
+			labelStyle,
+			infoStyle,
+			className, 
+			gapClassName, 
+			labelClassName, 
+			infoClassName
+		} = this.props;
 		let hasGap = label && label.length > 0 && info && info.length > 0;
 
 		return (
-			<div className={classNames(defaultStyles.pillButton, className)} >
-				<div className={classNames(defaultStyles.label, labelClassName)}>
+			<div className={classNames(defaultStyles.pillButton, className)} style={style} onClick={this.props.onClick}>
+				<div className={classNames(defaultStyles.label, labelClassName)} style={labelStyle}>
 					{label}
 				</div>
 				{
-					hasGap ? <div className={classNames(defaultStyles.gap, gapClassName)}></div> : ''
+					hasGap ? <div className={classNames(defaultStyles.gap, gapClassName)} style={gapStyle}></div> : ''
 				}
-				<div className={classNames(defaultStyles.info, infoClassName)}>
+				<div className={classNames(defaultStyles.info, infoClassName)} style={infoStyle}>
 					{info}
 				</div>
 			</div>
