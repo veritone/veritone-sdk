23a0564af64ee87a94c3b98a64a11e1bc8615428
Khuong Truong <ktruong@veritone.com>
2018-03-22 17:37:18 -0700

    VTN-6758 convert indentation from using 4 spaces to 1 tab

--- packages/veritone-react-common/src/components/LogoDetectionEngineOutput/index.js
+++ packages/veritone-react-common/src/components/LogoDetectionEngineOutput/index.js
@@ -12,187 +12,187 @@ import { msToReadableString } from '../../helpers/time'
 import styles from './styles.scss';
 
 export default class LogoDetectionEngineOutput extends Component {
-    static propTypes = {
-        label: PropTypes.string,
-        data: PropTypes.arrayOf(Object),
-        mediaPlayerTime: PropTypes.number,
-        className: PropTypes.string,
-        headerClassName: PropTypes.string,
-        bodyClassName: PropTypes.string,
-        itemClassName: PropTypes.string,
-        itemLabelClassName: PropTypes.string,
-        itemInfoClassName: PropTypes.string,
-        onItemSelected: PropTypes.func,
-        onEngineSelected: PropTypes.func,
-        onExpandViewClicked: PropTypes.func
-    };
+	static propTypes = {
+		label: PropTypes.string,
+		data: PropTypes.arrayOf(Object),
+		mediaPlayerTime: PropTypes.number,
+		className: PropTypes.string,
+		headerClassName: PropTypes.string,
+		bodyClassName: PropTypes.string,
+		itemClassName: PropTypes.string,
+		itemLabelClassName: PropTypes.string,
+		itemInfoClassName: PropTypes.string,
+		onItemSelected: PropTypes.func,
+		onEngineSelected: PropTypes.func,
+		onExpandViewClicked: PropTypes.func
+	};
 
-    static defaultProps = {
-        label: 'Logo Recognition',
-        data: [],
-        mediaPlayerTime: -1
-    };
+	static defaultProps = {
+		label: 'Logo Recognition',
+		data: [],
+		mediaPlayerTime: -1
+	};
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isExpanded: false
-        };
+	constructor(props) {
+		super(props);
+		this.state = {
+			isExpanded: false
+		};
 
-        this.onEngineChanged = this.onEngineChanged.bind(this);
-        this.onExpandViewClicked = this.onExpandViewClicked.bind(this);
-    }
+		this.onEngineChanged = this.onEngineChanged.bind(this);
+		this.onExpandViewClicked = this.onExpandViewClicked.bind(this);
+	}
 
-    //================================================================================
-    //=============================Interaction Functions==============================
-    //================================================================================
-    onItemSelected = item => (event) => {
-        let itemInfo = {
-            item: item,
-            sourceEngineId: this.state.sourceEngineId || this.props.data[0].sourceEngineId
-        }
-        
-        if (this.props.onItemSelected)      this.props.onItemSelected(itemInfo);
-    }
+	//================================================================================
+	//=============================Interaction Functions==============================
+	//================================================================================
+	onItemSelected = item => (event) => {
+		let itemInfo = {
+			item: item,
+			sourceEngineId: this.state.sourceEngineId || this.props.data[0].sourceEngineId
+		}
 
-    onEngineChanged (event) {
-        let selectedID = event.target.value;
-        this.setState({
-            sourceEngineId: selectedID
-        });
+		if (this.props.onItemSelected) this.props.onItemSelected(itemInfo);
+	}
 
-        if (this.props.onEngineSelected)    this.props.onEngineSelected(selectedID);
-    }
+	onEngineChanged(event) {
+		let selectedID = event.target.value;
+		this.setState({
+			sourceEngineId: selectedID
+		});
 
-    onExpandViewClicked (event) {
-        let newStatus = !this.state.isExpanded;
-        this.setState({
-            isExpanded: newStatus
-        });
+		if (this.props.onEngineSelected) this.props.onEngineSelected(selectedID);
+	}
 
-        if (this.props.onExpandViewClicked) this.props.onExpandViewClicked(newStatus);
-    }
+	onExpandViewClicked(event) {
+		let newStatus = !this.state.isExpanded;
+		this.setState({
+			isExpanded: newStatus
+		});
 
-    //================================================================================
-    //================================Helper Functions================================
-    //================================================================================
-    /**
-     * draw engine dropdown selections
-     * @param {Array<{sourceEngineId:string, sourceEngineName:string, taskId: string, series:Array>} data - engine output data
-     * @return MUI Select Element
-     */
-    drawEngineSelection (data) {
-        if (!data || data.length === 0)      return '';
+		if (this.props.onExpandViewClicked) this.props.onExpandViewClicked(newStatus);
+	}
 
-        let selectedID = this.state.sourceEngineId || data[0].sourceEngineId;
-        return (
-            <Select value={selectedID} onChange={this.onEngineChanged}>
-                {
-                    data.map((engineInfo) => {
-                        return (
-                            <MenuItem 
-                                value={engineInfo.sourceEngineId} 
-                                key={'engine-logo-' + engineInfo.sourceEngineId}
-                            >
-                                {engineInfo.sourceEngineName}
-                            </MenuItem>
-                        );
-                    })
-                }
-            </Select>
-        );
-    }
+	//================================================================================
+	//================================Helper Functions================================
+	//================================================================================
+	/**
+	 * draw engine dropdown selections
+	 * @param {Array<{sourceEngineId:string, sourceEngineName:string, taskId: string, series:Array>} data - engine output data
+	 * @return MUI Select Element
+	 */
+	drawEngineSelection(data) {
+		if (!data || data.length === 0) return '';
 
-    /**
-     * Draw detected logo entry in the series
-     * @param {Array<{sourceEngineId:string, series:Array<{startTimeMs:number, endTimeMs:number, logo:{label:string}}>>} data - engine output data
-     * @return {Array<PillButton>}
-     */
-    drawItems (data) {
-        let currentPlayTime = this.props.mediaPlayerTime;
-        let itemClassName = this.props.itemClassName;
-        let itemInfoClassName = this.props.itemInfoClassName;
-        let itemLabelClassName = this.props.itemLabelClassName;
-        let selectedEngineID = this.state.sourceEngineId || data[0].sourceEngineId;
+		let selectedID = this.state.sourceEngineId || data[0].sourceEngineId;
+		return (
+			<Select value={selectedID} onChange={this.onEngineChanged}>
+				{
+					data.map((engineInfo) => {
+						return (
+							<MenuItem
+								value={engineInfo.sourceEngineId}
+								key={'engine-logo-' + engineInfo.sourceEngineId}
+							>
+								{engineInfo.sourceEngineName}
+							</MenuItem>
+						);
+					})
+				}
+			</Select>
+		);
+	}
 
-        let items = [];
-        if (data && data.length > 0) {
-            data.map((engineInfo) => {
-                if (engineInfo.sourceEngineId === selectedEngineID) {                                   //Look for selected engine
+	/**
+	 * Draw detected logo entry in the series
+	 * @param {Array<{sourceEngineId:string, series:Array<{startTimeMs:number, endTimeMs:number, logo:{label:string}}>>} data - engine output data
+	 * @return {Array<PillButton>}
+	 */
+	drawItems(data) {
+		let currentPlayTime = this.props.mediaPlayerTime;
+		let itemClassName = this.props.itemClassName;
+		let itemInfoClassName = this.props.itemInfoClassName;
+		let itemLabelClassName = this.props.itemLabelClassName;
+		let selectedEngineID = this.state.sourceEngineId || data[0].sourceEngineId;
 
-                    // Sort detected logos by there start time and end time
-                    engineInfo.series.sort(function(item1, item2){
-                        let startTimeDiff = item1.startTimeMs - item2.startTimeMs;
-                        if (startTimeDiff !== 0)    return startTimeDiff;
-                        else                        return item1.endTimeMs - item2.endTimeMs;
-                    });
+		let items = [];
+		if (data && data.length > 0) {
+			data.map((engineInfo) => {
+				if (engineInfo.sourceEngineId === selectedEngineID) {									//Look for selected engine
 
-                    // Draw detected logos in the series
-                    engineInfo.series.map((itemInfo, index) => {
-                        if (itemInfo.logo) {                                                            //Look for detected logo
-                            let startTime = msToReadableString(itemInfo.startTimeMs);
-                            let endTime = msToReadableString(itemInfo.endTimeMs);
-                            let logoItem = (
-                                <PillButton
-                                    value={index} 
-                                    label={itemInfo.logo.label} 
-                                    info={startTime + ' - ' + endTime}
-                                    className={classNames(styles.item, itemClassName)}
-                                    labelClassName={classNames(styles.label, itemLabelClassName)}
-                                    infoClassName={itemInfoClassName}
-                                    key={'logo-' + engineInfo.sourceEngineId + index}
-                                    onClick={this.onItemSelected(itemInfo)}
-                                    highlight={(itemInfo.startTimeMs <= currentPlayTime) ? true : false}
-                                />
-                            );
-                            items.push(logoItem);
-                        }
-                    });
-                }
-            });
-        }
-        return items;
-    }
+					// Sort detected logos by there start time and end time
+					engineInfo.series.sort(function (item1, item2) {
+						let startTimeDiff = item1.startTimeMs - item2.startTimeMs;
+						if (startTimeDiff !== 0) return startTimeDiff;
+						else return item1.endTimeMs - item2.endTimeMs;
+					});
 
-    //================================================================================
-    //================================Render Function=================================
-    //================================================================================
-    render () {
-        let {
-                data,
-                label, 
-                className, 
-                headerClassName,
-                bodyClassName
-            } = this.props;
-        
-        return (
-            <div className={(this.state.isExpanded) ? classNames(styles.logoDetection, styles.expanded, className) : classNames(styles.logoDetection, className)}>
-                {/* -----Header----- */}
-                <EngineOutputHeader title={label} className={headerClassName}>
-                    <div className={styles.headerRight}>
-                        {/* -----Engine Selection----- */}
-                        <div>
-                            {this.drawEngineSelection(data)}
-                        </div>
+					// Draw detected logos in the series
+					engineInfo.series.map((itemInfo, index) => {
+						if (itemInfo.logo) {															//Look for detected logo
+							let startTime = msToReadableString(itemInfo.startTimeMs);
+							let endTime = msToReadableString(itemInfo.endTimeMs);
+							let logoItem = (
+								<PillButton
+									value={index}
+									label={itemInfo.logo.label}
+									info={startTime + ' - ' + endTime}
+									className={classNames(styles.item, itemClassName)}
+									labelClassName={classNames(styles.label, itemLabelClassName)}
+									infoClassName={itemInfoClassName}
+									key={'logo-' + engineInfo.sourceEngineId + index}
+									onClick={this.onItemSelected(itemInfo)}
+									highlight={(itemInfo.startTimeMs <= currentPlayTime) ? true : false}
+								/>
+							);
+							items.push(logoItem);
+						}
+					});
+				}
+			});
+		}
+		return items;
+	}
 
-                        {/* -----Fullscreen button----- */}
-                        <div>
-                            <IconButton onClick={this.onExpandViewClicked}>
-                                <ZoomOutMap />
-                            </IconButton>
-                        </div>
-                    </div>
-                </ EngineOutputHeader>
+	//================================================================================
+	//================================Render Function=================================
+	//================================================================================
+	render() {
+		let {
+			data,
+			label,
+			className,
+			headerClassName,
+			bodyClassName
+		} = this.props;
 
-                {/* -----Body----- */}
-                <div className={classNames(styles.logoDetectionBody, bodyClassName)}>
-                    { 
-                        /* -----Detected Logos----- */
-                        this.drawItems(data) 
-                    }
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className={(this.state.isExpanded) ? classNames(styles.logoDetection, styles.expanded, className) : classNames(styles.logoDetection, className)}>
+				{/* -----Header----- */}
+				<EngineOutputHeader title={label} className={headerClassName}>
+					<div className={styles.headerRight}>
+						{/* -----Engine Selection----- */}
+						<div>
+							{this.drawEngineSelection(data)}
+						</div>
+
+						{/* -----Fullscreen button----- */}
+						<div>
+							<IconButton onClick={this.onExpandViewClicked}>
+								<ZoomOutMap />
+							</IconButton>
+						</div>
+					</div>
+				</ EngineOutputHeader>
+
+				{/* -----Body----- */}
+				<div className={classNames(styles.logoDetectionBody, bodyClassName)}>
+					{
+						/* -----Detected Logos----- */
+						this.drawItems(data)
+					}
+				</div>
+			</div>
+		);
+	}
 }
--- packages/veritone-react-common/src/components/LogoDetectionEngineOutput/story.js
+++ packages/veritone-react-common/src/components/LogoDetectionEngineOutput/story.js
@@ -6,99 +6,98 @@ import LogoDetectionEngineOutput from './';
 
 
 storiesOf('LogoDetectionEngineOutput', module).add('Base', () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                Left Box
-            </div>
-            <LogoDetectionEngineOutput 
-                className={styles.outputViewRoot}
-                data={mockData}
-            />
-            <div className={styles.right}>
-                Right Box
-            </div>
-            <div className={styles.bottom}>
-                Bottom Box
-            </div>
-        </div>
-        
-    );
+	return (
+		<div className={styles.container}>
+			<div className={styles.left}>
+				Left Box
+			</div>
+			<LogoDetectionEngineOutput 
+				className={styles.outputViewRoot}
+				data={mockData}
+			/>
+			<div className={styles.right}>
+				Right Box
+			</div>
+			<div className={styles.bottom}>
+				Bottom Box
+			</div>
+		</div>
+	);
 });
 
 const mockData = [
-    {
-        startTimeMs: 0,
-        endTimeMs: 9000,
-        sourceEngineId: "2dc5166f-c0ad-4d84-8a85-515c42b5d357",
-        sourceEngineName: "Server-R",
-        taskId: "e1fa7d7c-6f1c-480e-b181-68940509f070-fef496da-f36e-49ec-a304-426d96017ddf",
-        series: [
-            {
-                startTimeMs: 0,
-                endTimeMs: 1000,
-                logo: {
-                    label: "ESPN",
-                    confidence: 0.942457377910614
-                }
-            },
-            {
-                startTimeMs: 4000,
-                endTimeMs: 5000,
-                logo: {
-                label: "Google",
-                confidence: 0.942457377910614
-                }
-            },
-            {
-                startTimeMs: 4000,
-                endTimeMs: 15000,
-                logo: {
-                    label: "Veritone",
-                    confidence: 0.942457377910614
-                }
-            },
-            {
-                startTimeMs: 7500,
-                endTimeMs: 238000,
-                logo: {
-                    label: "Veritone",
-                    confidence: 0.942457377910614
-                }
-            },
-            {
-                startTimeMs: 7500,
-                endTimeMs: 238000,
-                logo: {
-                    label: "This is a very long logo name it goes on for a while",
-                    confidence: 0.942457377910614
-                }
-            }
-        ]
-    },
-    {
-        startTimeMs: 0,
-        endTimeMs: 9000,
-        sourceEngineId: "2dc5166f-c0ad-4d84-8a85-515c42b5d358",
-        sourceEngineName: "Machine X",
-        taskId: "e1fa7d7c-6f1c-480e-b181-68940509f070-fef496da-f36e-49ec-a304-426d96017ddf",
-        series: [
-            {
-                startTimeMs: 0,
-                endTimeMs: 1000,
-                logo: {
-                    label: "data1",
-                    confidence: 0.942457377910614
-                }
-            },
-            {
-                startTimeMs: 2000,
-                endTimeMs: 123000,
-                logo: {
-                    label: "data2",
-                    confidence: 0.942457377910614
-                }
-            }
-        ]
-    }
+	{
+		startTimeMs: 0,
+		endTimeMs: 9000,
+		sourceEngineId: "2dc5166f-c0ad-4d84-8a85-515c42b5d357",
+		sourceEngineName: "Server-R",
+		taskId: "e1fa7d7c-6f1c-480e-b181-68940509f070-fef496da-f36e-49ec-a304-426d96017ddf",
+		series: [
+			{
+				startTimeMs: 0,
+				endTimeMs: 1000,
+				logo: {
+					label: "ESPN",
+					confidence: 0.942457377910614
+				}
+			},
+			{
+				startTimeMs: 4000,
+				endTimeMs: 5000,
+				logo: {
+				label: "Google",
+				confidence: 0.942457377910614
+				}
+			},
+			{
+				startTimeMs: 4000,
+				endTimeMs: 15000,
+				logo: {
+					label: "Veritone",
+					confidence: 0.942457377910614
+				}
+			},
+			{
+				startTimeMs: 7500,
+				endTimeMs: 238000,
+				logo: {
+					label: "Veritone",
+					confidence: 0.942457377910614
+				}
+			},
+			{
+				startTimeMs: 7500,
+				endTimeMs: 238000,
+				logo: {
+					label: "This is a very long logo name it goes on for a while",
+					confidence: 0.942457377910614
+				}
+			}
+		]
+	},
+	{
+		startTimeMs: 0,
+		endTimeMs: 9000,
+		sourceEngineId: "2dc5166f-c0ad-4d84-8a85-515c42b5d358",
+		sourceEngineName: "Machine X",
+		taskId: "e1fa7d7c-6f1c-480e-b181-68940509f070-fef496da-f36e-49ec-a304-426d96017ddf",
+		series: [
+			{
+				startTimeMs: 0,
+				endTimeMs: 1000,
+				logo: {
+					label: "data1",
+					confidence: 0.942457377910614
+				}
+			},
+			{
+				startTimeMs: 2000,
+				endTimeMs: 123000,
+				logo: {
+					label: "data2",
+					confidence: 0.942457377910614
+				}
+			}
+		]
+	}
 ]
