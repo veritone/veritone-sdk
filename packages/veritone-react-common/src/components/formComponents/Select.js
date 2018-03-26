5e6ad8e248fa0e7804b92ac6c282216738bb4292
AlexO <oiaroshchuk@veritone.com>
2018-03-26 14:40:17 -0700

    Import lib for some of the form components so those do not collide with material-ui imports.

--- packages/veritone-react-common/src/components/formComponents/Select.js
+++ packages/veritone-react-common/src/components/formComponents/Select.js
@@ -1,3 +1,9 @@
-import { Select } from 'redux-form-material-ui';
+import React from 'react';
+import LibSelect from 'redux-form-material-ui';
+
+/* eslint-disable react/prop-types */
+const Select = ({ meta, input, ...props }) => {
+  return <LibSelect {...input} {...props} />;
+};
 
 export default Select;
