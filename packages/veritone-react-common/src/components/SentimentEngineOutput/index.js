5f7362ddc8e06d3e812f3dee171decf6606b9b56
Khuong Truong <ktruong@veritone.com>
2018-03-27 12:02:48 -0700

    VTN-6650 add Sentiment Output Component

--- packages/veritone-react-common/src/components/SentimentEngineOutput/index.js
+++ packages/veritone-react-common/src/components/SentimentEngineOutput/index.js
@@ -1,183 +1,245 @@
 import React, { Component } from 'react';
-import { shape, string, arrayOf, number } from 'prop-types';
+import {number, string, arrayOf, any, func} from 'prop-types';
 import classNames from 'classnames';
-import Select from 'material-ui/Select';
-import { MenuItem } from 'material-ui/Menu';
-
-import EngineOutputHeader from '../EngineOutputHeader';
-import SentimentGraph from './SentimentGraph';
-
+import { msToReadableString } from '../../helpers/time';
 import styles from './styles.scss';
 
-// Smooth/normalize sentiment time-series data, using a set of Exponentially-Weighted Moving Averages (EWMA):
-function smoothAndNormalizeTimeSeries(series, duration) {
-  // Exponentially-Weighted Moving Averages:
-  let directionEwma = 0;
-  let positiveEwma = 0;
-  let negativeEwma = 0;
-  let finalEwma = 0;
-  // directionAlpha should be within the range [0,1], and controls the resistance of the smoothed
-  // data to changes in the sign of the underlying data. A higher directionAlpha setting results
-  // in *less* resistance to sign changes.
-  let directionAlpha = 0.3;
-  // positiveAlpha should be within the range [0,1], and controls smoothing of positive sections
-  // of the time series.
-  let positiveAlpha = 0.2;
-  // negativeAlpha should be within the range [0,1], and controls smoothing of negative sections
-  // of the time series.
-  let negativeAlpha = 0.2;
-  // finalAlpha should be within the range [0,1], and controls final smoothing applied to the
-  // pre-processed data.
-  let finalAlpha = 0.7;
-  // Scores are normalized relative to normalizationBase, which should be within the range [0,1].
-  // Any normalized values exceeding 1.0 or -1.0 will be clipped to 1.0 and -1.0, respectively.
-  let normalizationBase = 0.7;
+import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Area, ReferenceLine } from 'recharts';
 
-  let sumOfScores = 0;
-
-  let seriesLength = series.length;
-
-  let normalizedSeries = series.map(function mapDatum(datum) {
-    let normalizedDatum = normalizeDatum(datum);
-    if (seriesLength > 20) {
-      normalizedDatum = smoothDatum(normalizedDatum);
-    }
-    sumOfScores += normalizedDatum.score;
-    return normalizedDatum;
-  });
-
-  // Insert initial anchor data-point:
-  if (seriesLength && normalizedSeries[0].time !== 0) {
-    normalizedSeries.unshift({
-      score: 0,
-      time: 0
-    });
-    seriesLength = normalizedSeries.length;
-  }
-
-  // Remove data-points which extend beyond the media duration:
-  for (let i = seriesLength - 1; i >= 0; i--) {
-    if (normalizedSeries[i].time > duration) {
-      normalizedSeries.splice(i, 1);
-    }
-  }
-
-  // Insert final anchor data-point:
-  if (normalizedSeries.length > 0 && normalizedSeries[normalizedSeries.length - 1].time !== duration) {
-    normalizedSeries.push({
-      score: 0,
-      time: duration
-    });
-  }
-
-  let paddingInterval = normalizedSeries.length > 1 ? normalizedSeries[1].time : 0;
-
-  // Fill gaps (no sentiment data) with zero-padding:
-  for (let j = normalizedSeries.length - 1; j >= 1; j--) {
-    let t1 = normalizedSeries[j].time;
-    let t0 = normalizedSeries[j - 1].time;
-    if (t1 - t0 > paddingInterval) {
-      let insertions = Math.floor((t1 - t0) / paddingInterval);
-      for (let k = 0; k < insertions; k++) {
-        let time = t0 + (paddingInterval * (k + 1));
-        if (time >= t1) {
-          break;
-        }
-        normalizedSeries.splice(j + k, 0, {
-          score: 0,
-          time: time
-        });
-      }
-    }
-    paddingInterval = 5000;
-  }
-
-  // Calculate average of adjusted data (ignoring gaps with no sentiment data):
-  let average = !seriesLength ? 0 : (sumOfScores / seriesLength);
-  // Round average to 1 decimal place:
-  average = Math.round(average * 10) / 10;
-
-  return {
-    series: normalizedSeries,
-    average: average
-  };
-
-  // Map "score" from [0,1] range to [-100,100] range and set "time" to midpoint of start/end:
-  function normalizeDatum(datum) {
-    let normalizedDatum = {
-      score: (datum.score - 0.5) * 200,
-      time: (datum.start + datum.end) / 2
-    };
-
-    normalizedDatum.score /= normalizationBase;
-    if (normalizedDatum.score > 100) {
-      normalizedDatum.score = 100;
-    } else if (normalizedDatum.score < -100) {
-      normalizedDatum.score = -100;
-    }
-
-    return normalizedDatum;
-  }
-
-  // Apply exponential smoothing to a data-point:
-  function smoothDatum(datum) {
-    if (datum.score >= 0) {
-      directionEwma = directionAlpha + (1 - directionAlpha) * directionEwma;
-    } else {
-      directionEwma = -directionAlpha + (1 - directionAlpha) * directionEwma;
-    }
-    if (directionEwma >= 0) {
-      if (positiveEwma === 0) {
-        positiveEwma = datum.score;
-      } else {
-        positiveEwma = positiveAlpha * datum.score + (1 - positiveAlpha) * positiveEwma;
-      }
-      datum.score = positiveEwma;
-    } else {
-      if (negativeEwma === 0) {
-        negativeEwma = datum.score;
-      } else {
-        negativeEwma = negativeAlpha * datum.score + (1 - negativeAlpha) * negativeEwma;
-      }
-      datum.score = negativeEwma;
-    }
-    finalEwma = finalAlpha * datum.score + (1 - finalAlpha) * finalEwma;
-    datum.score = finalEwma;
-
-    return datum;
-  }
-}
-
-class SentimentEngineOutput extends Component {
+export default class SentimentEngineOutput extends Component {
   static propTypes = {
-    data: arrayOf(shape({
-      end: number,
-      start: number,
-      score: number
-    })),
-    className: string
+    data: arrayOf(any),
+    className: string,
+    mediaPlayerTime: number,
+    timeWindowSizeMs: number,
+    timeWindowStartMs: number,
+    timeTickIntervalMs: number,
+    sentimentTicks: arrayOf(number),
+    sentimentDomain: arrayOf(number),
+    onTimeClick: func,
+    onTimeScroll: func
   };
 
-  render() {
-    let { classes, data, className } = this.props;
-    let duration = data[data.length - 1]['end'] - data[0]['start'];
-    let taskData = smoothAndNormalizeTimeSeries(data || [], duration);
+  static defaultProps = {
+    data: [],
+    mediaPlayerTime: 0,
+    timeWindowSizeMs: 400000,
+    timeWindowStartMs: 0,
+    timeTickIntervalMs: 50000,
+    sentimentDomain: [100, -100],
+    sentimentTicks: [100, 80, 60, 40, 20, 0, -20, -40, -60, -80, -100]
+  };
+
+  //================================================================================
+  //=============================Interaction Functions==============================
+  //================================================================================
+  handleTimeScroll = (event) => {
+    if (this.props.onTimeScroll) {
+      this.scrollTimeOut && clearTimeout(this.scrollTimeOut);
+      this.scrollTimeOut = setTimeout(this.onDoneTimeScroll, 500, event);
+    }
+  }
+
+  onDoneTimeScroll = (event) => {
+    this.scrollTimeOut = null;
+    
+    if (this.props.onTimeScroll) {
+      let seriesData = this.props.data;
+      let totalTime = seriesData[seriesData.length - 1].stopTimeMs;
+      
+      let chartWidth = event.target.scrollWidth;
+      let chartPosition =  event.target.scrollLeft;
+
+      let time = totalTime * chartPosition / chartWidth;
+      this.props.onTimeScroll(time);
+    }
+  }
+  
+  handleTimeClick = (event) => {
+    this.props.onTimeClick && this.props.onTimeClick(event);
+  }
+
+  //================================================================================
+  //================================Helper Functions================================
+  //================================================================================
+  extractPropsData = () => {
+    let {
+      data,
+      sentimentTicks,
+      sentimentDomain,
+      mediaPlayerTime,
+      timeWindowSizeMs,
+      timeWindowStartMs,
+      timeTickIntervalMs
+    } = this.props;
+
+    let chartData = [{sartTimeMs: 0, stopTimeMs: 0, sentiment: 0}];
+    let numValidValue = 0;
+    let totalSentiment = 0;
+    if (data && Array.isArray(data) && data.length > 0) {
+      data.map((entry, index) => {
+        if(!entry.hasOwnProperty('status') || entry.status === 'complete') {
+          let sentimentValue = 0;
+          let sentiment = entry.sentiment;
+          if (sentiment.positiveConfidence > sentiment.negativeConfidence) {
+            sentimentValue = sentiment.positiveValue * 100;
+          } else if (sentiment.positiveConfidence < sentiment.negativeConfidence) {
+            sentimentValue = sentiment.negativeValue * 100;
+          }
+  
+          numValidValue++;
+          totalSentiment = totalSentiment + sentimentValue;
+  
+          let chartFriendlyData = {
+            startTimeMs: entry.startTimeMs,
+            stopTimeMs: entry.stopTimeMs,
+            sentiment: sentimentValue
+          }
+  
+          chartData.push(chartFriendlyData);
+        } else {
+          //handle failed & pending status
+          chartData.push({
+            startTimeMs: entry.startTimeMs,
+            stopTimeMs: entry.stopTimeMs,
+            sentiment: 0
+          });
+        }
+      });
+    }
+    
+    let totalTime = data[data.length - 1].stopTimeMs;
+    let xDomain = [0, totalTime];
+    let xTicks = [];
+    for (let xTickValue = 0; xTickValue <= totalTime; xTickValue = xTickValue + timeTickIntervalMs) {
+      xTicks.push(xTickValue);
+    }
+
+    return {
+      chartData: chartData,
+      average: totalSentiment / numValidValue,
+      xTicks: xTicks,
+      xDomain: xDomain,
+      yTicks: sentimentTicks,
+      yDomain: sentimentDomain,
+      referenceValue: mediaPlayerTime,
+      totalTime: totalTime,
+      scrollToTime: timeWindowStartMs,
+      scaleX: totalTime / timeWindowSizeMs
+    };
+  }
+
+  setScrollRef = (target) => {
+    this.scrollContent = target;
+  }
+
+  scrollTo (timeMs, totalTimeMs) {
+    let scrollTarget = this.scrollContent;
+    let scrollRatio = timeMs / totalTimeMs;
+    
+    scrollTarget.scrollLeft = scrollRatio * scrollTarget.scrollWidth;
+  }
+
+  formatXValues (value) {
+    return msToReadableString(value);
+  }
+
+  formatYValues (value) {
+    return value + "%"
+  }
+
+  //================================================================================
+  //================================Render Function=================================
+  //================================================================================
+  renderSummary (average) {
     return (
-      <div className={classNames(styles.sentimentOutputView, className)}>
-        <EngineOutputHeader title="Sentiment">
-          {/* TODO: update this to take in an array of engines  and create menu items */}
-          <Select value="medulla">
-            <MenuItem value="medulla">Medulla</MenuItem>
-          </Select>
-        </EngineOutputHeader>
-        <div className={styles.sentimentViewContent}>
-          <SentimentGraph 
-            data={taskData.series}
-          />
+      <div 
+        className={classNames(styles.summary, {
+          [styles.positive]: average >= 0,
+          [styles.negative]: average < 0
+        })}
+      >
+        {average + ((average >=0) ? '% positive' : '% negative')}
+      </div>
+    );
+  }
+
+  renderChart (extractedData) {
+    let {
+      chartData,
+      xTicks,
+      xDomain,
+      yTicks,
+      yDomain,
+      scaleX,
+      referenceValue,
+      totalTime,
+      scrollToTime
+     } = extractedData;
+
+    // Compute color offset
+    let maxSentiment = Math.max(...chartData.map(entry => entry.sentiment));
+    let minSentiment = Math.min(...chartData.map(entry => entry.sentiment));
+
+    let offset = 0;                                               // Everything is red;
+    if (maxSentiment > 0 && minSentiment > 0) {
+      offset = 1;                                                 // Everything is green
+    } else if (maxSentiment > 0) {
+      offset = maxSentiment / (maxSentiment - minSentiment);      // Above 0 is green & below 0 is red
+    }
+
+    setTimeout(() => {
+      this.scrollTo(scrollToTime, totalTime);
+    });
+
+    return (
+      <div className={styles.sentimentBody}>
+        {/* Draw Y axis this part is not scrollable */}
+        <div className={styles.yAxis}>
+          <ResponsiveContainer width="100%" height="100%" >
+            <AreaChart data={chartData} margin={{top: 10, right: 30, left: 0, bottom: 40}}>
+              <XAxis tick={false} type="number"/>
+              <YAxis dataKey="sentiment" domain={yDomain} ticks={yTicks} tickFormatter={this.formatYValues} type="number"/>
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+
+        {/* Draw Chart Content Container */}
+        <div className={styles.content} onScroll={this.handleTimeScroll} ref={this.setScrollRef}>
+
+          {/* Draw Scrollable Chart Content */}
+          <ResponsiveContainer width={(100 * scaleX).toString() + '%'} height="100%">
+            <AreaChart data={chartData} margin={{top: 10, right: 0, left: 0, bottom: 40}} onClick={this.handleTimeClick} >
+              <XAxis interval="preserveStartEnd" dataKey="stopTimeMs" domain={xDomain} ticks={xTicks} tickFormatter={this.formatXValues} type="number"/>
+              <YAxis hide dataKey="sentiment" domain={yDomain} ticks={yTicks} tickFormatter={this.formatYValues}/>
+              <CartesianGrid />
+
+              {/* Split Colors */}
+              <defs>
+                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset={offset} stopColor="green" stopOpacity={1}/>
+                  <stop offset={offset} stopColor="red" stopOpacity={1}/>
+                </linearGradient>
+              </defs>
+              <Area type="monotone" dataKey='sentiment' stroke='url(#splitColor)' fill='url(#splitColor)'/>
+
+              {/* Draw media player time */}
+              <ReferenceLine x={referenceValue} stroke="orange"/>
+            </AreaChart>
+          </ResponsiveContainer>
         </div>
       </div>
     );
   }
-}
 
-export default SentimentEngineOutput;
+  render () {
+    let extractedData = this.extractPropsData();
+    
+    return (
+      <div className={classNames(styles.sentimentOutput, this.props.className)}>
+        { this.renderSummary(extractedData.average) }
+        { this.renderChart(extractedData) }
+      </div>
+    );
+  }
+}
