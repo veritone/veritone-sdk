bbbf3f36690468a4ba2eeeacc906c7a49928ba16
Chris Pelletier <cpelletier@veritone.com>
2018-03-28 18:54:04 -0700

    VTN-6648 Created a component for displaying recognized faces in the the frame view.

---
+++ packages/veritone-react-common/src/components/FaceEngineOutput/FacesByFrame/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { number, objectOf, shape, string, func, arrayOf } from 'prop-types';
+
+import FaceFrameContainer from '../FaceFrameContainer';
+
+class FacesByFrame extends Component {
+  static propTypes = {
+    recognizedEntityObjectMap: objectOf(
+      shape({
+        count: number,
+        entityId: string,
+        fullName: string,
+        profileImage: string,
+        stopTimeMs: number
+      })
+    ),
+    framesBySeconds: objectOf(
+      objectOf(
+        shape({
+          startTimeMs: number,
+          endTimeMs: number,
+          originalImage: string,
+          entities: arrayOf(
+            shape({
+              confidence: number,
+              entityId: string
+            })
+          ),
+          boundingPoly: arrayOf(objectOf(number))
+        })
+      )
+    ),
+    mediaPlayerPosition: number,
+    onSelectEntity: func
+  };
+  render() {
+    let {
+      recognizedEntityObjectMap,
+      framesBySeconds,
+      mediaPlayerPosition,
+      onSelectEntity
+    } = this.props;
+
+    return (
+      <div>
+        {framesBySeconds[mediaPlayerPosition - mediaPlayerPosition % 1000] ? (
+          <div>
+            {Object.keys(
+              framesBySeconds[mediaPlayerPosition - mediaPlayerPosition % 1000]
+            ).map((k, i) => {
+              return (
+                <FaceFrameContainer
+                  key={'frame-container-' + k}
+                  faceFrame={
+                    framesBySeconds[
+                      mediaPlayerPosition - mediaPlayerPosition % 1000
+                    ][k]
+                  }
+                  recognizedEntityObjectMap={recognizedEntityObjectMap}
+                  onSelectEntity={onSelectEntity}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div>No Face Matches Found</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default FacesByFrame;
