855b891d9d17e90457bc3de9f5a83ce40ddeaefc
Chris Pelletier <cpelletier@veritone.com>
2018-03-28 19:48:01 -0700

    VTN-6648 Make container expandable

--- packages/veritone-react-common/src/components/FaceEngineOutput/FaceFrameContainer/index.js
+++ packages/veritone-react-common/src/components/FaceEngineOutput/FaceFrameContainer/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { number, string, arrayOf, shape, objectOf, func } from 'prop-types';
+import Icon from 'material-ui/Icon';
+import Button from 'material-ui/Button';
+import cx from 'classnames';
 
 import { msToReadableString } from 'helpers/time';
 import RecognizedFaceMatch from '../RecognizedFaceMatch';
@@ -36,33 +39,61 @@ class FaceFrameContainer extends Component {
     expanded: false
   };
 
+  expandContainer = () => {
+    this.setState({
+      expanded: true
+    });
+  };
+
   render() {
     let { faceFrame, recognizedEntityObjectMap, onSelectEntity } = this.props;
-    
+    let faceFrameEntities = faceFrame.entities;
+    if (!this.state.expanded && faceFrameEntities.length > 3) {
+      faceFrameEntities = faceFrameEntities.slice(0, 3);
+    }
     return (
       <div className={styles.frameContainer}>
-        {faceFrame.originalImage && (
-          <div className={styles.recognizedFaceContainer}>
-            <img src={faceFrame.originalImage} className={styles.sourceImage} />
-            <div className={styles.sourceImageTimestamp}>
-              {msToReadableString(faceFrame.startTimeMs)} -{' '}
-              {msToReadableString(faceFrame.stopTimeMs)}
-            </div>
-            <div className={styles.sourceImageSubtitle}>Source Image</div>
-          </div>
-        )}
-        <div className={styles.recognizedMatches}>
-          {faceFrame.entities.map((entity, i) => {
-            return (
-              <RecognizedFaceMatch
-                key={'frame-view-recognized-entity-' + entity.entityId}
-                entity={recognizedEntityObjectMap[entity.entityId]}
-                confidence={entity.confidence}
-                onViewDetailsClick={onSelectEntity}
+        <div className={styles.sourceAndImageContainer}>
+          {faceFrame.originalImage && (
+            <div className={styles.recognizedFaceContainer}>
+              <img
+                src={faceFrame.originalImage}
+                className={styles.sourceImage}
               />
-            );
-          })}
+              <div className={styles.sourceImageTimestamp}>
+                {msToReadableString(faceFrame.startTimeMs)} -{' '}
+                {msToReadableString(faceFrame.stopTimeMs)}
+              </div>
+              <div className={styles.sourceImageSubtitle}>Source Image</div>
+            </div>
+          )}
+          <div className={styles.recognizedMatches}>
+            {faceFrameEntities.map((entity, i) => {
+              return (
+                <RecognizedFaceMatch
+                  key={'frame-view-recognized-entity-' + entity.entityId}
+                  entity={recognizedEntityObjectMap[entity.entityId]}
+                  confidence={entity.confidence}
+                  onViewDetailsClick={onSelectEntity}
+                />
+              );
+            })}
+          </div>
         </div>
+        {!this.state.expanded &&
+          faceFrame.entities.length > 3 && (
+            <div className={styles.showMoreMatches}>
+              <Button
+                className={styles.showMoreButton}
+                onClick={this.expandContainer}
+              >
+                <span>view all {faceFrame.entities.length} matches found</span>
+                <Icon
+                  className={cx('icon-expand_more', styles.expandMoreIcon)}
+                />
+              </Button>
+            </div>
+          )}
       </div>
     );
   }
