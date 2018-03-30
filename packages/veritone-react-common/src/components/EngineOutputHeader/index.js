ad54402abde436fc708e3ef67450f1772cb42296
Chris Pelletier <cpelletier@veritone.com>
2018-03-30 16:50:15 -0700

    Added some more functionality to the EngineOutputHeader component.

--- packages/veritone-react-common/src/components/EngineOutputHeader/index.js
+++ packages/veritone-react-common/src/components/EngineOutputHeader/index.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
-import { string, bool, arrayOf, node, oneOfType } from 'prop-types';
+import {
+  string,
+  bool,
+  arrayOf,
+  node,
+  oneOfType,
+  shape,
+  func
+} from 'prop-types';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+import Icon from 'material-ui/Icon';
+import IconButton from 'material-ui/IconButton';
 
 import styles from './styles.scss';
 
@@ -7,15 +19,62 @@ class EngineOutputHeader extends Component {
   static propTypes = {
     title: string,
     hideTitle: bool,
+    engines: arrayOf(
+      shape({
+        id: string,
+        name: string
+      })
+    ),
+    selectedEngineId: string,
+    onEngineChange: func,
+    onExpandClicked: func,
     children: oneOfType([arrayOf(node), node])
   };
 
+  handleEngineChange = evt => {
+    if (this.props.onEngineChange) {
+      this.props.onEngineChange(evt.target.value);
+    }
+  };
+
   render() {
-    let { children, title, hideTitle } = this.props;
+    let {
+      children,
+      title,
+      hideTitle,
+      engines,
+      selectedEngineId,
+      onExpandClicked
+    } = this.props;
     return (
       <div className={styles.engineOutputHeader}>
         {!hideTitle && <div className={styles.headerTitle}>{title}</div>}
-        <div className={styles.headerActions}>{children}</div>
+        <div className={styles.headerActions}>
+          {children}
+          {engines &&
+            engines.length && (
+              <Select
+                value={selectedEngineId || engines[0].id}
+                onChange={this.handleEngineChange}
+              >
+                {engines.map((e, i) => {
+                  return (
+                    <MenuItem key={'engine-menu-item-' - e.id} value={e.id}>
+                      {e.name}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            )}
+        </div>
+        <div className={styles.actionIconDivider} />
+        <IconButton
+          aria-label="Expanded View"
+          className={styles.expandedViewButton}
+          onClick={onExpandClicked}
+        >
+          <Icon className="icon-expand2" />
+        </IconButton>
       </div>
     );
   }
