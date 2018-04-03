9990ff5aab53954dbe30a2fc2b8a2207a7fc40e8
Mitch Robb <mrobb@veritone.com>
2018-04-03 12:54:21 -0700

    beginning work on scheduler form

---
+++ packages/veritone-react-common/src/components/Scheduler/ImmediateSection.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class ImmediateSection extends React.Component {
+
+}
--- packages/veritone-react-common/src/components/Scheduler/story.js
+++ packages/veritone-react-common/src/components/Scheduler/story.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { reducer as formReducer, reduxForm, Field } from 'redux-form';
+import { combineReducers, createStore } from 'redux';
 
 import Scheduler from './';
 
-function updateSchedule(sched) {
-  console.log(sched);
-  schedule = sched;
-}
+const store = createStore(
+  combineReducers({
+    form: formReducer
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-let schedule = {};
+const StoryForm = reduxForm({
+  form: 'story',
+  initialValues: {}
+})(({ handleSubmit, children, submitting, pristine }) => (
+  <form onSubmit={handleSubmit}>
+    {children}
+    <br />
+    <button type="submit" disabled={pristine || submitting}>
+      Submit
+    </button>
+  </form>
+));
 
-storiesOf('Scheduler', module)
-  .add('Empty Scheduler', () => <Scheduler schedule={schedule} updateSchedule={updateSchedule}/>);
+/* eslint-disable react/jsx-no-bind */
+
+storiesOf('Scheduler', module).add('Empty Scheduler', () => (
+  <Provider store={store}>
+    <Scheduler />
+  </Provider>
+));
