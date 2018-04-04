bc2f1b5cc3264297b6b5f602136c8b70d3721f2d
Chris Pelletier <cpelletier@veritone.com>
2018-04-04 16:22:29 -0700

    Fixed the story mock data and there was a format change from prettier

--- packages/veritone-react-common/src/components/OCREngineOutputView/story.js
+++ packages/veritone-react-common/src/components/OCREngineOutputView/story.js
@@ -24,40 +24,40 @@ let ocrAssets = [
   {
     series: [
       {
-        end: 1000,
-        start: 0,
+        stopTimeMs: 1000,
+        startTimeMs: 0,
         object: {
           text:
             'WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray CNW Political Reporter \n LIVE \n NAS58.43 \n AONY ON CHAMPS-ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURITY I SITUATION ROOM \n '
         }
       },
       {
-        end: 2000,
-        start: 1000,
+        stopTimeMs: 2000,
+        startTimeMs: 1000,
         object: {
           text:
             'WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray CNW Political Reporter \n LIVE \n NAS -58.43 \n IONY ON CHAMPS-ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURITY It SITUATION ROOM \n '
         }
       },
       {
-        end: 3000,
-        start: 2000,
+        stopTimeMs: 3000,
+        startTimeMs: 2000,
         object: {
           text:
             'WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISINGN \n Sara Murray CNW Political Reporter \n LIVE \n NAS58.43 \n I CHAMPS-ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURITY IN HIGH-F sTUATION ROOM \n '
         }
       },
       {
-        end: 4000,
-        start: 3000,
+        stopTimeMs: 4000,
+        startTimeMs: 3000,
         object: {
           text:
             'THE WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray CW Political Reporter \n LIVE \n NAS58.43 \n ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURTY IN HIGH-PROFILE L SITUATION ROOM \n '
         }
       },
       {
-        end: 5000,
-        start: 4000,
+        stopTimeMs: 5000,
+        startTimeMs: 4000,
         object: {
           text:
             'THE WITH \n DEVELOPING NOW \n LIVE \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray \n MAYOR SAYS \n CNW Political Reporter \n NASS8.43 \n U.S. TIGHTENING SECURITY IN HIGH-PROFILE LOCATIONS SITUATION ROOM \n '
