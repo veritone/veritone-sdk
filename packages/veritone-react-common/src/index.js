9e23b33e5c3a87e8da8833fbee76a4151c5943a1
Alex Bu <abu@veritone.com>
2018-04-18 17:04:32 -0700

    refactor source dropdown menu to be reused

--- packages/veritone-react-common/src/index.js
+++ packages/veritone-react-common/src/index.js
@@ -41,6 +41,7 @@ export MediaInfoPanel from './components/MediaInfoPanel';
 
 export IngestionAdapters from './components/IngestionAdapters';
 export Scheduler from './components/Scheduler';
+export SourceDropdownMenu from './components/SourceManagement/SourceDropdownMenu';
 
 export SourceNullState from './components/SourceManagement/Nullstate';
 export SourceTileView from './components/SourceManagement/SourceTileView';
