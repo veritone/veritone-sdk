a266d2098d3f13851a0c034e556cf16b0e3d4c6e
Charlie Mantle <cmantle@veritone.com>
2018-05-17 10:30:04 -0700

    added UNSAFE prefix for componentWillReceiveProp instances

--- packages/veritone-react-common/src/components/FilePicker/FileList/FileListItem.js
+++ packages/veritone-react-common/src/components/FilePicker/FileList/FileListItem.js
@@ -33,12 +33,11 @@ class FileListItem extends Component {
     dataUrl: ''
   };
 
-  // eslint-disable-next-line react/sort-comp
   UNSAFE_componentWillMount() {
     this.readImageFile(this.props.file);
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProp(nextProps) {
     if (nextProps.file !== this.props.file) {
       this.readImageFile(nextProps.file);
     }
