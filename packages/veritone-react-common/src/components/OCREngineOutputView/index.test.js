10c8acad8ad9057287e43ee58efdfd3c52ab728c
Chris Pelletier <cpelletier@veritone.com>
2018-05-31 14:16:53 -0700

    Added a couple more tests for the OCREngineOutputView component

--- packages/veritone-react-common/src/components/OCREngineOutputView/index.test.js
+++ packages/veritone-react-common/src/components/OCREngineOutputView/index.test.js
@@ -14,16 +14,16 @@ const ocrAssets = [
       '23969da8-c532-46ae-b4cf-b002d44b31ce-82e4453e-b1a9-425f-8b5c-488915939bac',
     series: [
       {
-        end: 1000,
-        start: 0,
+        stopTimeMs: 1000,
+        startTimeMs: 0,
         object: {
           text:
             'WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray CNW Political Reporter \n LIVE \n NAS58.43 \n AONY ON CHAMPS-ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURITY I SITUATION ROOM \n '
         }
       },
       {
-        end: 2000,
-        start: 1000,
+        startTimeMs: 2000,
+        stopTimeMs: 1000,
         object: {
           text:
             'WITH \n DEVELOPING NOW \n TED CRUZ GAINING IN POLLS AND FUND-RAISING CNN \n Sara Murray CNW Political Reporter \n LIVE \n NAS -58.43 \n IONY ON CHAMPS-ELYSEES, MAYOR SAYS \n U.S. TIGHTENING SECURITY It SITUATION ROOM \n '
@@ -52,5 +52,20 @@ describe('OCREngineOutputView', () => {
     expect(wrapper.find('.ocrContainer')).toHaveLength(
       ocrAssets[0].series.length
     );
+  });
+
+  it('adds props.className to the root', function() {
+    const wrapper = mount(<OCREngineOutputView data={ocrAssets} className="ocr-class"/>);
+    expect(wrapper.hasClass('ocr-class')).toBe(true);
+  });
+
+  it('calls props.onOcrClicked when user clicks an orc pill', () => {
+    const handler = jest.fn();
+    const wrapper = mount(<OCREngineOutputView data={ocrAssets} onOcrClicked={handler} />);
+    wrapper
+      .find('.ocrContainer')
+      .first()
+      .simulate('click');
+    expect(handler).toHaveBeenCalledWith(0, 1000);
   });
 });
