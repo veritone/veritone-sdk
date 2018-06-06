2cfdf2d4de4d03e43fa6da759b273dbb501fd46c
AlexO <oiaroshchuk@veritone.com>
2018-06-06 10:58:41 -0700

    Engine outputs fail when no data comes in from the GraphQL - reducer should process only response data that have jsondata

--- packages/veritone-widgets/src/redux/modules/mediaDetails/index.js
+++ packages/veritone-widgets/src/redux/modules/mediaDetails/index.js
@@ -8,7 +8,8 @@ import {
   map,
   values,
   uniqBy,
-  keyBy
+  keyBy,
+  isMatch
 } from 'lodash';
 import { helpers } from 'veritone-redux-common';
 const { createReducer } = helpers;
@@ -216,10 +217,10 @@ export default createReducer(defaultState, {
 
     forEach(resultsGroupedByEngineId, (results, engineId) => {
       // process only results with jsondata
-      results = results.filter(result => !!result && !!result.jsondata);
+      const resultsWithData = results.filter(result => !!result && !!result.jsondata);
       if (!previousResultsByEngineId[engineId]) {
         // Data hasn't been retrieved for this engineId yet
-        previousResultsByEngineId[engineId] = map(results, 'jsondata');
+        previousResultsByEngineId[engineId] = map(resultsWithData, 'jsondata');
       } else {
         // New results need to be merged with previously fetched results
         let insertionIndex = findIndex(previousResultsByEngineId[engineId], {
@@ -231,18 +232,19 @@ export default createReducer(defaultState, {
         // TODO: fitler out any duplicate data that overflows time chunks.
         previousResultsByEngineId[engineId] = [
           ...previousResultsByEngineId[engineId].slice(0, insertionIndex),
-          ...map(results, 'jsondata'),
+          ...map(resultsWithData, 'jsondata'),
           ...previousResultsByEngineId[engineId].slice(insertionIndex + 1)
         ];
       }
+      const fetchingOffsetRequest = {
+        startOffsetMs: startOffsetMs,
+        stopOffsetMs: stopOffsetMs,
+        status: 'FETCHING'
+      };
       engineResultRequestsById[engineId] = engineResultRequestsById[
         engineId
         ].map(request => {
-        if (
-          request.startOffsetMs === startOffsetMs &&
-          request.stopOffsetMs === stopOffsetMs &&
-          request.status === 'FETCHING'
-        ) {
+        if (isMatch(request, fetchingOffsetRequest)) {
           return {
             ...request,
             status: 'SUCCESS'
