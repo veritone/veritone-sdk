a5df3a2872cf13ed34642567d10145e98fe8feb8
Andrew Tung <atungw@gmail.com>
2018-06-06 17:33:08 -0700

    Fix tests

--- packages/veritone-react-common/src/components/ExpandableInputField/index.test.js
+++ packages/veritone-react-common/src/components/ExpandableInputField/index.test.js
@@ -8,22 +8,32 @@ import TextField from '@material-ui/core/TextField';
 import ExpandableInputField from './';
 
 describe('ExpandableInputField', function() {
-  const onSearch = jest.fn();
-  const onFocus = jest.fn();
-  const onBlur = jest.fn();
-  const onChange = jest.fn();
-
-  const expandableInputField = mount(
-    <ExpandableInputField
-      onSearch={onSearch}
-      icon={<SearchIcon />}
-      onChange={onChange}
-    />
-  );
-
   const enterKey = { key: 'Enter', keyCode: 13, which: 13 };
   const searchValue = 'hello world';
   const changeValue = { target: { value: searchValue } };
+
+  let onSearch;
+  let onFocus;
+  let onBlur;
+  let onChange;
+  let expandableInputField;
+
+  beforeEach(function() {
+    onSearch = jest.fn();
+    onFocus = jest.fn();
+    onBlur = jest.fn();
+    onChange = jest.fn();
+
+    expandableInputField = mount(
+      <ExpandableInputField
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onSearch={onSearch}
+        icon={<SearchIcon />}
+        onChange={onChange}
+      />
+    );
+  });
 
   it('should exist', function() {
     expect(expandableInputField).toHaveLength(1);
@@ -38,28 +48,36 @@ describe('ExpandableInputField', function() {
     expect(expandableInputField.find(TextField)).toHaveLength(1);
   });
 
-  it('called onFocus after it was clicked', function() {
-    expect(onFocus.mock.calls.length).toBe(0);
+  it('calls onFocus when the input is focused', function() {
+    expandableInputField.find(SearchIcon).simulate('click');
+    expandableInputField.find('input').simulate('focus');
+    expect(onFocus.mock.calls.length).toBe(1);
   });
 
   it("renders a reset button after it's been clicked", function() {
+    expandableInputField.find(SearchIcon).simulate('click');
     expect(expandableInputField.find(CloseIcon)).toHaveLength(1);
   });
 
   it('calls onChange with the new input value', function() {
+    expandableInputField.find(SearchIcon).simulate('click');
     expandableInputField.find('input').simulate('change', changeValue);
     expect(onChange.mock.calls.length).toBe(1);
     expect(onChange.mock.calls[0][0].target.value).toBe(searchValue);
   });
 
   it("calls onSearch when the enter key is pressed", function() {
+    expandableInputField.find(SearchIcon).simulate('click');
+    expandableInputField.find('input').simulate('change', changeValue);
     expandableInputField.find('input').simulate('keypress', enterKey);
     expect(onSearch.mock.calls.length).toBe(1);
   });
 
   it('calls onBlur after focus is lost', function() {
+    expandableInputField.find(SearchIcon).simulate('click');
+    expandableInputField.find('input').simulate('change', changeValue);
     expandableInputField.find('input').simulate('blur');
-    expect(onBlur.mock.calls.length).toBe(0);
+    expect(onBlur.mock.calls.length).toBe(1);
   });
 
   it('resets the search value when the reset button is clicked', function() {
