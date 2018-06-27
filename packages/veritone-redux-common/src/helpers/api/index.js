47b6055d5e7790182fe64565b5d7fb8eee865052
Andrew Tung <atungw@gmail.com>
2018-06-27 14:20:01 -0700

    GraphQL helper for returning promises from async calls.

--- packages/veritone-redux-common/src/helpers/api/index.js
+++ packages/veritone-redux-common/src/helpers/api/index.js
@@ -36,3 +36,77 @@ export function callGraphQLApi({
     }
   }).then(r => r.json());
 }
+
+export async function callGraphQL({
+  actionTypes: [requestType, successType, failureType],
+  query,
+  variables,
+  operationName,
+  bailout = constant(false),
+  dispatch,
+  getState
+}) {
+  if (!isFunction(dispatch) || !isFunction(getState)) {
+    throw new Error('callGraphQLApi requires dispatch and getState functions')
+  }
+
+  const state = getState();
+  const config = getConfig(state);
+  const endpoint = `${config.apiRoot}/${config.graphQLEndpoint}`;
+  const token = selectOAuthToken(state) || selectSessionToken(state);
+
+  const shouldBail = bailout(state);
+  if (shouldBail) {
+    return;
+  }
+
+  dispatch({ type: requestType, meta: { variables, operationName, query } });
+
+  let response;
+  try {
+    response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({
+        query,
+        variables,
+        operationName
+      }),
+      headers: {
+        Authorization: token ? `bearer ${token}` : null,
+        'Content-Type': 'application/json'
+      }
+    }).then(r => r.json());
+  } catch (e) {
+    dispatch({
+      type: failureType,
+      error: true,
+      payload: e,
+      meta: { variables, operationName, query }
+    });
+
+    let error = new Error('API call failed');
+    error.errors = [e];
+    return error;
+  }
+
+  if (response.errors && response.errors.length) {
+    dispatch({
+      type: failureType,
+      error: true,
+      payload: response.errors,
+      meta: { variables, operationName, query }
+    });
+
+    let error = new Error('API response included errors');
+    error.errors = response.errors;
+    return error;
+  }
+
+  dispatch({
+    type: successType,
+    payload: response.data,
+    meta: { variables, operationName, query }
+  });
+
+  return response.data;
+}
