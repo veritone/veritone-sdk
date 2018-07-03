241fc3f10b61a0bd998ca0217b87b42d746aac5f
AlexO <oiaroshchuk@veritone.com>
2018-07-03 15:20:13 -0700

    Do not render '0' on the place of language selector

--- packages/veritone-widgets/src/redux/modules/mediaDetails/saga.js
+++ packages/veritone-widgets/src/redux/modules/mediaDetails/saga.js
@@ -933,6 +933,7 @@ function* createTranscriptBulkEditAssetSaga(
         payload: {
           originalTranscriptAssetId: "${originalTranscriptAssetId}",
           temporaryBulkEditAssetId: "${bulkTextAssetId}",
+          originalEngineId: "${selectedEngineId}",
           saveTtmlToVtnStandard: true
         }
       },
