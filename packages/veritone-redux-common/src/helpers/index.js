728c24f6a6e95c0d0447c5a98c90dab0cfc814c3
Mitch Robb <mrobb@veritone.com>
2018-07-18 12:27:49 -0700

    Add callGraphQLApi helper, for use within a redux-thunk

--- packages/veritone-redux-common/src/helpers/index.js
+++ packages/veritone-redux-common/src/helpers/index.js
@@ -4,3 +4,4 @@ export { default as paginate } from './redux/paginate';
 export * as paginateModule from './redux/paginate';
 
 export { default as fetchGraphQLApi } from './api/fetchGraphQLApi';
+export { default as callGraphQLApi } from './api/callGraphQLApi';
