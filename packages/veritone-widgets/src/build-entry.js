19bb10cba06c6fa64a72272fe53748011e6c077b
Mitch Robb <mrobb@veritone.com>
2018-07-30 13:49:29 -0700

    export notifications and filepicker reducers/sagas for use in smart components w/o VeritoneApp wrapper

--- packages/veritone-widgets/src/build-entry.js
+++ packages/veritone-widgets/src/build-entry.js
@@ -7,12 +7,15 @@ export OAuthLoginButton, {
   OAuthLoginButtonWidget
 } from './widgets/OAuthLoginButton';
 export FilePicker, { FilePickerWidget } from './widgets/FilePicker';
+export filePickerReducer, * as filePickerModule from './redux/modules/filePicker';
+export filePickerSaga from './redux/modules/filePicker/filePickerSaga';
 export { EngineSelectionWidget } from './widgets/EngineSelection';
 export { TableWidget } from './widgets/Table';
 export {
   GlobalNotificationDialog,
   GlobalSnackBar
 } from './widgets/Notifications';
+export notificationsReducer, * as notificationsModule from './redux/modules/notifications';
 export ContentTemplate from './widgets/ContentTemplate';
 export ContentTemplateForm from './widgets/ContentTemplateForm';
 export SourceManagementForm from './widgets/SourceManagementForm';
