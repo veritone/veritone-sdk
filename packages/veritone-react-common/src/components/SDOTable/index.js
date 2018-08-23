45f2ac1b708063374691cd4f7e8c78dc6b27bddf
AlexO <oiaroshchuk@veritone.com>
2018-08-23 12:18:20 -0700

    Make SdoTable able to render schemas with nested object properties - flatten all to one level and build columns. Surface onClick and focusedRow to allow users react to on row/cell click and highlight specific rows.

--- packages/veritone-react-common/src/components/SDOTable/index.js
+++ packages/veritone-react-common/src/components/SDOTable/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { arrayOf, object, objectOf, bool } from 'prop-types';
+import { arrayOf, object, objectOf, bool, number, func } from 'prop-types';
 import { Table, PaginatedTable, Column } from 'components/DataTable';
 import { map, startCase, partial, omit } from 'lodash';
 import DotDotDot from 'react-dotdotdot';
@@ -9,7 +9,10 @@ export default class SDOTable extends React.Component {
   static propTypes = {
     data: arrayOf(object).isRequired,
     schema: objectOf(object).isRequired,
-    paginate: bool
+    paginate: bool,
+    onCellClick: func,
+    focusedRow: number,
+    renderFocusedRowDetails: func
   };
 
   static defaultProps = {
@@ -37,11 +40,45 @@ export default class SDOTable extends React.Component {
     return <DotDotDot clamp={2}>{formattedData}</DotDotDot>;
   };
 
+  buildColumnsForObjectTypeProperty = (propDetails, propKey) => {
+    const allProps = {};
+    this.flattenAllObjectTypeProperties(allProps, propDetails, propKey);
+    return map(allProps, (nestedPropDetails, nestedProp) => (
+      <Column
+        dataKey={nestedProp}
+        header={nestedPropDetails.title || startCase(nestedProp)}
+        cellRenderer={partial(
+          this.renderCell,
+          partial.placeholder,
+          nestedPropDetails.type
+        )}
+        key={nestedPropDetails.$id}
+      />
+    ));
+  };
+
+  flattenAllObjectTypeProperties = (allProps, propDetails, propKey) => {
+    for (let nestedPropKey in propDetails.properties) {
+      if (propDetails.properties[nestedPropKey].type === 'object') {
+        this.flattenAllObjectTypeProperties(
+          allProps,
+          propDetails.properties[nestedPropKey],
+          `${propKey}.${nestedPropKey}`
+        );
+      } else {
+        allProps[`${propKey}.${nestedPropKey}`] =
+          propDetails.properties[nestedPropKey];
+      }
+    }
+  };
+
   render() {
     const tableProps = omit(this.props, ['data', 'schema', 'paginate']);
-
     const TableComponent = this.props.paginate ? PaginatedTable : Table;
     const tableColumns = map(this.props.schema, (propDetails, prop) => {
+      if (propDetails.type === 'object') {
+        return this.buildColumnsForObjectTypeProperty(propDetails, prop);
+      }
       return (
         <Column
           dataKey={prop}
@@ -61,6 +98,8 @@ export default class SDOTable extends React.Component {
         {...tableProps}
         rowGetter={this.getRowData}
         rowCount={this.props.data.length}
+        focusedRow={this.props.focusedRow}
+        onCellClick={this.props.onCellClick}
       >
         {tableColumns}
       </TableComponent>
