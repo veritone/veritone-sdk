3873dd62e9fcbe585043a99d90259640af32a805
Mitch Robb <mrobb@veritone.com>
2018-09-05 13:16:39 -0700

    shore up tests to make sure exports are correct

--- packages/veritone-redux-common/src/helpers/redux/index.test.js
+++ packages/veritone-redux-common/src/helpers/redux/index.test.js
@@ -1,13 +1,22 @@
-import * as reduxHelpers from './'
+import { isEmpty, isFunction } from 'lodash';
+import * as reduxHelpers from './';
 
 describe('helpers/redux', function() {
   it('exports the right stuff', function() {
-    expect(Object.keys(reduxHelpers)).toEqual([
+    const expectedKeys = [
       'promiseMiddleware',
       'createReducer',
       'reduceReducers',
       'handleApiCall',
       'fetchingStatus'
-    ])
+    ];
+
+    expect(Object.keys(reduxHelpers)).toEqual(expectedKeys);
+
+    expectedKeys.forEach(k => {
+      const exported = reduxHelpers[k];
+      const isEmptyExport = isEmpty(exported) && !isFunction(exported);
+      expect(isEmptyExport).toEqual(false);
+    });
   });
 });
