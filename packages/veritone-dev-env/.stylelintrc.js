29efb40edfbf49409413e0122cc4de34e57df184
Mitch Robb <mrobb@veritone.com>
2018-09-06 11:54:14 -0700

    add stylelint-config-prettier; add style linting to veritone-wigets; fix existing lint errors

--- packages/veritone-dev-env/.stylelintrc.js
+++ packages/veritone-dev-env/.stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {
     'selector-pseudo-class-no-unknown': [
       true,
