75ae996137fd887df47405f91fdcb6edeaa391a4
Andrew Tung <atung@veritone.com>
2018-09-24 01:13:18 -0700

    Autocomplete values

---
+++ packages/veritone-react-common/src/components/StructuredDataModal/SearchAttribute/autocomplete.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import Downshift from 'downshift';
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import { List, ListItem, ListItemText } from '@material-ui/core';
+
+import styles from '../styles.scss';
+
+export default class StringValuePicker extends React.Component {
+  render() {
+    return (
+      <Downshift
+        onSelect={this.props.onSelect}
+        itemToString={item => (item ? item.value : '')}
+        isOpen={this.props.open}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem
+        }) => (
+          <div className={styles.autocomplete_container}>
+            <TextField
+              fullWidth
+              autoFocus
+              margin={'none'}
+              {...getInputProps({
+                onChange: this.props.onChange,
+                value: this.props.value,
+                onBlur: this.props.onBlurAutocomplete,
+                onFocus: this.props.onFocusAutocomplete
+              })}
+            />
+            <Paper>
+              {isOpen &&
+                this.props.items && (
+                  <ListItem style={{ borderBottom: '1px dashed #ccc' }}>
+                    <ListItemText secondary={'Suggestions'} />
+                  </ListItem>
+                )}
+              {isOpen && this.props.loading ? (
+                <ListItem>{this.props.loader}</ListItem>
+              ) : null}
+              {isOpen && !this.props.loading && this.props.items
+                ? this.props.items.map((item, index) => (
+                    <ListItem
+                      {...getItemProps({
+                        key: item,
+                        index,
+                        item,
+                        style: {
+                          backgroundColor:
+                            highlightedIndex === index ? '#eeeeee' : null,
+                          fontWeight: selectedItem === item ? 'bold' : 'normal'
+                        }
+                      })}
+                    >
+                      <ListItemText primary={item} />
+                    </ListItem>
+                  ))
+                : null}
+              {isOpen && !this.props.loading && (!this.props.items || this.props.items.length === 0)
+                ? this.props.items.map((item, index) => (
+                    <ListItem>
+                      <ListItemText secondary="None" />
+                    </ListItem>
+                  ))
+                : null}
+            </Paper>
+          </div>
+        )}
+      </Downshift>
+    );
+  }
+}
--- packages/veritone-react-common/src/components/StructuredDataModal/SearchAttribute/fetchAutocomplete.js
+++ packages/veritone-react-common/src/components/StructuredDataModal/SearchAttribute/fetchAutocomplete.js
@@ -49,7 +49,7 @@ const fetchAutocomplete = function fetchAutocomplete(api, auth, value) {
     body: JSON.stringify(
       {
         "index": ["schema"],
-      "fields": ["schemaFields.name"],
+      "fields": ['schemaFields.name'],
         "text": value,
         "returnContext": true
     })
@@ -64,7 +64,34 @@ const fetchAutocomplete = function fetchAutocomplete(api, auth, value) {
   );
 }
 
+const fetchAutocompleteValues = function fetchAutocomplete(api, auth, value, field) {
+  return fetch(`${api}v1/search/search/autocomplete`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + auth
+    },
+    body: JSON.stringify(
+      {
+        "index": ["mine", "global"],
+      "fields": [field],
+        "text": value,
+        "returnContext": true
+    })
+  }).then(
+    response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        return false;
+      }
+    }
+  );
+}
+
+
 export {
   fetchAutocomplete,
+  fetchAutocompleteValues,
   fetchSDOSchema
 }
