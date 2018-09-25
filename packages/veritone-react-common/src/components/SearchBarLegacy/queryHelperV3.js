f2363831e459c6de2b3f3ecf0e34bc1b9ba5383d
Mitch Robb <mrobb@veritone.com>
2018-09-25 14:22:41 -0700

    fix linter errors in queryHelperV3

--- packages/veritone-react-common/src/components/SearchBarLegacy/queryHelperV3.js
+++ packages/veritone-react-common/src/components/SearchBarLegacy/queryHelperV3.js
@@ -1,44 +1,43 @@
 // The purpose of this helper is to provide functions to easily translate/construct V3 query conditions
-const _ = require('lodash');
+import { endsWith, startsWith, includes, isString, isUndefined } from 'lodash';
 
 // TODO: For all exported functions, we need to ensure the parameter passed into
-// 	each respective function is correct.
+// each respective function is correct.
 export function generateTranscriptionConditions(queryParams) {
   return addQueryStringFilters(queryParams);
-};
+}
 
 export function generateSentimentConditions(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateAudioCondition(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateFaceCondition(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateObjectCondition(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateGeolocationCondition(queryParams) {
   return buildGeoDistanceOperator(queryParams);
-};
+}
 
 export function generateStationPayoutCondition(queryParams) {
   // TODO
-  return;
-};
+}
 
 export function generateOcrCondition(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateLogoCondition(queryParams) {
   return addMetadataFilters(queryParams);
-};
+}
 
 export function generateTagCondition(queryParams) {
   return buildTagQuery(queryParams);
@@ -46,9 +45,7 @@ export function generateTagCondition(queryParams) {
 
 export function generateTimeCondition(queryParams) {
   // TODO
-  return;
 }
-
 
 // Private functions
 function addQueryStringFilters(queryParams) {
@@ -56,18 +53,32 @@ function addQueryStringFilters(queryParams) {
     return [];
   }
 
-  var fields = queryParams.fullTextFields ? queryParams.fullTextFields.split(',') : ['transcript'];
-  var filters = [];
+  const fields = queryParams.fullTextFields
+    ? queryParams.fullTextFields.split(',')
+    : ['transcript'];
+  let filters = [];
   fields.forEach(function addQueryFilter(field) {
     if (field === 'transcript') {
       // langugage query`
-      if (queryParams.language && queryParams.language.length && queryParams.language.toLowerCase().indexOf('en') !== 0) {
+      if (
+        queryParams.language &&
+        queryParams.language.length &&
+        startsWith(queryParams.language.toLowerCase(), 'en')
+      ) {
         filters.push(buildTranslatedQuery(queryParams.language, queryParams.q));
       } else {
         if (queryParams.qlike) {
-          filters.push(buildSimilarQuery('transcript.transcript', queryParams.qlike, queryParams.q));
+          filters.push(
+            buildSimilarQuery(
+              'transcript.transcript',
+              queryParams.qlike,
+              queryParams.q
+            )
+          );
         } else {
-          filters.push(buildStringQuery('transcript.transcript', queryParams.q));
+          filters.push(
+            buildStringQuery('transcript.transcript', queryParams.q)
+          );
         }
       }
     } else {
@@ -87,39 +98,73 @@ function buildSimilarQuery(field, similarContext, query) {
   };
 }
 
-function buildStringQuery(field, queryExpression, highlight, keepCasing, analyzer) {
+function buildStringQuery(
+  field,
+  queryExpression,
+  highlight,
+  keepCasing,
+  analyzer
+) {
   if (queryExpression.indexOf(',') > 0) {
-    var conditions = queryExpression.split(',').map(function processOrExpression(subExpression) {
-      return buildStringQueryAnd(field, subExpression, highlight, keepCasing, analyzer);
-    });
+    const conditions = queryExpression
+      .split(',')
+      .map(function processOrExpression(subExpression) {
+        return buildStringQueryAnd(
+          field,
+          subExpression,
+          highlight,
+          keepCasing,
+          analyzer
+        );
+      });
     return buildOrOperator(conditions.filter(x => x));
   }
 
-  return buildStringQueryAnd(field, queryExpression, highlight, keepCasing, analyzer);
+  return buildStringQueryAnd(
+    field,
+    queryExpression,
+    highlight,
+    keepCasing,
+    analyzer
+  );
 }
 
-function buildStringQueryAnd(field, queryExpression, highlight, keepCasing, analyzer) {
+function buildStringQueryAnd(
+  field,
+  queryExpression,
+  highlight,
+  keepCasing,
+  analyzer
+) {
   if (!field || !queryExpression) {
     return null;
   }
   // wrap simple queries in quotes
-  if (queryExpression.indexOf(' ') < 0) {
-    return buildQueryStringOperator(field, keepCasing ? queryExpression : queryExpression.toLowerCase(), highlight, analyzer);
+  if (!includes(queryExpression, ' ')) {
+    return buildQueryStringOperator(
+      field,
+      keepCasing ? queryExpression : queryExpression.toLowerCase(),
+      highlight,
+      analyzer
+    );
   }
 
-  // deffer to query_string to handle the negation
-  var queryText = queryExpression.toLowerCase().replace('_not_', 'NOT');
+  // defer to query_string to handle the negation
+  const queryText = queryExpression.toLowerCase().replace('_not_', 'NOT');
 
   // if it is a simple query, i.e. no within
-  if (queryExpression.indexOf(' w/') < 0) {
+  if (!includes(queryExpression, ' w/')) {
     return buildQueryStringOperator(field, queryText, highlight, analyzer);
   }
 
   // build 2 queries - all words query and just and AND-query of all spanning expressions
   // all words query
-  var textSearchQuery = buildQueryStringOperator(field, queryText.replace(/\s+w\/(\d+|[ps])\s+/g, ' '));
+  const textSearchQuery = buildQueryStringOperator(
+    field,
+    queryText.replace(/\s+w\/(\d+|[ps])\s+/g, ' ')
+  );
   // spanning expressions
-  var spanQueries = buildSpanStringQuery(field, queryText);
+  let spanQueries = buildSpanStringQuery(field, queryText);
 
   // add to the same AND expression as the span expressions
   spanQueries.push(textSearchQuery);
@@ -128,7 +173,7 @@ function buildStringQueryAnd(field, queryExpression, highlight, keepCasing, anal
 }
 
 function buildQueryStringOperator(field, queryText, highlight, analyzer) {
-  var op = {
+  let op = {
     operator: 'query_string',
     field: field,
     value: queryText
@@ -144,16 +189,19 @@ function buildQueryStringOperator(field, queryText, highlight, analyzer) {
 
 function buildSpanStringQuery(field, queryString) {
   // cleanup redundant whitespace
-  var queryText = queryString.replace(/\s{2+}/g, ' ');
-  var start, end, distance;
-  var spanConditions = [];
-  var proximityOperatorPos = queryText.indexOf(' w/', start),
+  const queryText = queryString.replace(/\s{2+}/g, ' ');
+  let start, end, distance;
+  let spanConditions = [];
+  let proximityOperatorPos = queryText.indexOf(' w/'),
     proximityOperatorEnd;
 
-  while (proximityOperatorPos > 1 && proximityOperatorPos < queryText.length - 5) {
+  while (
+    proximityOperatorPos > 1 &&
+    proximityOperatorPos < queryText.length - 5
+  ) {
     // parse the span distance
     proximityOperatorEnd = queryText.indexOf(' ', proximityOperatorPos + 1);
-    var val = queryText.charAt(proximityOperatorPos + 3);
+    const val = queryText.charAt(proximityOperatorPos + 3);
     switch (val) {
       case 'p':
         distance = 75;
@@ -162,7 +210,9 @@ function buildSpanStringQuery(field, queryString) {
         distance = 30;
         break;
       default:
-        distance = parseInt(queryText.substring(proximityOperatorPos + 3, proximityOperatorEnd));
+        distance = parseInt(
+          queryText.substring(proximityOperatorPos + 3, proximityOperatorEnd)
+        );
         break;
     }
 
@@ -192,17 +242,16 @@ function buildSpanStringQuery(field, queryString) {
       }
     }
 
-    spanConditions.push(
-      {
-        operator: 'word_proximity',
-        field: field,
-        inOrder: false,
-        distance: distance,
-        values: [
-          queryText.substring(start, proximityOperatorPos),
-          queryText.substring(proximityOperatorEnd + 1, end)
-        ]
-      });
+    spanConditions.push({
+      operator: 'word_proximity',
+      field: field,
+      inOrder: false,
+      distance: distance,
+      values: [
+        queryText.substring(start, proximityOperatorPos),
+        queryText.substring(proximityOperatorEnd + 1, end)
+      ]
+    });
 
     // find the next span operand
     proximityOperatorPos = queryText.indexOf(' w/', proximityOperatorPos + 1);
@@ -211,8 +260,8 @@ function buildSpanStringQuery(field, queryString) {
 }
 
 function buildTermsOperator(field, values) {
-  var cleanedValues = values.map(function stripQuoting(val) {
-    if (val.indexOf('"') === 0) {
+  const cleanedValues = values.map(function stripQuoting(val) {
+    if (startsWith(val, '"')) {
       return val.substring(1, val.length - 1);
     } else {
       return val;
@@ -248,23 +297,26 @@ function buildOrOperator(conditions) {
 }
 
 function buildTranslatedQuery(language, q) {
-  if (typeof language !== 'string' || typeof q !== 'string') {
+  if (!isString(language) || !isString(q)) {
     return [];
   }
-  return buildAndOperator([{
-    field: 'transcript.transcript',
-    operator: 'query_string',
-    value: q
-  }, {
-    field: 'transcript.language',
-    operator: 'term',
-    value: language
-  }]);
+  return buildAndOperator([
+    {
+      field: 'transcript.transcript',
+      operator: 'query_string',
+      value: q
+    },
+    {
+      field: 'transcript.language',
+      operator: 'term',
+      value: language
+    }
+  ]);
 }
 
 function overrideSentimentQuery(queryParam, queryParams) {
   if (queryParam === 'sentiment-veritone.series.score') {
-    var val = queryParams[queryParam];
+    let val = queryParams[queryParam];
     if (val === '0' || val === 0) {
       val = 'min:0.5';
     } else if (val === '1' || val === 1) {
@@ -274,15 +326,15 @@ function overrideSentimentQuery(queryParam, queryParams) {
   }
 }
 
-function addMetadataFilters(request, queryParams, callback) {
+function addMetadataFilters(request, queryParams) {
   // collapse same field queries so we can build correct query_object operator
-  var qParams = {};
-  var filters = [];
-  for (var queryParam in queryParams) {
-    if (queryParams.hasOwnProperty(queryParam) && (queryParam.indexOf('.') > 0)) {
+  let qParams = {};
+  let filters = [];
+  for (const queryParam in queryParams) {
+    if (queryParams.hasOwnProperty(queryParam) && queryParam.indexOf('.') > 0) {
       overrideSentimentQuery(queryParam, queryParams);
-      var typeKey = queryParam;
-      var sIndex = queryParam.indexOf('.series');
+      let typeKey = queryParam;
+      const sIndex = queryParam.indexOf('.series');
       if (sIndex > 0) {
         typeKey = queryParam.substr(0, sIndex + '.series'.length);
       }
@@ -296,15 +348,27 @@ function addMetadataFilters(request, queryParams, callback) {
     }
   }
 
-  var fullTextFields = [];
-  for (var fieldKey in qParams) {
+  let fullTextFields = [];
+  for (const fieldKey in qParams) {
     if (qParams.hasOwnProperty(fieldKey)) {
-      var qOperator = qParams[fieldKey];
+      const qOperator = qParams[fieldKey];
       if (qOperator.length === 1) {
-        filters.push(buildMetadataQuery(qOperator[0].param, qOperator[0].expression, fullTextFields));
+        filters.push(
+          buildMetadataQuery(
+            qOperator[0].param,
+            qOperator[0].expression,
+            fullTextFields
+          )
+        );
       } else {
-        for (var i = 0, len = qOperator.length; i < len; i++) {
-          filters.push(buildMetadataQuery(qOperator[i].param.substr(fieldKey.length + 1), qOperator[i].expression, fullTextFields));
+        for (let i = 0, len = qOperator.length; i < len; i++) {
+          filters.push(
+            buildMetadataQuery(
+              qOperator[i].param.substr(fieldKey.length + 1),
+              qOperator[i].expression,
+              fullTextFields
+            )
+          );
         }
       }
     }
@@ -315,27 +379,38 @@ function addMetadataFilters(request, queryParams, callback) {
   // doesn't have access to the root request object....
   // TODO: figure out how to remedy this
   // if (fullTextFields.length) {
-  // 	request._fulltext = fullTextFields;
+  //   request._fulltext = fullTextFields;
   // }
   return filters;
 }
 
-
 function buildMetadataQuery(field, valueExpression, fullTextFields) {
-  if (_.endsWith(field, '.fulltext')) {
+  if (endsWith(field, '.fulltext')) {
     fullTextFields.push(field.substring(0, field.indexOf('.')));
     return buildOrOperator([
-      buildStringQuery(field.substring(0, field.lastIndexOf('.fulltext')), valueExpression, false, false, 'none'),
+      buildStringQuery(
+        field.substring(0, field.lastIndexOf('.fulltext')),
+        valueExpression,
+        false,
+        false,
+        'none'
+      ),
       buildStringQuery(field, valueExpression, false, false, 'none')
     ]);
   }
 
   // field=[a:b,c:d] - build multi-range query
-  if (_.startsWith(valueExpression, '[') && _.endsWith(valueExpression, ']') && valueExpression.indexOf(':') > 0) {
-    var parts = valueExpression.substring(1, valueExpression.length - 1).split(',');
-    var conditions = [];
+  if (
+    startsWith(valueExpression, '[') &&
+    endsWith(valueExpression, ']') &&
+    valueExpression.indexOf(':') > 0
+  ) {
+    const parts = valueExpression
+      .substring(1, valueExpression.length - 1)
+      .split(',');
+    let conditions = [];
     parts.forEach(function eachRange(r) {
-      var range = buildRangeQuery(field, r);
+      const range = buildRangeQuery(field, r);
       if (range) {
         conditions.push(range);
       }
@@ -352,36 +427,35 @@ function buildMetadataQuery(field, valueExpression, fullTextFields) {
 
   // field = x;;y;;z - build AND query on the time-slice level
   if (valueExpression.indexOf(';;') > 0) {
-    var andOperator = buildAndOperator(valueExpression
-      .split(';;')
-      .map(function buildQueryTerm(val) {
+    let andOperator = buildAndOperator(
+      valueExpression.split(';;').map(function buildQueryTerm(val) {
         //strip quotation - old api ignores them and a lot of clients still send them
-        if (val.indexOf('"') === 0) {
+        if (startsWith(val, '"')) {
           return buildTermOperator(field, val.substring(1, val.length - 1));
         } else {
           return buildTermOperator(field, val);
         }
-      }));
+      })
+    );
     andOperator.strictMode = 'disable';
     return andOperator;
   }
 
   // field=x;y;z - build AND query
   if (valueExpression.indexOf(';') > 0) {
-    return valueExpression.split(';').map(
-      function buildQueryTerm(val) {
-        //strip quotation - old api ignores them and a lot of clients still send them
-        if (val.indexOf('"') === 0) {
-          return buildTermOperator(field, val.substring(1, val.length - 1));
-        } else {
-          return buildTermOperator(field, val);
-        }
-      });
+    return valueExpression.split(';').map(function buildQueryTerm(val) {
+      //strip quotation - old api ignores them and a lot of clients still send them
+      if (startsWith(val, '"')) {
+        return buildTermOperator(field, val.substring(1, val.length - 1));
+      } else {
+        return buildTermOperator(field, val);
+      }
+    });
   }
 
   // field=N:N - build range query
   if (valueExpression.indexOf(':') > 0) {
-    var rangeOperator = buildRangeQuery(field, valueExpression);
+    const rangeOperator = buildRangeQuery(field, valueExpression);
     if (rangeOperator) {
       return rangeOperator;
     }
@@ -396,13 +470,16 @@ function buildMetadataQuery(field, valueExpression, fullTextFields) {
   }
 
   // if the expression contains wildcard - build a query_string for the value
-  if (valueExpression.indexOf('*') >= 0 && valueExpression.indexOf('"') !== 0) {
+  if (includes(valueExpression, '*') && !startsWith(valueExpression, '"')) {
     return buildQueryStringOperator(field, valueExpression);
   }
 
   //strip quotation - old api ignores them and a lot of clients still send them
-  if (valueExpression.indexOf('"') === 0) {
-    return buildTermOperator(field, valueExpression.substring(1, valueExpression.length - 1));
+  if (startsWith(valueExpression, '"')) {
+    return buildTermOperator(
+      field,
+      valueExpression.substring(1, valueExpression.length - 1)
+    );
   }
 
   // else build exact match query
@@ -411,18 +488,18 @@ function buildMetadataQuery(field, valueExpression, fullTextFields) {
 
 function buildRangeQuery(field, queryExpression) {
   if (queryExpression.indexOf(':') > 0) {
-    var parts = queryExpression.split(':');
+    const parts = queryExpression.split(':');
 
     if (parts.length === 4 && parts[0] === 'distance') {
       return buildGeoDistanceOperator(field, parts);
     }
 
-    var rangeOperator = {
+    let rangeOperator = {
       operator: 'range',
       field: field
     };
 
-    var validNumberExpression = true;
+    let validNumberExpression = true;
     if (parts[0] && parts[0].toLowerCase() !== 'min') {
       rangeOperator.gte = parts[0];
       validNumberExpression = !isNaN(parts[0]);
@@ -439,23 +516,30 @@ function buildRangeQuery(field, queryExpression) {
 }
 
 function buildGeoDistanceOperator(queryParams) {
-  if (!queryParams || typeof queryParams.longitude === 'undefined' || typeof queryParams.latitude === 'undefined' || typeof queryParams.distance === 'undefined') {
+  if (
+    !queryParams ||
+    isUndefined(queryParams.longitude) ||
+    isUndefined(queryParams.latitude) ||
+    isUndefined(queryParams.distance)
+  ) {
     return [];
   }
-  var distance = /([\d\.]+)([a-zA-Z]*)/.exec(queryParams.distance) || [];
-  return [{
-    operator: 'geo_distance',
-    field: 'geolocation.series',
-    latitude: parseFloat(queryParams.latitude) || 0,
-    longitude: parseFloat(queryParams.longitude) || 0,
-    distance: parseFloat(queryParams.distance) || 0,
-    units: queryParams.unit || 'mi'
-  }];
+  // var distance = /([\d\.]+)([a-zA-Z]*)/.exec(queryParams.distance) || [];
+  return [
+    {
+      operator: 'geo_distance',
+      field: 'geolocation.series',
+      latitude: parseFloat(queryParams.latitude) || 0,
+      longitude: parseFloat(queryParams.longitude) || 0,
+      distance: parseFloat(queryParams.distance) || 0,
+      units: queryParams.unit || 'mi'
+    }
+  ];
 }
 
 function buildTagQuery(queryExpression) {
-  var parts = queryExpression.split(':');
-  var tagQuery;
+  const parts = queryExpression.split(':');
+  let tagQuery;
   if (parts.length > 1) {
     tagQuery = buildAndOperator([
       markDotField(buildTermOperator('tags.key', parts[0])),
