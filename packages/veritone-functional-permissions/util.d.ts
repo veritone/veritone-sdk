da6d9e9dee8d6efe1ff5a408860058b01af11c20
Patrick Martin <pmartin@veritone.com>
2018-10-25 15:32:22 -0700

    export util as namespace

--- packages/veritone-functional-permissions/util.d.ts
+++ packages/veritone-functional-permissions/util.d.ts
@@ -1,56 +1,52 @@
-/**
- * Combine Permissions.
- * This combines two permissions into one.
- */
-function combinePermissions(
-  permissions1: number[],
-  permissions2: number[]
-): number[];
+export namespace util {
+  /**
+   * Combine Permissions.
+   * This combines two permissions into one.
+   */
+  export function combinePermissions(
+    permissions1: number[],
+    permissions2: number[]
+  ): number[];
 
-/**
- * Get Permissions IDs from mask.
- * This converts a mask to permission ids.
- */
-function getPermissionIdsFromMask(permissions: number[]): number[];
+  /**
+   * Get Permissions IDs from mask.
+   * This converts a mask to permission ids.
+   */
+  export function getPermissionIdsFromMask(permissions: number[]): number[];
 
-/**
- * Get Mask From Permissions.
- * This converts permission ids to a mask. Optionally, the caller can pass in a mask that will be used to add the permissions to.
- */
-function getMaskFromPermissionIds(
-  permissionIds: number | number[],
-  mask?: number[]
-): number[];
+  /**
+   * Get Mask From Permissions.
+   * This converts permission ids to a mask. Optionally, the caller can pass in a mask that will be used to add the permissions to.
+   */
+  export function getMaskFromPermissionIds(
+    permissionIds: number | number[],
+    mask?: number[]
+  ): number[];
 
-/**
- * Has Permission.
- * This checks if the user's permissions satisfies the the requested permission check.
- */
-function hasAccessTo(permissionId: number, userPermissions: number[]): boolean;
+  /**
+   * Has Permission.
+   * This checks if the user's permissions satisfies the the requested permission check.
+   */
+  export function hasAccessTo(
+    permissionId: number,
+    userPermissions: number[]
+  ): boolean;
 
-/**
- * Has Any Permission.
- * This checks if the user's permissions satisfies any of the requested permission checks.
- */
-function hasAccessToAny(
-  permissionIds: number[],
-  userPermissions: number[]
-): boolean;
+  /**
+   * Has Any Permission.
+   * This checks if the user's permissions satisfies any of the requested permission checks.
+   */
+  export function hasAccessToAny(
+    permissionIds: number[],
+    userPermissions: number[]
+  ): boolean;
 
-/**
- * Has All Permissions.
- * This checks if the user's permissions satisfies all of the requested permission checks.
- */
-function hasAccessToAll(
-  permissionIds: number[],
-  userPermissions: number[]
-): boolean;
-
-export const util = {
-  combinePermissions,
-  getPermissionIdsFromMask,
-  getMaskFromPermissionIds,
-  hasAccessTo,
-  hasAccessToAll,
-  hasAccessToAny
-};
+  /**
+   * Has All Permissions.
+   * This checks if the user's permissions satisfies all of the requested permission checks.
+   */
+  export function hasAccessToAll(
+    permissionIds: number[],
+    userPermissions: number[]
+  ): boolean;
+}
