4aa4568ba3cda71e9264cceb02a0859ec1531924
Andrew Tung <atung@veritone.com>
2019-03-08 16:13:02 -0800

    Add transcript to the validator

--- packages/veritone-json-schemas/src/index.js
+++ packages/veritone-json-schemas/src/index.js
@@ -1,6 +1,9 @@
 import * as Ajv from 'ajv';
 import * as OBJECT_SCHEMA from '../schemas/vtn-standard/object/object.json';
+import * as TRANSCRIPT_SCHEMA from '../schemas/vtn-standard/transcript/transcript.json';
 import * as MASTER_SCHEMA from '../schemas/vtn-standard/master.json';
+
+import { isEmpty } from 'lodash';
 
 const verifyObject = objectResult => {
   const ajv = new Ajv.default({
@@ -17,4 +20,46 @@ const verifyObject = objectResult => {
   }
 };
 
-export { verifyObject };
+const verifyTranscript = transcriptResult => {
+  const ajv = new Ajv.default({
+    allErrors: true,
+    schemas: [MASTER_SCHEMA, TRANSCRIPT_SCHEMA]
+  });
+
+  const validateBestPath = function (schema, data) {
+    validateBestPath.errors = [];
+    if(data.filter( word => word.bestPath === true).length !== 1) {
+      validateBestPath.errors.push(
+        {
+          keyword: 'requireBestPath',
+          message: 'there should be one and only one bestPath in a transcription lattice',
+          params: {
+            words: data
+          }
+        }
+      );
+      return false
+    }
+
+    if(isEmpty(validateBestPath.errors)) {
+      this.errors = undefined;
+      return true
+    }
+  };
+
+  ajv.addKeyword('requireBestPath', {
+    validate: validateBestPath,
+    errors: true
+  });
+
+  const validate = ajv.compile(TRANSCRIPT_SCHEMA);
+  const valid = validate(transcriptResult);
+
+  if (!valid) {
+    return validate.errors;
+  } else {
+    return true;
+  }
+}
+
+export { verifyObject, verifyTranscript };
--- packages/veritone-json-schemas/src/index.test.js
+++ packages/veritone-json-schemas/src/index.test.js
@@ -1,23 +1,82 @@
-import { verifyObject } from './index';
+import { verifyObject, verifyTranscript } from './index';
+
 import * as objectSummary from '../schemas/vtn-standard/object/examples/summary.json';
 import * as objectSeries from '../schemas/vtn-standard/object/examples/series.json';
 import * as objectFull from '../schemas/vtn-standard/object/examples/full-capability.json';
 
-test('it should verify that a valid object summary passes validation', () => {
-  expect(
-    verifyObject(objectSummary)
-  ).toEqual(true);
+import * as transcriptBasic from '../schemas/vtn-standard/transcript/examples/basic.json';
+import * as transcriptLattice from '../schemas/vtn-standard/transcript/examples/lattice.json';
+import * as transcriptBasicWithConfidence from '../schemas/vtn-standard/transcript/examples/confidence.json';
+
+import * as transcriptBasicEmptyWords from '../schemas/vtn-standard/transcript/invalid-examples/basic.empty.words.json';
+import * as transcriptBasicNoWords from '../schemas/vtn-standard/transcript/invalid-examples/basic.no.words.json';
+import * as transcriptNegativeConfidence from '../schemas/vtn-standard/transcript/invalid-examples/basic.negative.confidence.json';
+import * as transcriptStringConfidence from '../schemas/vtn-standard/transcript/invalid-examples/basic.string.confidence.json';
+import * as transcriptLatticeWithMissingConfidence from '../schemas/vtn-standard/transcript/invalid-examples/lattice.missing.confidence.json';
+import * as transcriptLatticeWithMissingUtterance from '../schemas/vtn-standard/transcript/invalid-examples/lattice.missing.utterance.json';
+import * as transcriptLatticeWithNoBestPath from '../schemas/vtn-standard/transcript/invalid-examples/lattice.no.best.path.json';
+import * as transcriptLatticeWithUtteranceBelowOne from '../schemas/vtn-standard/transcript/invalid-examples/lattice.utterance.below.one.json';
+import * as transcriptBasicWithBrokenHeader from '../schemas/vtn-standard/transcript/invalid-examples/basic.broken.header.json';
+
+
+
+test('it should verify that a valid object summary output passes validation', () => {
+  expect(verifyObject(objectSummary)).toEqual(true);
 });
 
-test('it should verify that a valid object series passes validation', () => {
-  expect(
-    verifyObject(objectSeries)
-  ).toEqual(true);
+test('it should verify that a valid object series output passes validation', () => {
+  expect(verifyObject(objectSeries)).toEqual(true);
 });
 
 test('it should verify that a valid object output with full capability passes validation', () => {
-  expect(
-    verifyObject(objectFull)
-  ).toEqual(true);
+  expect(verifyObject(objectFull)).toEqual(true);
 });
 
+test('it should verify that a basic transcript output passes validation', () => {
+  expect(verifyTranscript(transcriptBasic)).toEqual(true);
+});
+
+test('it should verify that a basic transcript with a broken header DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptBasicWithBrokenHeader)).not.toBe(true);
+});
+
+test('it should verify that a basic transcript output with no words DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptBasicNoWords)).not.toBe(true);
+});
+
+test('it should verify that a basic transcript output with empty words DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptBasicEmptyWords)).not.toBe(true);
+});
+
+test('it should verify that a basic transcript output with a negative confidence DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptNegativeConfidence)).not.toBe(true);
+});
+
+test('it should verify that a basic transcript output with a string confidence DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptStringConfidence)).not.toBe(true);
+});
+
+test('it should verify that a lattice transcript output with missing confidence DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptLatticeWithMissingConfidence)).not.toBe(true);
+});
+
+test('it should verify that a lattice transcript output with missing utterance DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptLatticeWithMissingUtterance)).not.toBe(true);
+});
+
+test('it should verify that a lattice transcript output with no best path DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptLatticeWithNoBestPath)).not.toBe(true);
+});
+
+test('it should verify that a lattice transcript output with an invalid utterance value DOES NOT pass validation', () => {
+  expect(verifyTranscript(transcriptLatticeWithUtteranceBelowOne)).not.toBe(true);
+});
+
+
+test('it should verify that a transcript with lattices output passes validation', () => {
+  expect(verifyTranscript(transcriptLattice)).toEqual(true);
+});
+
+test('it should verify that a basic transcript with confidence output passes validation', () => {
+  expect(verifyTranscript(transcriptBasicWithConfidence)).toEqual(true);
+});
