a7df4f85424a90d27bbb6095900b8518028a0ead
seta-tuha <tu.ha@setacinq.vn>
2019-05-06 17:19:44 +0700

    Update story for breadcrumb component

--- packages/veritone-react-common/src/components/Breadcrumbs/story.js
+++ packages/veritone-react-common/src/components/Breadcrumbs/story.js
@@ -4,10 +4,17 @@ import { action } from '@storybook/addon-actions';
 
 import Breadcrumbs from './';
 
+
 storiesOf('Breadcrumbs', module)
   .add('Full path list', () => {
     const pathList = [
-      { id: 'root' },
+      { id: 'root',
+        icon: <span
+            className="icon-work"
+            style={{ color: '#2196F3', fontSize: '1.5rem'}}
+          />,
+        label: 'root'
+      },
       { id: 'first', label: 'Parent' },
       { id: 'second', label: 'Child' },
       { id: 'third', label: 'GrandChild' }
