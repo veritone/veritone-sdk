e1522b98780edcd2bbe43d18b0e02471b3213017
Andrew Tung <atung@veritone.com>
2019-05-07 18:03:26 -0700

    Changes to file picker
    — Added a non-modal style so it can sit flat in a modal, so we don’t have a modal on top of a modal
    — Added maxFiles limit. When used, it shows {numberOfFilesToUpload} / {maxFiles}.
    — Allow the upload button to be set via props, in case we want it to say “Benchmark” or “Process” as the next step instead.
    — Better error messages when >1 file when multiple is false, better error messages when multiple is true, and maxFiles limit is set.

---
+++ packages/veritone-react-common/src/components/FilePicker/FilePickerHeader/FilePickerFlatHeader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { string, number } from 'prop-types';
+
+import Typography from '@material-ui/core/Typography';
+
+import styles from './styles.scss';
+
+const FilePickerFlatHeader = ({ title, fileCount, maxFiles }) => {
+  return (
+    <div className={styles.filePickerFlatHeader}>
+      <Typography variant={'title'}>
+        {title}
+        &nbsp;<span className={styles.count}>
+          {fileCount}/{maxFiles}
+        </span>
+      </Typography>
+    </div>
+  );
+};
+
+FilePickerFlatHeader.propTypes = {
+  title: string,
+  fileCount: number,
+  maxFiles: number
+};
+
+FilePickerFlatHeader.defaultProps = {
+  title: 'Uploaded',
+  fileCount: 0,
+  maxFiles: 1
+};
+
+export default FilePickerFlatHeader;
