44151868d10a1bed2c4fb28a33806bbdf3b660ec
seta-tuha <tu.ha@setacinq.vn>
2019-05-13 08:47:25 +0700

    Add root icon example

--- packages/veritone-react-common/src/components/Breadcrumbs/story.js
+++ packages/veritone-react-common/src/components/Breadcrumbs/story.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Work from '@material-ui/icons/Work';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
@@ -7,7 +8,7 @@ import Breadcrumbs from './';
 storiesOf('Breadcrumbs', module)
   .add('Full path list', () => {
     const pathList = [
-      { id: 'root' },
+      { id: 'root', icon: <Work /> },
       { id: 'first', label: 'Parent' },
       { id: 'second', label: 'Child' },
       { id: 'third', label: 'GrandChild' }
@@ -21,7 +22,7 @@ storiesOf('Breadcrumbs', module)
   })
   .add('Hidden path list', () => {
     const pathList = [
-      { id: 'root' },
+      { id: 'root', icon: <Work /> },
       { id: 'first', label: 'Parent' },
       { id: 'second', label: 'Child' },
       { id: 'third', label: 'GrandChild' },
