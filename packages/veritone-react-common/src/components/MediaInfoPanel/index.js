b5e23f416708daf74a980f1793f26a3dcd7e10a3
seta-tuha <tu.ha@setacinq.vn>
2019-05-14 14:35:04 +0700

    Add story to media info panel

--- packages/veritone-react-common/src/components/MediaInfoPanel/index.js
+++ packages/veritone-react-common/src/components/MediaInfoPanel/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { bool, string, arrayOf, shape } from 'prop-types';
+import { string, arrayOf, shape, bool } from 'prop-types';
+import { Transition } from 'react-transition-group';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import {
@@ -14,12 +15,14 @@ import MediaPlayer from './SimpleMediaPlayer';
 
 const styles = {
   root: {
-    transition: 'width 0.5s',
     width: 0,
+    flexBasis: 0,
+    overflowX: 'hidden',
+    transition: 'flex-basis 500ms',
     backgroundColor: '#F4FAFE'
   },
   open: {
-    width: 500
+
   },
   content: {
     width: 500,
@@ -43,88 +46,161 @@ const styles = {
   category: {
     fontSize: 14,
     color: 'rgba(0, 0, 0, 0.37)'
-  }
+  },
+  entering: {
+    opacity: 1,
+    flex: '0 0 500px',
+    width: 500
+  },
+  entered: {
+    opacity: 1,
+    flex: '0 0 500px',
+    width: 500
+  },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
 }
 
-const getDuration = (stopTime, startTime)  => {
+const tdoShape = shape({
+  name: string.isRequired,
+  startDateTime: string,
+  stopDateTime: string,
+  thumbnailUrl: string,
+  sourceImageUrl: string,
+  primaryAsset: shape({
+    id: string,
+    contentType: string.isRequired,
+    signedUri: string.isRequired
+  }),
+  streams: arrayOf(shape({
+    uri: string.isRequired,
+    protocol: string.isRequired
+  })),
+  createdDateTime: string.isRequired,
+  modifiedDateTime: string.isRequired
+});
+
+const getDuration = (stopTime, startTime) => {
   const duration = (
     (new Date(stopTime)).getTime() - (new Date(startTime)).getTime()
   ) / 1000;
-  return duration;
+  return Math.floor(duration);
 };
 
+const displayNumber = (number, digits=2) => {
+  if (number === 0) {
+    return '0'.repeat(digits)
+  }
+  return number < Math.pow(10, digits - 1) ?
+    `${'0'.repeat(Math.floor(Math.log10(number)) + 1)}${number}` : number
+}
+
+const formatAsDuration = (seconds) => {
+  const hourNumber = Math.floor(seconds/3600);
+
+  const minuteNumber = Math.floor((seconds - hourNumber * 3600) / 60);
+
+  const secondNumber = Math.floor(
+    (seconds - hourNumber * 3600 - minuteNumber * 60) % 60);
+
+  return (
+    `${displayNumber(hourNumber)}:` +
+    `${displayNumber(minuteNumber)}:` +
+    `${displayNumber(secondNumber)}`
+  );
+}
+
+const MediaInfo = ({ selectedItem, classes }) => (
+  <div className={classes.content}>
+    <MediaPlayer
+      src={selectedItem.primaryAsset.signedUri}
+      streams={selectedItem.streams}
+      poster={selectedItem.thumbnailUrl}
+    />
+    <Typography className={classes.name}>
+      {selectedItem.name}</Typography>
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell
+            className={classNames(
+              classes.tableCell, classes.category
+            )}
+          >
+            Created
+          </TableCell>
+          <TableCell
+            className={classes.tableCell}
+          >
+            {selectedItem.createdDateTime}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell
+            className={classNames(
+              classes.tableCell, classes.category
+            )}
+          >
+            Modified
+          </TableCell>
+          <TableCell
+            className={classes.tableCell}
+          >
+            {selectedItem.modifiedDateTime}
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell
+            className={classNames(
+              classes.tableCell, classes.category
+            )}
+          >
+            Duration
+          </TableCell>
+          <TableCell
+            className={classes.tableCell}
+          >
+            {
+              formatAsDuration(getDuration(
+                selectedItem.startDateTime,
+                selectedItem.stopDateTime
+              ))
+            }
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+);
+
+MediaInfo.propTypes = {
+  selectedItem: tdoShape,
+  classes: shape(Object.keys(styles).reduce((styleShape, key) => ({
+    ...styleShape,
+    [key]: string
+  }), {})),
+}
 
 const MediaInfoPanel = ({ open, classes, selectedItems }) => {
-  const selectedItem = selectedItems.length === 1 ? selectedItems[0] : null;
+  const selectedItem = selectedItems[0];
   return (
-    <div className={classNames(classes.root, { [classes.open]: open })}>
-      <div className={classes.content}>
-        {
-          selectedItem && (
-            <React.Fragment>
-              <MediaPlayer
-                src={selectedItem.primaryAsset.signedUri}
-                streams={selectedItem.streams}
-                poster={selectedItem.thumbnailUrl}
-              />
-              <Typography className={classes.name}>
-                {selectedItem.name}</Typography>
-              <Table>
-                <TableBody>
-                  <TableRow>
-                    <TableCell
-                      className={classNames(
-                        classes.tableCell, classes.category
-                      )}
-                    >
-                      Created
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableCell}
-                    >
-                      {selectedItem.createdDateTime}
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell
-                      className={classNames(
-                        classes.tableCell, classes.category
-                      )}
-                    >
-                      Modified
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableCell}
-                    >
-                      {selectedItem.modifiedDateTime}
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell
-                      className={classNames(
-                        classes.tableCell, classes.category
-                      )}
-                    >
-                      Duration
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableCell}
-                    >
-                      {
-                        getDuration(
-                          selectedItem.startDateTime,
-                          selectedItem.stopDateTime
-                        )
-                      }
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </React.Fragment>
-          )
-        }
-      </div>
-    </div>
+    <Transition in={open && selectedItems.length > 0} timeout={500}>
+      {
+        state => selectedItems.length > 0 && (
+          <div className={classNames(classes.root, classes[state])}>
+            {
+              selectedItems.length > 1 ? (
+                <div selectedNumber={selectedItems.length}>
+                  You have selected {selectedItems.length} items
+                </div>
+              ) : (
+                <MediaInfo selectedItem={selectedItem} classes={classes} />
+              )
+            }
+          </div>
+        )
+      }
+    </Transition>
   )
 }
 
@@ -134,25 +210,7 @@ MediaInfoPanel.propTypes = {
     ...styleShape,
     [key]: string
   }), {})),
-  selectedItems: arrayOf(shape({
-    name: string.isRequired,
-    startDateTime: string,
-    stopDateTime: string,
-    thumbnailUrl: string,
-    sourceImageUrl: string,
-    primaryAsset: shape({
-      id: string,
-      contentType: string.isRequired,
-      signedUri: string.isRequired
-    }),
-    streams: arrayOf(shape({
-      uri: string.isRequired,
-      protocol: string.isRequired
-    })),
-    createdDateTime: string.isRequired,
-    modifiedDateTime: string.isRequired
-  }))
+  selectedItems: arrayOf(tdoShape)
 }
-
 
 export default withStyles(styles)(MediaInfoPanel);
