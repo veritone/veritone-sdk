ece2965421457ce99c3d40f450dd2c6df5e0e8f6
MmtBkn <mbakan@veritone.com>
2019-07-24 09:45:18 -0700

    Add inject reducer and inject saga methods to store

--- packages/veritone-widgets/src/redux/configureStore.js
+++ packages/veritone-widgets/src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import { getBaseMiddlewares, getDevOnlyMiddlewares } from './store';
@@ -26,14 +26,58 @@ const enhancer = composeEnhancers(
   )
 );
 
+function createReducer(asyncReducers) {
+  return combineReducers({
+    ...createRootReducer(),
+    ...asyncReducers
+  });
+}
+
+// runSaga is middleware.run function
+// rootSaga is a your root saga for static saagas
+function createSagaInjector(runSaga, rootSaga) {
+  // Create a dictionary to keep track of injected sagas
+  const injectedSagas = new Map();
+
+  const isInjected = key => injectedSagas.has(key);
+
+  const injectSaga = (key, saga) => {
+    // We won't run saga if it is already injected
+    if (isInjected(key)) {
+      return;
+    }
+
+    // Sagas return task when they executed, which can be used
+    // to cancel them
+    const task = runSaga(saga);
+
+    // Save the task if we want to cancel it in the future
+    injectedSagas.set(key, task);
+  };
+
+  // Inject the root saga as it a staticlly loaded file,
+  injectSaga('root', rootSaga);
+
+  return injectSaga;
+}
+
 export default function configureStore(initialState) {
   const store = {
     ...createStore(createRootReducer(), initialState, enhancer),
     asyncReducers: {},
     runSaga: sagaMiddleware.run,
     sagaMiddleware
-    // close: () => store.dispatch(END);
   };
+
+  // Create an inject reducer function
+  // This function adds the async reducer, and creates a new combined reducer
+  store.injectReducer = (key, asyncReducer) => {
+    store.asyncReducers[key] = asyncReducer;
+    store.replaceReducer(createReducer(store.asyncReducers));
+  };
+
+  // Add injectSaga method to our store
+  store.injectSaga = createSagaInjector(sagaMiddleware.run, rootSaga);
 
   store.runSaga(rootSaga);
 
