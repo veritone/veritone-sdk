c243805bd08e3944bc3f4c57d6c7e68cb7736c1c
seta-linhle <linh.le@setacinq.vn>
2019-08-02 17:36:56 +0700

    advancedPanel unittest

--- packages/veritone-react-common/src/components/AdvancedPanel/index.test.js
+++ packages/veritone-react-common/src/components/AdvancedPanel/index.test.js
@@ -9,6 +9,7 @@ describe('Advanced Panel', () => {
     const advancedOptions = {};
     const onAddAdvancedSearchParams = jest.fn();
     const searchByTag = "search by tag mock";
+
     it('Advanced Search: Should render with the default value filled in', () => {
         const open = true;
         const wrapper = shallow(
@@ -21,7 +22,19 @@ describe('Advanced Panel', () => {
                 searchByTag={searchByTag}
             />
         );
-        expect(wrapper.find('InfoOutlineIcon').exists()).toBeTruthy();
-        // console.log(wrapper);
+        expect(wrapper.find('.title-text').text()).toEqual('Advanced Options');
+        expect(wrapper.find('InfoOutlineIcon').exists());
+        expect(wrapper.find('CloseIcon').exists());
+        expect(wrapper.find('LocationSelect').exists());
+        expect(wrapper.find('RangeSelect').exists());
+        expect(wrapper.find('.only-return-text').first().text()).toEqual(`Only return search results for this ${searchByTag} if they appear in a defined region.`);
+        expect(wrapper.find('.only-return-text').last().text()).toEqual(`Search by the percentage of confidence of this ${searchByTag}.`);
+        
+        wrapper.find('.reset-all').simulate('click');
+        expect(handleReset.mock.calls.length).toEqual(1);
+
+        wrapper.find('.vbtn-cancel').simulate('click');
+        expect(handleClose.mock.calls.length).toEqual(1);
+
     });
 });
