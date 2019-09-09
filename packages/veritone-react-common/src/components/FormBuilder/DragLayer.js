f51d7cf8c35c6a66b708b8a9dccae3ecc4d7161a
seta-tuha <tu.ha@setacinq.vn>
2019-09-09 14:50:20 +0700

    Create Form builder blocks

---
+++ packages/veritone-react-common/src/components/FormBuilder/DragLayer.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useDragLayer } from 'react-dnd'
+import { form } from './configuration';
+import { BlockPreview } from './FormBlocks'
+
+const layerStyles = {
+  position: 'fixed',
+  pointerEvents: 'none',
+  zIndex: 100,
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: '100%',
+}
+function getItemStyles(initialOffset, currentOffset) {
+  if (!initialOffset || !currentOffset) {
+    return {
+      display: 'none',
+    }
+  }
+  let { x, y } = currentOffset;
+
+  const transform = `translate(${x}px, ${y}px)`
+  return {
+    transform,
+    WebkitTransform: transform,
+  }
+}
+const CustomDragLayer = props => {
+  const {
+    itemType,
+    isDragging,
+    item,
+    initialOffset,
+    currentOffset,
+  } = useDragLayer(monitor => ({
+    item: monitor.getItem(),
+    itemType: monitor.getItemType(),
+    initialOffset: monitor.getInitialSourceClientOffset(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging(),
+  }))
+  function renderItem() {
+    switch (itemType) {
+      case form:
+        if (item.isBlock) {
+          return <BlockPreview type={item.id} />
+        }
+        return null;
+      default:
+        return null
+    }
+  }
+  if (!isDragging) {
+    return null
+  }
+  return (
+    <div style={layerStyles}>
+      <div
+        style={getItemStyles(initialOffset, currentOffset)}
+      >
+        {renderItem()}
+      </div>
+    </div>
+  )
+}
+export default CustomDragLayer
---
+++ packages/veritone-react-common/src/components/FormBuilder/FormBlocks/index.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { string, func, node } from 'prop-types';
+import {
+  Title,
+  RadioButtonChecked,
+  CheckBoxOutlined,
+  CalendarToday,
+  StarBorderOutlined,
+  Notes
+} from '@material-ui/icons';
+import { noop } from 'lodash';
+import MenuIcon from '@material-ui/icons/Menu';
+import EditAttributesIcon from '@material-ui/icons/EditAttributes';
+import { useDrag } from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
+import { form } from '../configuration';
+import styles from './styles.scss';
+
+
+export const blockMaps = {
+  textInput: {
+    label: 'Input Text',
+    icon: <Title />
+  },
+  paragraph: {
+    label: 'Paragraph',
+    icon: <Notes />,
+  },
+  radio: {
+    label: 'Radio',
+    icon: <RadioButtonChecked />
+  },
+  checkBox: {
+    label: 'Checkbox',
+    icon: <CheckBoxOutlined />
+  },
+  dateTime: {
+    label: 'Date Time',
+    icon: <CalendarToday />
+  },
+  rating: {
+    label: 'Rating',
+    icon: <StarBorderOutlined />
+  },
+  select: {
+    label: 'Select',
+    icon: <MenuIcon />
+  },
+  switch: {
+    label: 'Switch',
+    icon: <EditAttributesIcon />
+  }
+}
+
+export const blockTypes = [
+  {
+    type: 'textInput',
+    label: 'Input Text',
+    icon: <Title />
+  },
+  {
+    type: 'paragraph',
+    label: 'Paragraph',
+    icon: <Notes />,
+  },
+  {
+    type: 'radio',
+    label: 'Radio',
+    icon: <RadioButtonChecked />
+  },
+  {
+    type: 'checkBox',
+    label: 'Checkbox',
+    icon: <CheckBoxOutlined />
+  },
+  {
+    type: 'dateTime',
+    label: 'Date Time',
+    icon: <CalendarToday />
+  },
+  {
+    type: 'rating',
+    label: 'Rating',
+    icon: <StarBorderOutlined />
+  },
+  {
+    type: 'select',
+    label: 'Select',
+    icon: <MenuIcon />
+  },
+  {
+    type: 'switch',
+    label: 'Switch',
+    icon: <EditAttributesIcon />
+  }
+];
+
+export function BlockPreview({ type }) {
+  return (
+    <div className={styles['drag-preview-container']}>
+      {blockMaps[type].icon}
+      {blockMaps[type].label}
+    </div>
+  )
+}
+
+BlockPreview.propTypes = {
+  type: string
+}
+
+export default function Block({ type, label, icon, removeBlock }) {
+  const [{ opacity }, drag, preview] = useDrag({
+    item: {
+      id: type,
+      type: form,
+      isBlock: true
+    },
+    collect: monitor => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+    end: (item, monitor) => {
+      if (!monitor.didDrop() && !isNaN(item.index)) {
+        removeBlock(item.index);
+      }
+    }
+  });
+
+  React.useEffect(() => {
+    preview(getEmptyImage(), { captureDraggingState: true })
+  }, []);
+
+
+  return (
+    <div ref={drag} style={{ opacity }} className={styles['block-container']}>
+      {icon}
+      <p>{label}</p>
+    </div>
+  )
+}
+
+Block.propTypes = {
+  type: string,
+  label: string,
+  icon: node,
+  removeBlock: func
+}
+
+Block.defaultProps = {
+  removeBlock: noop
+}
