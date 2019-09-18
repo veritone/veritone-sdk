cb231a86c5f3353f2b03ff1dbef38b590cab1f0e
seta-tuha <tu.ha@setacinq.vn>
2019-09-18 19:46:17 +0700

    Merge react migrate

--- packages/veritone-react-common/src/components/FormBuilder/FormItems/FormItems.story.js
+++ packages/veritone-react-common/src/components/FormBuilder/FormItems/FormItems.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { DndProvider } from "react-dnd-cjs";
-import HTML5Backend from "react-dnd-html5-backend-cjs";
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import { noop } from 'lodash';
 import formItems, { PreviewWrapper } from './';
 
