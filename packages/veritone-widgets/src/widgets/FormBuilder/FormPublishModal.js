0d02461c9e4d77157b34f5e7d0b98fe8f7d2f928
SETA-tuha <tu.ha@setacinq.vn>
2019-10-15 14:54:27 +0700

    Create form publish modal

--- packages/veritone-widgets/src/widgets/FormBuilder/FormPublishModal.js
+++ packages/veritone-widgets/src/widgets/FormBuilder/FormPublishModal.js
@@ -3,16 +3,18 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
-import { NullState } from 'veritone-react-common';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import styles from './FormPublishModal.scss';
 
 export default function FormPublishModal({
   fetchLocations,
@@ -21,49 +23,109 @@ export default function FormPublishModal({
   locations,
   selectedLocations,
   onClose,
-  onSelect,
-  onSelectAll,
+  onChange,
   open,
+  onPublish
 }) {
+
   React.useEffect(() => {
     if (!(locationLoading || loactionLoaded)) {
       fetchLocations();
     }
   }, []);
 
+  const onSelectBox = React.useCallback((event) => {
+    const { id } = event.currentTarget.dataset;
+    onChange({
+      ...selectedLocations,
+      [id]: !selectedLocations[id]
+    });
+  }, [selectedLocations, onChange]);
+
+  const onSelectAll = React.useCallback((event) => {
+    const { checked } = event.target;
+    onChange(Object.keys(selectedLocations).reduce((l, id) => ({
+      ...l,
+      [id]: checked
+    }), {}))
+  }, [selectedLocations, onChange]);
+
+  const numberSelectedLocations = Object.keys(selectedLocations)
+    .filter(id => selectedLocations[id])
+    .length;
+
   return (
     <Dialog open={open}>
-      <DialogTitle>Publish Form</DialogTitle>
-      <DialogContent>
-        <Typography>
-          Make this form available to other users of your organization. Before you publish, select the location(s) that this form will appear in your Veritone apps.
-        </Typography>
+      <DialogTitle className={styles.dialogTitle}>
+        Publish Form
+      </DialogTitle>
+      <DialogContent className={styles.dialogContent}>
+        <DialogContentText>
+          <Typography variant="caption">
+            Make this form available to other users of your organization. Before you publish, select the location(s) that this form will appear in your Veritone apps.
+          </Typography>
+        </DialogContentText>
         {
           (locationLoading ? (
-            "Loading"
+            <CircularProgress className={styles.dialogLoading} />
           ) : (locations.length === 0) ? (
             "No locations found"
           ) :
-              <Table>
-                <TableHead>
-                  <Checkbox
-                    inputProps={{ 'aria-label': 'select all desserts' }}
-                  />
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-
-                  </TableRow>
-                </TableBody>
-              </Table>
+              <div>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  display="block"
+                >
+                  Available Locations
+                </Typography>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          checked={locations.length === numberSelectedLocations}
+                          indeterminate={numberSelectedLocations > 0 && numberSelectedLocations < locations.length}
+                          onChange={onSelectAll}
+                          color="primary"
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="caption">
+                          All locations
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                    {
+                      locations.map(({ id, name }) => (
+                        <TableRow key={id} data-id={id} onClick={onSelectBox}>
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              checked={Boolean(selectedLocations[id])}
+                              color="primary"
+                            />
+                          </TableCell>
+                          <TableCell>{name}</TableCell>
+                        </TableRow>
+                      ))
+                    }
+                  </TableBody>
+                </Table>
+              </div>
           )
         }
       </DialogContent>
       <DialogActions>
-        <Button>Cancel</Button>
-        <Button>Publish</Button>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button variant="contained" color="primary" onClick={onPublish}>
+          Publish
+        </Button>
       </DialogActions>
     </Dialog>
   )
+}
 
+FormPublishModal.defaultProps = {
+  locations: [],
+  selectedLocations: {}
 }
