dd1cfe2afb1dd674699657137536332183a412c2
seta-tuha <tu.ha@setacinq.vn>
2019-11-15 10:49:37 +0700

    Fixing styling and remove comment

--- packages/veritone-react-common/src/components/FormBuilder/FormBuilder.style.js
+++ packages/veritone-react-common/src/components/FormBuilder/FormBuilder.style.js
@@ -3,28 +3,24 @@ import { createStyles, makeStyles } from '@material-ui/core/styles';
 export default makeStyles(_ =>
   createStyles({
     formBuilder: {
-      display: 'flex',
+      'display': 'flex',
       'max-width': '1200px',
-      margin: 'auto',
-      height: '100%',
-    }
-    ,
+      'margin': 'auto',
+      'height': '100%',
+    },
     formBlocks: {
       'max-width': '350px',
       'flex-basis': '50%',
-    }
-    ,
+    },
     formBlocksTitle: {
       'margin-left': '8px !important',
       'margin-bottom': '12px !important',
-    }
-    ,
+    },
     blocksWrapper: {
       'display': 'flex',
       'flex-flow': 'row',
       'flex-wrap': 'wrap',
-    }
-    ,
+    },
     blocksPreview: {
       'flex-basis': '70%',
       'max-height': '100%',
@@ -32,15 +28,13 @@ export default makeStyles(_ =>
       'overflow': '-moz-scrollbars-none',
 
       '&::-webkit-scrollbar': {
-        width: '0 !important'
+        'width': '0 !important'
       }
-    }
-    ,
+    },
     configurationContainer: {
       'padding-left': '10px',
       'width': '400px',
-    }
-    ,
+    },
     formConfigurationTitle: {
       'margin-bottom': '12px !important',
     }
