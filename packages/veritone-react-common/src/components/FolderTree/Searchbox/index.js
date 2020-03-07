a300f2e45f49f9626aeafde76b559460be825864
seta-linhle <41145625+seta-linhle@users.noreply.github.com>
2020-03-07 08:46:50 +0700

    ES-117: Folder Name search behavior (#469)
    * ES-117: Folder Name search behavior
    
    * ES-117: search behavior - change styles
    
    Co-authored-by: Mohamed <MmtBkn@users.noreply.github.com>

--- packages/veritone-react-common/src/components/FolderTree/Searchbox/index.js
+++ packages/veritone-react-common/src/components/FolderTree/Searchbox/index.js
@@ -4,20 +4,19 @@ import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
+import CloseIcon from '@material-ui/icons/Close';
 import styles from './styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   ...styles
 }));
 
-export default function Searchbox({
-  onSearch,
-  placeholder = 'Search'
-}) {
+export default function Searchbox({ onSearch, placeholder = 'Search' }) {
   const classes = useStyles();
   const inputRef = React.createRef();
   const [focusing, setFocusing] = React.useState(false);
   const [data, setData] = React.useState('');
+
   function handleSearch() {
     if (!focusing && !data) {
       setFocusing(true);
@@ -37,27 +36,42 @@ export default function Searchbox({
       onSearch(data);
     }
   }
+  function handleRemoveText() {
+    setData('');
+    onSearch('');
+  }
   return (
     <div className={classes.searchBoxRoot}>
       <InputBase
         onChange={handleChangeInput}
         inputRef={inputRef}
+        value={data}
         className={classes.input}
         placeholder={placeholder}
         inputProps={{ 'aria-label': 'Search-input' }}
         onKeyDown={handleKeyDown}
       />
-      <IconButton
-        onClick={handleSearch}
-        className={classes.iconButton}
-        aria-label="search-button"
-      >
-        <SearchIcon />
-      </IconButton>
+      {data !== '' ? (
+        <IconButton
+          className={classes.iconButton}
+          onClick={handleRemoveText}
+          size="small"
+        >
+          <CloseIcon color="action" />
+        </IconButton>
+      ) : (
+        <IconButton
+          onClick={handleSearch}
+          className={classes.iconButton}
+          aria-label="search-button"
+        >
+          <SearchIcon />
+        </IconButton>
+      )}
     </div>
   );
 }
 Searchbox.propTypes = {
   onSearch: func.isRequired,
   placeholder: string
-}
+};
--- packages/veritone-react-common/src/components/FolderTree/Searchbox/styles.js
+++ packages/veritone-react-common/src/components/FolderTree/Searchbox/styles.js
@@ -1,18 +1,21 @@
 export default {
-    searchBoxRoot: {
-        padding: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: '100%',
-        borderBottom: '1px solid #e3e3e3',
-        '& $input': {
-            marginLeft: '8px',
-            flex: '1',
-        },
-        '& $iconButton': {
-            padding: '10px',
-        }
+  searchBoxRoot: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    borderBottom: '1px solid #e3e3e3',
+    '& $input': {
+      marginLeft: '8px',
+      flex: '1'
     },
-    input: {},
-    iconButton: {}
-}
+    '& $iconButton': {
+      padding: '10px'
+    }
+  },
+  input: {},
+  iconButton: {},
+  closeIcon: {
+    color: '#7e7e7e'
+  }
+};
