de189b77142ff67a15285c16a5a4c934eb1ccbf7
seta-adam <adam@setacinq.vn>
2020-03-13 19:21:30 +0700

    fix bug

--- packages/veritone-react-common/src/components/Notifier/NotificationList.js
+++ packages/veritone-react-common/src/components/Notifier/NotificationList.js
@@ -7,7 +7,6 @@ import RefreshIcon from '@material-ui/icons/Refresh';
 import ErrorIconSvg from 'images/icon_error.svg';
 import CheckIconSvg from 'images/icon_check_circle.svg';
 
-
 import { string, arrayOf, oneOf, shape, func, node } from 'prop-types';
 
 import classNames from 'classnames';
@@ -39,25 +38,19 @@ export const notificationListPropTypes = arrayOf(
     customNode: node,
     description1: string,
     description2: string,
+    description3: string,
     statusDescription: string,
     onActionClick: func,
     onRemoveClick: func,
-    introAnimation: oneOf([
-      INTRO_NONE,
-      INTRO_FADE_IN
-    ]),
-    outroAnimation: oneOf([
-      OUTRO_NONE,
-      OUTRO_FADE_OUT,
-      OUTRO_SLIDE_OUT
-    ])
+    introAnimation: oneOf([INTRO_NONE, INTRO_FADE_IN]),
+    outroAnimation: oneOf([OUTRO_NONE, OUTRO_FADE_OUT, OUTRO_SLIDE_OUT])
   })
 );
 
 export default class NotificationList extends React.Component {
   static propTypes = {
     notifications: notificationListPropTypes
-  }
+  };
 
   state = {};
 
@@ -68,8 +61,8 @@ export default class NotificationList extends React.Component {
       return {
         currentEntries,
         removedEntryIds: [],
-        allEntries: currentEntries,
-      }
+        allEntries: currentEntries
+      };
     } else if (_.isEqual(prevState.currentEntries, currentEntries)) {
       return prevState;
     } else {
@@ -78,7 +71,7 @@ export default class NotificationList extends React.Component {
       //Find New Entry
       const newEntryIds = currentEntries.reduce((ids, entry) => {
         const entryId = entry.id;
-        const matchedEntry = _.find(prevEntries, {id: entryId});
+        const matchedEntry = _.find(prevEntries, { id: entryId });
         !matchedEntry && ids.push(entryId);
         return ids;
       }, []);
@@ -86,7 +79,7 @@ export default class NotificationList extends React.Component {
       //Find Removed Entry
       const removedEntryIds = prevEntries.reduce((ids, entry) => {
         const entryId = entry.id;
-        const matchedEntry = _.find(currentEntries, {id: entryId});
+        const matchedEntry = _.find(currentEntries, { id: entryId });
         !matchedEntry && ids.push(entryId);
         return ids;
       }, []);
@@ -94,16 +87,15 @@ export default class NotificationList extends React.Component {
       //Merge new entries & prev entries
       let allEntries = currentEntries.reduce((accumulator, entry) => {
         let newAccumulator = accumulator.concat([]);
-        const matchedPrevIndex = _.findIndex(prevEntries, {id: entry.id});
+        const matchedPrevIndex = _.findIndex(prevEntries, { id: entry.id });
         if (matchedPrevIndex !== -1) {
-
           //Append outdated entries for outro animation
           if (matchedPrevIndex > 0) {
             const outDatedEntries = prevEntries.splice(0, matchedPrevIndex);
             newAccumulator = accumulator.concat(outDatedEntries);
           }
 
-          prevEntries.splice(0, 1);   //Remove duplicated entry
+          prevEntries.splice(0, 1); //Remove duplicated entry
         }
 
         newAccumulator.push(entry);
@@ -118,104 +110,139 @@ export default class NotificationList extends React.Component {
         currentEntries,
         newEntryIds,
         removedEntryIds
-      }
+      };
     }
   }
 
   handleEntryActionClicked = entry => event => {
     entry.onActionClick && entry.onActionClick(entry);
-  }
+  };
   handleRowClicked = entry => event => {
     entry.onItemClick && entry.onItemClick(entry);
-  }
+  };
 
   handleEntryRemoveClick = entry => event => {
     entry.onRemoveClick && entry.onRemoveClick(entry);
-  }
+  };
 
-  drawCustomItem = (entryData) => {
+  drawCustomItem = entryData => {
     const entryId = entryData.id;
     const isNewEntry = _.includes(this.state.newEntryIds, entryId);
-    const isRemoved = !isNewEntry && _.includes(this.state.removedEntryIds, entryId);
+    const isRemoved =
+      !isNewEntry && _.includes(this.state.removedEntryIds, entryId);
 
     return (
-      <div key={entryId} className={
-        classNames({
-          [styles.fadeIn]: isNewEntry && entryData.introAnimation === INTRO_FADE_IN,
-          [styles.fadeOut]: isRemoved && entryData.outroAnimation === OUTRO_FADE_OUT,
-          [styles.slideOut]: isRemoved && entryData.outroAnimation === OUTRO_SLIDE_OUT,
-          [styles.noOutro]: isRemoved && (!entryData.outroAnimation || entryData.outroAnimation === OUTRO_NONE)
-        })
-      }>
-        { entryData.customNode }
+      <div
+        key={entryId}
+        className={classNames({
+          [styles.fadeIn]:
+            isNewEntry && entryData.introAnimation === INTRO_FADE_IN,
+          [styles.fadeOut]:
+            isRemoved && entryData.outroAnimation === OUTRO_FADE_OUT,
+          [styles.slideOut]:
+            isRemoved && entryData.outroAnimation === OUTRO_SLIDE_OUT,
+          [styles.noOutro]:
+            isRemoved &&
+            (!entryData.outroAnimation ||
+              entryData.outroAnimation === OUTRO_NONE)
+        })}
+      >
+        {entryData.customNode}
       </div>
     );
-  }
+  };
 
   drawGenericItem = (originalData, formatedData) => {
     const entryId = formatedData.id;
     const isNewEntry = _.includes(this.state.newEntryIds, entryId);
-    const isRemoved = !isNewEntry && _.includes(this.state.removedEntryIds, entryId);
+    const isRemoved =
+      !isNewEntry && _.includes(this.state.removedEntryIds, entryId);
 
     return (
-      <div onClick={this.handleRowClicked(originalData)} key={formatedData.id} className={
-        classNames(
-          styles.entry,
-          {
-            [styles.unread]: originalData.unread,
-            [styles.fadeIn]: isNewEntry && formatedData.introAnimation === INTRO_FADE_IN,
-            [styles.fadeOut]: isRemoved && formatedData.outroAnimation === OUTRO_FADE_OUT,
-            [styles.slideOut]: isRemoved && formatedData.outroAnimation === OUTRO_SLIDE_OUT,
-            [styles.noOutro]: isRemoved && (!formatedData.outroAnimation || formatedData.outroAnimation === OUTRO_NONE)
-          }
-        )
-      }>
+      <div
+        onClick={this.handleRowClicked(originalData)}
+        key={formatedData.id}
+        className={classNames(styles.entry, {
+          [styles.unread]: originalData.unread,
+          [styles.fadeIn]:
+            isNewEntry && formatedData.introAnimation === INTRO_FADE_IN,
+          [styles.fadeOut]:
+            isRemoved && formatedData.outroAnimation === OUTRO_FADE_OUT,
+          [styles.slideOut]:
+            isRemoved && formatedData.outroAnimation === OUTRO_SLIDE_OUT,
+          [styles.noOutro]:
+            isRemoved &&
+            (!formatedData.outroAnimation ||
+              formatedData.outroAnimation === OUTRO_NONE)
+        })}
+      >
         <div className={classNames(styles.visualStatus)}>
-          { formatedData.statusIcon }
+          {formatedData.statusIcon}
         </div>
         <div className={classNames(styles.description)}>
-          <div className={classNames(styles.title)}>{formatedData.description1}</div>
-          <div className={classNames(styles.subtitle)}>{formatedData.description2}</div>
+          <div className={classNames(styles.title)}>
+            {formatedData.description1}
+          </div>
+          <div className={classNames(styles.subtitle)}>
+            {formatedData.description2}
+          </div>
+        </div>
+        <div className={classNames(styles.secondExtra)}>
+          <div className={classNames(styles.subtitle)}>
+            {formatedData.description3}
+          </div>
         </div>
         <div className={classNames(styles.extra)}>
           <div className={classNames(styles.description)}>
             {formatedData.statusDescription}
           </div>
           <div className={classNames(styles.actions)}>
-          {
-            formatedData.onActionClick &&
-            <IconButton
-              color="default"
-              className={classNames(styles.iconButton)}
-              onClick={this.handleEntryActionClicked(originalData)}
-              data-veritone-element={formatedData.btnActionTrackName || 'notification-action-button'}
-            >
-              { formatedData.actionIcon }
-            </IconButton>
-          }
-          {
-            formatedData.onRemoveClick &&
-            <IconButton
-              color="default"
-              className={classNames(styles.iconButton)}
-              onClick={this.handleEntryRemoveClick(originalData)}
-              data-veritone-element={formatedData.btnRemoveTrackName || 'notification-remove-button'}
-            >
-              {
-                formatedData.removeIcon || <CloseIcon className={classNames(styles.icon)} style={{ fontSize: "20px" }} />
-              }
-            </IconButton>
-          }
+            {formatedData.onActionClick && (
+              <IconButton
+                color="default"
+                className={classNames(styles.iconButton)}
+                onClick={this.handleEntryActionClicked(originalData)}
+                data-veritone-element={
+                  formatedData.btnActionTrackName ||
+                  'notification-action-button'
+                }
+              >
+                {formatedData.actionIcon}
+              </IconButton>
+            )}
+            {formatedData.onRemoveClick && (
+              <IconButton
+                color="default"
+                className={classNames(styles.iconButton)}
+                onClick={this.handleEntryRemoveClick(originalData)}
+                data-veritone-element={
+                  formatedData.btnRemoveTrackName ||
+                  'notification-remove-button'
+                }
+              >
+                {formatedData.removeIcon || (
+                  <CloseIcon
+                    className={classNames(styles.icon)}
+                    style={{ fontSize: '20px' }}
+                  />
+                )}
+              </IconButton>
+            )}
           </div>
         </div>
       </div>
     );
-  }
+  };
 
-  drawFailedItem = (entryData) => {
+  drawFailedItem = entryData => {
     const failedEntryData = {
       statusIcon: <img src={ErrorIconSvg} />,
-      actionIcon: <RefreshIcon className={classNames(styles.icon)} style={{ fontSize: "20px" }} />,
+      actionIcon: (
+        <RefreshIcon
+          className={classNames(styles.icon)}
+          style={{ fontSize: '20px' }}
+        />
+      ),
       statusDescription: 'failed',
       btnActionTrackName: 'failed-notification-retry-botton',
       btnRemoveTrackName: 'failed-notification-remove-button',
@@ -227,7 +254,7 @@ export default class NotificationList extends React.Component {
     return this.drawGenericItem(entryData, failedEntryData);
   };
 
-  drawCompleteItem = (entryData) => {
+  drawCompleteItem = entryData => {
     const completeEntryData = {
       statusIcon: <img src={CheckIconSvg} />,
       statusDescription: 'complete',
@@ -241,7 +268,7 @@ export default class NotificationList extends React.Component {
     return this.drawGenericItem(entryData, completeEntryData);
   };
 
-  drawPreparingItem = (entryData) => {
+  drawPreparingItem = entryData => {
     const preparingEntryData = {
       statusIcon: this.drawSpinner(),
       statusDescription: 'preparing',
@@ -255,7 +282,7 @@ export default class NotificationList extends React.Component {
     return this.drawGenericItem(entryData, preparingEntryData);
   };
 
-  drawProcessingItem = (entryData) => {
+  drawProcessingItem = entryData => {
     const processingEntryData = {
       statusIcon: this.drawSpinner(),
       statusDescription: 'processing',
@@ -270,16 +297,14 @@ export default class NotificationList extends React.Component {
   };
 
   drawSpinner = () => {
-    return (
-        <div className={classNames(styles.spinner)} />
-    );
+    return <div className={classNames(styles.spinner)} />;
   };
 
   render() {
     return (
       <div className={classNames(styles.notificationList)}>
-      {
-        this.state.allEntries.map(entry => {
+        
+        {this.state.allEntries.map(entry => {
           switch (entry.type) {
             case TYPE_PREPARING:
               return this.drawPreparingItem(entry);
@@ -299,8 +324,7 @@ export default class NotificationList extends React.Component {
             default:
               return null;
           }
-        })
-      }
+        })}
       </div>
     );
   }
