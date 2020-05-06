01674ae6ab2dd81aa84c9141f64a5d8cb92e142c
seta-linhle <41145625+seta-linhle@users.noreply.github.com>
2020-05-06 19:41:33 +0700

    ES-610: Collection still exists when its folder is deleted (#497)

--- packages/veritone-widgets/src/redux/modules/folder/sagas/deleteFolderSaga.js
+++ packages/veritone-widgets/src/redux/modules/folder/sagas/deleteFolderSaga.js
@@ -1,4 +1,5 @@
 import {
+  all,
   call,
   takeEvery,
   put,
@@ -9,6 +10,7 @@ import omit from 'lodash/omit';
 import { handleRequest } from '../helper';
 import * as actions from '../actions';
 import * as folderSelector from '../selector';
+import { folderType } from '../reducer';
 import { showNotification } from '../../notifications';
 
 export default function* deleteFolderSaga() {
@@ -17,21 +19,18 @@ export default function* deleteFolderSaga() {
 
 function* deleteFolder(action) {
   const { folderId, workSpace } = action.payload;
+  const { type: folderRootType } = yield select(folderSelector.config);
+  const { childsType, deleteContent } = folderType[folderRootType];
   yield put(actions.deleteFolderStart(folderId));
-  const foldersData = yield select(folderSelector.folderData);
   const folderSelected = yield select(folderSelector.selected);
   const { selectable } = yield select(folderSelector.config);
   const rootIds = yield select(folderSelector.rootFolderIds);
-  const folder = get(foldersData, ['byId', folderId], {});
-  if (folder.hasContent) {
-    //TODO improving
-  }
   const queryFolder = `query folder($id:ID!){
     folder(id: $id){
       id
       name
       orderIndex
-      parent{
+      parent {
         id
       }
       treeObjectId
@@ -50,6 +49,18 @@ function* deleteFolder(action) {
     return yield put(actions.deleteFolderError(folderId));
   }
   const { orderIndex, parent, treeObjectId } = get(response, 'data.folder', {});
+  const childContents = yield getAllContent(folderId, childsType);
+  if (childContents.length) {
+    const deleteContentQuery = `
+    mutation ${deleteContent}($id: ID!){
+      ${deleteContent}(id: $id) {
+        id
+      }
+    }`;
+    yield all(childContents.map(content =>
+      call(handleRequest, { query: deleteContentQuery, variables: { id: content.id } }))
+    );
+  }
   const query = `
     mutation deleteFolder($id: ID!,$orderIndex: Int! ){
       deleteFolder(input:{
@@ -82,5 +93,57 @@ function* deleteFolder(action) {
     }
   }
   yield put(actions.deleteFolderSuccess(folderId, parent.id));
+  yield put(showNotification("Delete folder successful"));
   return yield put(actions.updateSearchData());
+}
+
+function* getAllContent(folderId, childsType) {
+  let contentArr = [];
+  function* getContents(currentId) {
+    const queryFolder = `query folder($id:ID!){
+      folder(id: $id){
+        id
+        name
+        orderIndex
+        parent {
+          id
+        }
+        treeObjectId
+        childFolders(offset: 0, limit: 999) {
+          records {
+            id
+            name
+          }
+        }
+        ${childsType}(limit: 999) {
+          records {
+            id
+            name
+          }
+        }
+      }
+    }`;
+    const variablesFolder = {
+      id: currentId
+    }
+    const { error, response } = yield call(handleRequest,
+      {
+        query: queryFolder,
+        variables: variablesFolder
+      }
+    );
+    if (error) {
+      return [];
+    }
+    const childFolders = get(response, 'data.folder.childFolders.records', []);
+    const childContents = get(response, ['data', 'folder', childsType, 'records'], []);
+    if (childFolders.length > 0) {
+      yield all(childFolders.map(folder =>
+        getContents(folder.id, childsType))
+      );
+    }
+    contentArr = contentArr.concat(childContents);
+  }
+  yield getContents(folderId);
+  return contentArr;
 }
