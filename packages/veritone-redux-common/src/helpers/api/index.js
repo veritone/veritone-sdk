a9e25710a2f2fcd2502c72f5059c611b2a491426
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2021-05-21 16:55:42 -0700

    remove logic in getCredentialsMode and allow support for all domains

--- packages/veritone-redux-common/src/helpers/api/index.js
+++ packages/veritone-redux-common/src/helpers/api/index.js
@@ -21,10 +21,5 @@ export function commonHeaders(state) {
 }
 
 export function getCredentialsMode() {
-  const origin = (window && window.origin) || '';
-  // app.veritone.com on port 80 in prod, or
-  // ie. local.veritone.com on any port in dev
-  const isVeritoneInternalApp = origin.match(/\.veritone\.com(:\d{1,5})?$/);
-  const isAiwareAnywhereInternalApp = origin.match(/\.aiware\.com(:\d{1,5})?$/);
-  return (isVeritoneInternalApp || isAiwareAnywhereInternalApp) ? 'include' : 'omit';
+  return 'include';
 }
